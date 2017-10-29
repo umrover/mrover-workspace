470d9cb7940518dbf4dd57c728f505c5747f7a42
Spencer Farrand <spenfarr@umich.edu>
2017-10-29 14:31:10 -0400

    [teleop] implemented kill switch and locks for lcm

--- onboard/teleop/src/__main__.py
+++ onboard/teleop/src/__main__.py
@@ -7,6 +7,8 @@ from rover_msgs import Odometry, Joystick, Motors
 
 
 lcm_ = aiolcm.AsyncLCM()
+kill_switch = False
+lock = asyncio.Lock()
 
 
 def connection_state_changed(c):
@@ -27,27 +29,37 @@ def deadzone(magnitude, threshold):
 
 
 def joystick_callback(channel, msg):
+    global kill_switch
+
+    if kill_switch:
+        return
+
     input_data = Joystick.decode(msg)
 
     new_motor = Motors()
 
-    magnitude = deadzone(input_data.forward_back, 0.1)
-    theta = deadzone(input_data.left_right, 0.1)
+    if input_data.kill:
+        new_motor.left = 0
+        new_motor.right = 0
+        kill_switch = True
+    else:
+        magnitude = deadzone(input_data.forward_back, 0.1)
+        theta = deadzone(input_data.left_right, 0.1)
 
-    new_motor.left = abs(magnitude)
-    new_motor.right = new_motor.left
+        new_motor.left = abs(magnitude)
+        new_motor.right = new_motor.left
 
-    if theta > 0:
-        new_motor.right *= 1 - theta/2
-    elif theta < 0:
-        new_motor.left *= 1 + theta/2
+        if theta > 0:
+            new_motor.right *= 1 - theta/2
+        elif theta < 0:
+            new_motor.left *= 1 + theta/2
 
-    if magnitude < 0:
-        new_motor.left *= -1
-        new_motor.right *= -1
-    elif magnitude == 0:
-        new_motor.left += theta/2
-        new_motor.right -= theta/2
+        if magnitude < 0:
+            new_motor.left *= -1
+            new_motor.right *= -1
+        elif magnitude == 0:
+            new_motor.left += theta/2
+            new_motor.right -= theta/2
 
     lcm_.publish('/motor', new_motor.encode())
 
@@ -58,7 +70,9 @@ async def transmit_fake_odometry():
         new_odom.latitude_deg = random.uniform(42, 43)
         new_odom.longitude_deg = random.uniform(-84, -82)
         new_odom.bearing_deg = random.uniform(0, 359)
-        lcm_.publish('/odom', new_odom.encode())
+
+        with await lock:
+            lcm_.publish('/odom', new_odom.encode())
 
         # print("Published new odometry")
         await asyncio.sleep(0.5)
@@ -69,19 +83,14 @@ async def transmit_fake_joystick():
         new_joystick = Joystick()
         new_joystick.forward_back = random.uniform(-1, 1)
         new_joystick.left_right = random.uniform(-1, 1)
-        lcm_.publish('/joystick', new_joystick.encode())
+        new_joystick.kill = False
+
+        with await lock:
+            lcm_.publish('/joystick', new_joystick.encode())
 
         print("Published new joystick\nfb: {}   lr: {}"
               .format(new_joystick.forward_back, new_joystick.left_right))
         await asyncio.sleep(0.5)
-
-
-async def lc_handle():
-    try:
-        while True:
-            await lcm_.handle()
-    except KeyboardInterrupt:
-        pass
 
 
 def motor_callback(channel, msg):
@@ -96,4 +105,4 @@ def main():
     lcm_.subscribe("/joystick", joystick_callback)
     # lcm_.subscribe('/motor', motor_callback)
     run_coroutines(hb.loop(), transmit_fake_odometry(),
-                   lc_handle())
+                   transmit_fake_joystick(), lcm_.loop())
