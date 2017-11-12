0af8652ec5cc5ba94403191588a81a0984374553
Faruk Kurtovic <kurtovic@umich.edu>
2017-11-12 14:00:12 -0500

    [nav] started work on layer1

--- onboard/nav/layer1.cpp
+++ onboard/nav/layer1.cpp
@@ -1,40 +1,39 @@
 #include <cmath>
+#include <tgmath.h>
 #include <chrono>
 #include <lcm/lcm-cpp.hpp>
+#include "rover_msgs/Odometry.hpp"
+#include <cassert>
 
-#define PI 3.1415926
+#define earthRadiusMeters 6371000
+#define PI 3.141592654
 #define JOYSTICK_CHANNEL "/joystick"
 
-class GPS {
-public:
-    double latitude;
-    double longitude; 
-    GPS() : latitude(0), longitude(0) {}
-};
-
 // function to calculate bearing from location 1 to location 2
-// GPS latitude and longitude in degrees
+// Odometry latitude_deg and longintude_deg in degrees
 // return bearing in degrees
-double calc_bearing(const GPS &gps1, const GPS &gps2);
+double calc_bearing(const rover_msgs::Odometry &odom1, const rover_msgs::Odometry &odom2);
+
+double estimate_noneuclid(const rover_msgs::Odometry &start, const rover_msgs::Odometry &dest);
 
 // function to turn rover to face destination
-// GPS latitude and longitude in degrees
-void turn_to_destination(const GPS &cur_gps, const GPS &dest_gps, const double time, const double threshold);
+// Odometry latitude_deg and longintude_deg in degrees
+void turn_to_destination(const rover_msgs::Odometry &cur_odom, const rover_msgs::Odometry &dest_odom, const double time, const double threshold);
 
 // function to drive to destination
-// GPS latitude and longitude in degrees
+// Odometry latitude_deg and longintude_deg in degrees
 // distance: how much to distance to move in one iteration
 // threshold: to accept and stop moving certain difference within current location and destination
-void go_setpoint(const GPS &dest_gps, const double time, const double gps_threshold, const double bearing_threshold);
+void go_setpoint(const rover_msgs::Odometry &dest_odom, const double time, const double odom_threshold, const double bearing_threshold);
 
 lcm::LCM lcm;
 
-Joystick make_joystick_msg(const double forward_back, const double left_right, const bool kill) {
-	Joystick js;
-	js.forward_back = forward_back;
-	js.left_right = left_right;
-	js.kill = kill;
-	return js;
+Joystick make_joystick_msg(const double forwardesback, const double left_right, const bool kill) {
+    Joystick js;
+    js.forwardesback = forwardesback;
+    js.left_right = left_right;
+    js.kill = kill;
+    return js;
 }
 
 void publish_joystick_for(const std::chrono::milliseconds ms, const Joystick &js)
@@ -48,28 +47,28 @@ void publish_joystick_for(const std::chrono::milliseconds ms, const Joystick &js
 }
 
 void publish_zero_joystick() {
-	lcm.publish(make_joystick_msg(0, 0, false));
-}	
+    lcm.publish(make_joystick_msg(0, 0, false));
+}   
 
 // assuming that left is 1
 void turn_left(const double time) {
     // publish_joystick_for(time, make_joystick_msg(0.5, 0.5, false));
-	publish_joystick_for(time, make_joystick_msg(0, 1, false));
-	publish_zero_joystick();
+    publish_joystick_for(time, make_joystick_msg(0, 1, false));
+    publish_zero_joystick();
 }
 
 // assuming that right is -1
 void turn_right(const double time) {
     // publish_joystick_for(time, make_joystick_msg(0.5, -0.5, false));
-	publish_joystick_for(time, make_joystick_msg(0, -1, false));
-	publish_zero_joystick();
+    publish_joystick_for(time, make_joystick_msg(0, -1, false));
+    publish_zero_joystick();
 }
 
 // function tp move rover forward by certain distance
 // send joystick input for certain time
 void drive_forward(const double time) {
     publish_joystick_for(time, make_joystick_msg(1, 0, false));
-	publish_zero_joystick();
+    publish_zero_joystick();
 }
 
 // function tp move rover backward by certain distance
@@ -80,12 +79,12 @@ void drive_backward(const double time) {
 }
 
 // convert degree to radian
-double degree_to_radian(const double x) {
+inline double degree_to_radian(const double x) {
     return x * PI / 180;
 }
 
 // convert radian to degree
-double radian_to_degree(const double x) {
+inline double radian_to_degree(const double x) {
     return x * 180 / PI;
 }
 
@@ -101,60 +100,78 @@ double get_current_bearing() {
 // angle in degree
 // send joystick input for certain time
 void turn_rover(const double angle, const double time) {
-	if(angle > 0) {
-		turn_left(time);
-	}
-	else if(angle < 0) {
-		turn_right(time);
-	}
+    if(angle > 0) {
+        turn_left(time);
+    }
+    else if(angle < 0) {
+        turn_right(time);
+    }
 }
 
-// functin to get current gps
-GPS get_current_gps() {
-    return GPS();
+double calc_bearing(const rover_msgs::Odometry &start, const rover_msgs::Odometry &dest) {
+    double strt_lat = degree_to_radian(start.latitude_deg);
+    double strt_long = degree_to_radian(start.longintude_deg);
+    double dest_lat = degree_to_radian(dest.latitude_deg);
+    double dest_long = degree_to_radian(dest.longintude_deg);
+
+
+    double vert_component_dist = earthRadiusMeters * std::sin(dest_lat - strt_lat);
+    double noneuc_dist = estimate_noneuclid(start,dest);
+
+    double bearing_phi = std::acos(vert_component_dist / noneuc_dist);
+
+    if(vert_component_dist < 1 && vert_component_dist > -1){
+        dest_long - strt_long > 0 ? bearing_phi = degree_to_radian(90) : bearing_phi = degree_to_radian(270);
+    }
+
+    return bearing_phi;
+  
 }
 
-double calc_bearing(const GPS &gps1, const GPS &gps2) {
-    double lat1 = degree_to_radian(gps1.latitude);
-    double long1 = degree_to_radian(gps1.longitude);
-    double lat2 = degree_to_radian(gps2.latitude);
-    double long2 = degree_to_radian(gps2.longitude);
-
-    double y = std::sin(long2 - long1) * std::cos(lat2);
-    double x = std::cos(lat1) * std::sin(lat2) - std::sin(lat1) * std::cos(lat2) * std::cos(long2 - long1);
-
-    return radian_to_degree(std::atan2(y, x));
-}
-
-void turn_to_destination(const GPS &cur_gps, const GPS &dest_gps, const double time, const double threshold) {
+void turn_to_destination(const rover_msgs::Odometry &cur_odom, const rover_msgs::Odometry &dest_odom, const double time, const double threshold) {
     double cur_bear = get_current_bearing();
-    double dest_bearing = calc_bearing(cur_gps, dest_gps);
+    double dest_bearing = calc_bearing(cur_odom, dest_odom);
     while(abs(cur_bear - dest_bearing) > threshold) {
-    	turn_rover(dest_bearing - cur_bear, time);
-    	cur_bear = get_current_bearing();
+        turn_rover(dest_bearing - cur_bear, time);
+        cur_bear = get_current_bearing();
     }
     return;
 }
 
-bool within_threshold(const GPS &gps1, const GPS &gps2, const double threshold) {
-    return (std::abs(gps1.latitude - gps2.latitude) <= threshold ) && (std::abs(gps1.longitude - gps2.longitude) <= threshold);
+double estimate_noneuclid(const rover_msgs::Odometry &start, const rover_msgs::Odometry &dest){
+    double st_lat = degree_to_radian(start.lat);
+    double st_long = degree_to_radian(start.long); 
+    double des_lat = degree_to_radian(dest.lat);
+    double des_long = degree_to_radian(dest.long);
+
+    double dlon = (des_long - st_long) * cos((st_lat + des_lat) / 2);
+    dlon *= dlon;
+    double dlat = (des_lat - st_lat);
+    dlat *= dlat;
+
+    assert(dlon > 0 && dlat > 0);
+    return sqrt(dlon + dlat)*earthRadiusMeters;
 }
 
-void go_setpoint(const GPS &dest_gps, const double time, const double gps_threshold, const double bearing_threshold) {
-    // current gps location
-    GPS cur_gps = get_current_gps();
+bool within_threshold(const rover_msgs::Odometry &odom1, const rover_msgs::Odometry &odom2, const double threshold) {
+    return estimate_noneuclid(odom1,odom2) <= threshold;
+}
+
+void go_setpoint(const rover_msgs::Odometry &dest_odom, const double time, const double odom_threshold, const double bearing_threshold) {
+    // current odom location
+    rover_msgs::Odometry cur_odom = get_current_odom();
 
     // turn rover
-    turn_to_destination(cur_gps, dest_gps, time, bearing_threshold);
+    turn_to_destination(cur_odom, dest_odom, time, bearing_threshold);
 
-    while(!within_threshold(dest_gps, cur_gps, gps_threshold)) {
+    while(!within_threshold(dest_odom, cur_odom, odom_threshold)) {
         // move rover
         drive_forward(time);
 
         // readjust rover bearing
-        cur_gps = get_current_gps();
+        cur_odom = get_current_odom();
 
-        turn_to_destination(cur_gps, dest_gps, time, bearing_threshold);
+        turn_to_destination(cur_odom, dest_odom, time, bearing_threshold);
     }
 
     return;
