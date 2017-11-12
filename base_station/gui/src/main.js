213fd32fae9e1f20e970b844661d18a2b364b998
ianhrobinson <ihr0@umich.edu>
2017-11-12 12:54:30 -0500

    [GUI] Added GPS.html

--- base_station/gui/src/main.js
+++ base_station/gui/src/main.js
@@ -2,32 +2,30 @@ import App from './components/App.html'
 import LCMBridge from 'bridge'
 
 const app = new App({
-    target: document.querySelector('main')
+  target: document.querySelector('main')
 })
 
 const lcm_ = new LCMBridge(
-    "ws://localhost:8001",
-    // Update WebSocket connection state
-    (online) => {
-        app.set({
-            websocket_connected: online
-        })
-    },
-    // Update LCM connection state
-    (online) => {
-        app.set({
-            lcm_connected: online
-        })
-    },
-    // Subscribed LCM message received
-    (msg) => app.lcm_message_recv(msg),
-    // Subscriptions
-    [
-        { 
-            'topic': '/odom',
-            'type': 'Odometry'
-        }
-    ]
+  "ws://localhost:8001",
+  // Update WebSocket connection state
+  (online) => {
+    app.set({
+      websocket_connected: online
+    })
+  },
+  // Update LCM connection state
+  (online) => {
+    app.set({
+      lcm_connected: online
+    })
+  },
+  // Subscribed LCM message received
+  (msg) => app.lcm_message_recv(msg),
+  // Subscriptions
+  [{
+    'topic': '/odom',
+    'type': 'Odometry'
+  }]
 )
 
 window.addEventListener("gamepadconnected", e => {
@@ -39,29 +37,31 @@ window.addEventListener("gamepadconnected", e => {
 
 // Go to http://html5gamepad.com/ to check joystick button/axes indices
 const JOYSTICK_CONFIG = {
-    "forward_back" : 1,
-    "left_right" : 2,
-    "dampen" : 3,
-    "kill" : 4,
-    "restart" : 5
+  "forward_back": 1,
+  "left_right": 2,
+  "dampen": 3,
+  "kill": 4,
+  "restart": 5
 };
 
 window.setInterval(() => {
-    const gamepads = navigator.getGamepads();
-    const gamepad = gamepads[0];
-    if(!gamepad)
-        return;
+  const gamepads = navigator.getGamepads();
+  const gamepad = gamepads[0];
+  if (!gamepad)
+    return;
 
 
-    const joystickData = {'type' : 'Joystick',
-                        'forward_back' : -gamepad.axes[JOYSTICK_CONFIG["forward_back"]],
-                        'left_right' : gamepad.axes[JOYSTICK_CONFIG["left_right"]],
-                        'dampen' : gamepad.axes[JOYSTICK_CONFIG["dampen"]],
-                        'kill' : gamepad.buttons[JOYSTICK_CONFIG["kill"]]['pressed'],
-                        'restart' : gamepad.buttons[JOYSTICK_CONFIG["restart"]]['pressed']};
+  const joystickData = {
+    'type': 'Joystick',
+    'forward_back': -gamepad.axes[JOYSTICK_CONFIG["forward_back"]],
+    'left_right': gamepad.axes[JOYSTICK_CONFIG["left_right"]],
+    'dampen': gamepad.axes[JOYSTICK_CONFIG["dampen"]],
+    'kill': gamepad.buttons[JOYSTICK_CONFIG["kill"]]['pressed'],
+    'restart': gamepad.buttons[JOYSTICK_CONFIG["restart"]]['pressed']
+  };
 
-    console.log(joystickData['dampen']);
+  console.log(joystickData['dampen']);
 
-    lcm_.publish('/joystick', joystickData);
+  lcm_.publish('/joystick', joystickData);
 
 }, 100);
