60a670bccce1f78bd24d9fed7e7e583cd13e1fd0
Nathan Moos <moosingin3space@gmail.com>
2017-11-26 14:37:46 -0500

    [simulator] nav simulator now publishes goal messages
    simulators: added goal message publishing
    nav: added odom/goal reception
    jarvis: fixed `jarvis exec` when running C++ programs

--- onboard/nav/main.cpp
+++ onboard/nav/main.cpp
@@ -1,16 +1,46 @@
-#include <cstdio>
+#include <iostream>
 #include <thread>
 #include <chrono>
 #include <lcm/lcm-cpp.hpp>
 #include "rover_msgs/Odometry.hpp"
 #include "layers.hpp"
 
+struct LcmHandlers {
+    void odom(
+            const lcm::ReceiveBuffer *rbuf,
+            const std::string &channel,
+            const rover_msgs::Odometry *odom) {
+        std::cout << "currently at " << odom->latitude_deg << " deg "
+                  << odom->latitude_min << " min latitude by "
+                  << odom->longitude_deg << " deg " << odom->longitude_min
+                  << " min longitude bearing " << odom->bearing_deg
+                  << " degrees True" << std::endl;
+    }
+
+    void goal(
+            const lcm::ReceiveBuffer *rbuf,
+            const std::string &channel,
+            const rover_msgs::Odometry *odom) {
+        std::cout << "going to " << odom->latitude_deg << " deg "
+                  << odom->latitude_min << " min latitude by "
+                  << odom->longitude_deg << " deg " << odom->longitude_min
+                  << " min longitude." << std::endl;
+    }
+};
+
 int main(int argc, char **argv) {
     using namespace std::chrono_literals;
     Auton::System sys;
-    // TODO create more Auton::Layer objects
-    while (true) {
-        std::this_thread::sleep_for(2s);
+    lcm::LCM lcm_;
+    if (!lcm_.good()) {
+        std::cerr << "error: cannot create LCM" << std::endl;
+        return 1;
     }
+    LcmHandlers handlers;
+    lcm_.subscribe("/odom", &LcmHandlers::odom, &handlers);
+    lcm_.subscribe("/goal", &LcmHandlers::goal, &handlers);
+
+    // TODO create Auton::Layer objects and attach them to the System
+    while (lcm_.handle() == 0);
     return 0;
 }
