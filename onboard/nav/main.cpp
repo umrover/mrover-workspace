6cc2dcbea593311567d09ae05ab6c4ae6267db4b
Joseph Alestra <Joe@Joes-MacBook.local>
2017-11-28 22:19:21 -0500

    [Auton] implemented PID loop for turning to destination

--- onboard/nav/main.cpp
+++ onboard/nav/main.cpp
@@ -4,27 +4,32 @@
 #include <lcm/lcm-cpp.hpp>
 #include "rover_msgs/Odometry.hpp"
 #include "layers.hpp"
+#include "Layer1_subclass.h"
 
 struct LcmHandlers {
+  Layer1 & layer1;
     void odom(
             const lcm::ReceiveBuffer *rbuf,
             const std::string &channel,
             const rover_msgs::Odometry *odom) {
-        std::cout << "currently at " << odom->latitude_deg << " deg "
+        layer1.cur_odom = *odom;
+        /*std::cout << "currently at " << odom->latitude_deg << " deg "
                   << odom->latitude_min << " min latitude by "
                   << odom->longitude_deg << " deg " << odom->longitude_min
                   << " min longitude bearing " << odom->bearing_deg
-                  << " degrees True" << std::endl;
+                  << " degrees True" << std::endl;*/
     }
 
     void goal(
             const lcm::ReceiveBuffer *rbuf,
             const std::string &channel,
             const rover_msgs::Odometry *odom) {
-        std::cout << "going to " << odom->latitude_deg << " deg "
+        layer1.goal_odom = *odom;
+        layer1.resume();
+        /*std::cout << "going to " << odom->latitude_deg << " deg "
                   << odom->latitude_min << " min latitude by "
                   << odom->longitude_deg << " deg " << odom->longitude_min
-                  << " min longitude." << std::endl;
+                  << " min longitude." << std::endl;*/
     }
 };
 
@@ -32,11 +37,12 @@ int main(int argc, char **argv) {
     using namespace std::chrono_literals;
     Auton::System sys;
     lcm::LCM lcm_;
+    Layer1 layer1(sys, lcm_);
     if (!lcm_.good()) {
         std::cerr << "error: cannot create LCM" << std::endl;
         return 1;
     }
-    LcmHandlers handlers;
+    LcmHandlers handlers{ layer1 };
     lcm_.subscribe("/odom", &LcmHandlers::odom, &handlers);
     lcm_.subscribe("/goal", &LcmHandlers::goal, &handlers);
 
