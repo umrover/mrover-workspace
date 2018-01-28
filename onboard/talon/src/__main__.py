2ade202684cde3b5eaf23da0c160201f11d77a86
Milo Hartsoe <milo.red.duck@gmail.com>
2018-01-28 12:28:23 -0500

    [hwi] fix encoder messagge and add set demand
    fixes init of Rover in onboard/talon and add ability to set demand

--- onboard/talon/src/__main__.py
+++ onboard/talon/src/__main__.py
@@ -4,6 +4,7 @@ from rover_common.aiohelper import run_coroutines
 from rover_msgs import DriveMotors
 from rover_msgs import SetParam
 from rover_msgs import Encoder
+from rover_msgs import SetDemand
 from .lowlevel import LowLevel
 from enum import Enum
 import can
@@ -30,11 +31,13 @@ class Rover:
         for i in range(NUM_TALONS):
             self.talons.append(LowLevel(can.interface.Bus(), i))
         # make left_back talon follow left_front
-        self.talons[RoverTalons.left_back].set_demand(
-            RoverTalons.left_front, talon_srx.TalonControlMode.kFollower.value)
+        self.talons[RoverTalons.left_back.value].set_demand(
+            RoverTalons.left_front.value,
+            talon_srx.TalonControlMode.kFollowerMode.value)
         # make right_back talon follow right_front
-        self.talons[RoverTalons.right_back].set_demand(
-            RoverTalons.right_front, talon_srx.TalonControlMode.kFollower.value)
+        self.talons[RoverTalons.right_back.value].set_demand(
+            RoverTalons.right_front.value,
+            talon_srx.TalonControlMode.kFollowerMode.value)
 
     def left_drive(self, speed):
         converted_speed = 1023 * speed
@@ -62,16 +65,22 @@ def set_param_callback(channel, msg):
     rover.set_param(m.paramID, m.value)
 
 
+def set_demand_callback(channel, msg):
+    print("Recieving {} bytes from {}".format(len(msg), channel))
+    m = SetDemand.decode(msg)
+    rover.talons[m.deviceID].set_demand(m.value, m.control_mode)
+
+
 async def publish_response():
     while True:
         ec = Encoder()
-        ec.left_front = int(
+        ec.joint_a = int(
             rover.talons[RoverTalons.left_front.value].read_enc_value() or 0)
-        ec.left_back = int(
+        ec.joint_b = int(
             rover.talons[RoverTalons.left_back.value].read_enc_value() or 0)
-        ec.right_front = int(
+        ec.joint_c = int(
             rover.talons[RoverTalons.right_front.value].read_enc_value() or 0)
-        ec.right_back = int(
+        ec.joint_d = int(
             rover.talons[RoverTalons.right_back.value].read_enc_value() or 0)
 
         lcm_.publish('/encoder', ec.encode())
@@ -85,4 +94,5 @@ def main():
     rover = Rover()
     lcm_.subscribe("/motor", motor_callback)
     lcm_.subscribe("/setparam", set_param_callback)
+    lcm_.subscribe("/setdemand", set_demand_callback)
     run_coroutines(lcm_.loop(), publish_response())
