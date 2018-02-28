d3462bfc57936217bab19b5a9d6a3b3653b0b35e
spenfarr <spenfarr@umich.edu>
2018-02-28 16:07:30 -0500

    [gui] reworked control switching

--- onboard/teleop/src/__main__.py
+++ onboard/teleop/src/__main__.py
@@ -34,7 +34,6 @@ kill_motor = False
 lock = asyncio.Lock()
 enc_in = None
 drill_on = Toggle(False)
-door_open = Toggle(False)
 
 
 def connection_state_changed(c, _):
@@ -151,8 +150,17 @@ def sa_control_callback(channel, msg):
     new_sa_motors.lead_screw = math.copysign(
         new_sa_motors.lead_screw ** 2,
         new_sa_motors.lead_screw)
-    val = door_open.new_reading(xbox.left_bumper > 0.5)
-    new_sa_motors.door_actuator = 0.50 if val else -0.50
+    new_sa_motors.drill = -0.6 if val else 0.0
+    if deadzone(xbox.left_js_y, 0.5) > 0.5:
+        new_sa_motors.lead_screw = 0.4
+    if deadzone(xbox.left_js_y, 0.5) < -0.5:
+        new_sa_motors.lead_screw = -0.4
+    if xbox.d_pad_up:
+        new_sa_motors.door_actuator = 0.5
+    elif xbox.d_pad_down:
+        new_sa_motors.door_actuator = -0.5
+    else:
+        new_sa_motors.door_actuator = 0
     new_sa_motors.cache = deadzone(xbox.right_js_x, 0.2)
 
     print('drill? {} door: {} lead-screw: {}'.format(
