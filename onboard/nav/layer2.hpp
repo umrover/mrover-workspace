3b0366d1287bdabe02a3be5e8f583606e23b7264
amverni <amverni@umich.edu>
2018-03-13 20:03:11 -0400

    [nav] finite state machine refactor

--- onboard/nav/layer2.hpp
+++ onboard/nav/layer2.hpp
@@ -12,28 +12,41 @@
 #include "rover_msgs/Obstacle.hpp"
 #include "rover_msgs/TennisBall.hpp"
 #include <deque>
-
 #include "thor.hpp"
+#include <iostream>
+#include <cassert>
+#include <iomanip>
 
 const double EARTH_CIRCUM = 40075000; // meters
 const double LAT_METER_IN_MINUTES = 0.0005389625; // meters/minute
-const int PATH_WIDTH = 50; // meters
+const int PATH_WIDTH = 2; // meters
 const double DIRECTION_THRESH = 1.0;
 const double INNER_SEARCH_THRESH = .0001;
 const double BALL_THRESH = 2; // meters
-const double CV_THRESH = 3; // meters
-#define NAV_STATUS_CHANNEL "/nav_status"
+const double CV_THRESH = 10; // meters
 
-//typedef rover_msgs::Odometry odom;
-// typedef rover_msgs::Waypoint waypoint;
+#define NAV_STATUS_CHANNEL "/nav_status"
 using waypoint = rover_msgs::Waypoint;
+using odom = rover_msgs::Odometry;
+using TennisBall = rover_msgs::TennisBall;
+using Obstacle = rover_msgs::Obstacle;
+using AutonState = rover_msgs::AutonState;
 
 enum class State {
-	translational, 
-	faceN,
-	rotation120,
-	rotation240,
-	rotationN,
+	off,						// 0
+	turn_and_drive,				// 10
+	search_face0,				// 20
+	search_turn120,				// 21
+	search_turn240,				// 22
+	search_turn360,				// 23
+	search_turn_and_drive,		// 24 
+	turn_to_ball,				// 28
+	drive_to_ball,				// 29
+	turn_around_obs,			// 30
+	drive_around_obs,			// 31
+	search_turn_around_obs,		// 32
+	search_drive_around_obs,	// 33
+	done						// 1
 };
 
 class Layer2 {
@@ -57,9 +70,53 @@ public:
 
 private:
 	struct CourseData{
-		std::deque <rover_msgs::Waypoint> overall;
+		std::deque <waypoint> overall;
 		int64_t hash;
 	};
+
+	class BallUpdate {
+	private: 
+		const TennisBall * prev_ball;
+
+	public:
+		BallUpdate(const TennisBall * ball) : prev_ball(ball) {}
+
+		bool operator()(const TennisBall & new_ball) const {
+			return new_ball.found != prev_ball->found;
+		}
+	};
+
+	class ObstacleUpdate {
+	private:
+		const Obstacle * prev_obs;
+
+	public:
+		ObstacleUpdate(const Obstacle * obs) : prev_obs(obs) {}
+
+		bool operator()(const Obstacle & new_obs) const {
+			return new_obs.detected != prev_obs->detected;
+		}
+	};
+
+	class AutonStateUpdate {
+	private:
+		const AutonState * prev_state;
+
+	public:
+		AutonStateUpdate(const AutonState * state) : prev_state(state) {}
+
+		bool operator()(const AutonState & new_state) const {
+			return new_state.is_auton != prev_state->is_auton;
+		}
+	};
+
+	odom rover_cur_odom;
+	TennisBall rover_ball;
+	Obstacle rover_obstacle;
+	CourseData rover_course;
+	AutonState rover_auton_state;
+	odom dummy_obs_odom;
+	waypoint search_center;
 
   	State state;
   	lcm::LCM & lcm_;
@@ -78,11 +135,12 @@ private:
 
 	void add_four_points_to_search(const waypoint & origin_way);
 
-	void search_func(const waypoint & search_origin);
+	// void search_func(const waypoint & search_origin);
 
-	bool rotation_pred(const odom & cur_odom, const double theta1, const double theta2);
+	bool rotation_pred(const odom & cur_odom, const double theta1, 
+					   const double theta2);
 
-	void search_rotation(const odom & cur_odom);
+	// void search_rotation(const odom & cur_odom);
 
 	bool waypoint_eq(const waypoint & way1, const waypoint & way2);
 
@@ -92,13 +150,19 @@ private:
 
 	void long_meter_mins(const odom & cur_odom);
   
-  void go_to_ball(odom & cur_odom, TennisBall & ball);
+  	// void go_to_ball(odom & cur_odom, TennisBall & ball);
 
-	void obstacle_avoidance(odom & cur_odom, Obstacle & obs);
+	// void obstacle_avoidance(odom & cur_odom, Obstacle & obs);
   
-  void turn(odom & cur_odom, double bearing_offset);
+  	void turn(odom & cur_odom, double bearing_offset);
   
-  void obstacle_dummy_odom(odom & new_odom, const double cur_bearing,
-                           const double dist);
+  	void obstacle_dummy_odom(odom & new_odom, const double cur_bearing,
+                             const double dist);
+
+	void updateRover();
+
+	void updateRover_ballUnconditional();
+
+	void updateRover_obsUnconditional();
 
 };
