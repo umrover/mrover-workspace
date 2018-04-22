b4d74bffcc7cc24b20abe8d27fcfb2821e27b444
Jason Whitfield <jtwhit@umich.edu>
2018-04-22 01:00:49 -0400

    [pis] added gstreamer interface on raspberry pis

---
+++ pi_zeroes/video/src/__main__.py
@@ -0,0 +1,50 @@
+import sys
+import gi
+gi.require_version("Gst", "1.0")
+from gi.repository import Gst # noqa
+from rover_common import aiolcm # noqa
+from rover_common.aiohelper import run_coroutines # noqa
+from rover_msgs import PiCamera # noqa
+
+lcm_ = aiolcm.AsyncLCM()
+
+pipeline = None
+pipeline_string = ("v4l2src device=/dev/video0"
+                   " ! image/jpeg, width=1280, height=720, framerate=30/1"
+                   " ! jpegdec ! videoconvert ! queue ! x264enc ! queue"
+                   " ! rtph264pay ! udpsink host=192.168.0.98 port=5000")
+index = -1
+
+
+def init_pipeline():
+    global pipeline
+    global pipeline_string
+    Gst.init(None)
+    pipeline = Gst.parse_launch(pipeline_string)
+
+    print("Initialized gstreamer pipeline.")
+
+
+def camera_callback(channel, msg):
+    global pipeline
+    global index
+
+    cam_info = PiCamera.decode(msg)
+
+    if cam_info.active_index == index:
+        pipeline.set_state(Gst.State.PLAYING)
+        print("Playing pipeline.")
+    else:
+        pipeline.set_state(Gst.State.READY)
+        print("Pausing pipeline.")
+
+
+def main():
+    global index
+    index = int(sys.argv[1])
+
+    init_pipeline()
+
+    lcm_.subscribe("/pi_camera", camera_callback)
+
+    run_coroutines(lcm_.loop())
