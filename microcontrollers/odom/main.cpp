0949f77d62f635adc807f527a162ecebd3242d50
rayfok <rayfok@umich.edu>
2018-04-29 16:12:52 -0400

    [mbed] send sensor data over UART

--- microcontrollers/odom/main.cpp
+++ microcontrollers/odom/main.cpp
@@ -1,67 +1,24 @@
 #include <mbed.h>
 #include "math_3d.hpp"
 #include "imu.hpp"
+#include "pins.hpp"
+#include "frame_serial.hpp"
 
-#define DT 2.0
-#define LAT_MIN     0x50100000
-#define LAT_DEG     0x50200000
-#define LON_MIN     0x50300000
-#define LON_DEG     0x50400000
-#define SPEED_KNOTS 0x50500000
-#define TRACK_ANGLE 0x50600000
-#define ROLL        0x50700000
-#define PITCH       0x50800000
-#define YAW         0x50800000
+DigitalOut led(LED1_PROD);
+// Serial dbg(USBTX, USBRX, 9600);
+Serial serial(RTK_UART_TX1_PROD, RTK_UART_RX1_PROD, 115200);
+Serial gps_in(RTK_UART_TX2_PROD, RTK_UART_RX2_PROD, 115200);
+Imu imu(IMU_I2C_SDA_PROD, IMU_I2C_SCL_PROD);
+Thread thread1, thread2;
+Mutex mutex;
 
-DigitalOut green_led(LED1);
-DigitalOut red_led(LED2);
-Serial dbg(USBTX, USBRX, 9600);
-Serial gps_in(PA_0, PA_1, 115200);
-Imu imu(PB_7, PB_6);
-Thread thread;
-CAN can1(PB_9, PB_10); // Change these pins
-
-int rtk_counter = 0;
-int imu_counter = 0;
-
-
-float fxp_10_22_to_float(int fxp) {
-    float CONVERSION_CONST = 0.0000002384185791015625;
-    float raw = float(fxp);
-    return raw * CONVERSION_CONST;
-}
-
-
-int float_to_fxp_10_22(float val) {
-    float CONVERSION_CONST = float(0x400000);
-    return int(val * CONVERSION_CONST);
-}
-
-
-void send_msg(int arbId, int counter, float data, int bytes_to_send=8) {
-    // Encode msg into CAN data format
-    int encoded_data = float_to_fxp_10_22(data);
-
-    char msg[8];
-    memcpy(&msg[0], &encoded_data, sizeof(int));
-    memcpy(&msg[4], &counter, sizeof(int));
-
-    // Send the message
-    if (can1.write(CANMessage(arbId, msg, bytes_to_send))) {
-        dbg.printf("Message sent: %d\n", data);
-    }
-}
-
-
-void recv_msg() {
-    CANMessage msg;
-    if (can1.read(msg)) {
-        int data;
-        memcpy(&data, &msg.data, sizeof(int));
-        dbg.printf("Message received: %d\n", fxp_10_22_to_float(data));
-    }
-}
-
+float roll = 0;
+float pitch = 0;
+float bearing = 0;
+float LatDeg = 0;
+float LatMin = 0;
+float LonDeg = 0;
+float LonMin = 0;
 
 namespace ReadState {
     enum ReadState {
@@ -129,7 +86,7 @@ private:
     size_t data_type_idx = 0;
     int latDegMultiplier, lonDegMultiplier;
     double latMinMultiplier, lonMinMultiplier, speedMultiplier, angleMultiplier;
-    int LatDeg, LonDeg;
+    float LatDeg, LonDeg;
     double LatMin, LonMin, SpeedKnots, TrackAngle;
     int count = 0;
 
@@ -258,29 +215,25 @@ private:
     void wait_until_end(char c){
         if(c == '\n'){
             next_state = ReadState::Invalid;
-            print();
-            send_can_msg();
+            // print();
+            update_globals();
             valid = false;
         }
         return;
     }
 
-    void print(){
-        red_led = false;
-        green_led = true;
-        dbg.printf("%d deg %2.7f min by %d deg %2.7f min moving at %2.4f knots along course %3.2f\n",
-                LatDeg, LatMin, LonDeg, LonMin, SpeedKnots, TrackAngle);
-    }
+    // void print(){
+    //     // dbg.printf("%d deg %2.7f min by %d deg %2.7f min moving at %2.4f knots along course %3.2f\n",
+    //     //         LatDeg, LatMin, LonDeg, LonMin, SpeedKnots, TrackAngle);
+    // }
 
-    void send_can_msg(){
-        send_msg(LAT_DEG, rtk_counter, LatDeg);
-        send_msg(LAT_MIN, rtk_counter, LatMin);
-        send_msg(LON_DEG, rtk_counter, LonDeg);
-        send_msg(LON_MIN, rtk_counter, LonMin);
-        send_msg(SPEED_KNOTS, rtk_counter, SpeedKnots);
-        send_msg(TRACK_ANGLE, rtk_counter, TrackAngle);
-
-        ++rtk_counter;
+    void update_globals() {
+        mutex.lock();
+        ::LatDeg = LatDeg;
+        ::LatMin = LatMin;
+        ::LonDeg = LonDeg;
+        ::LonMin = LonMin;
+        mutex.unlock();
     }
 };
 
@@ -289,11 +242,10 @@ void get_IMU() {
     while(true) {
         Math::Vector3f V = { 0 };
         while (!imu.valid()) {
-            green_led = false;
-            red_led = true;
-            dbg.printf("invalid IMU, check connection\r\n");
+            led = false;
+            // dbg.printf("invalid IMU, check connection\r\n");
             imu.init();
-            dbg.printf("reconnecting...\r\n");
+            // dbg.printf("reconnecting...\r\n");
             wait(1.0);
         }
 
@@ -305,60 +257,78 @@ void get_IMU() {
             Math::Vector3f a = imu.accelerometer();
             Math::Vector3f m = imu.magnetometer();
             // Math::Vector3f g = imu.gyroscope();
-            dbg.printf("-----------RAW VALUES------------\r\n");
-            dbg.printf("A: (x=%.4f, y=%.4f, z=%.4f) m/s^2\r\n", a.x, a.y, a.z);
-            dbg.printf("M: (x=%.4f, y=%.4f, z=%.4f) uT\r\n", m.x, m.y, m.z);
+            // dbg.printf("-----------RAW VALUES------------\r\n");
+            // dbg.printf("A: (x=%.4f, y=%.4f, z=%.4f) m/s^2\r\n", a.x, a.y, a.z);
+            // dbg.printf("M: (x=%.4f, y=%.4f, z=%.4f) uT\r\n", m.x, m.y, m.z);
             //dbg.printf("G: (x=%.4f, y=%.4f, z=%.4f) deg/s\r\n", g.x, g.y, g.z);
-            dbg.printf("\r\n");
+            // dbg.printf("\r\n");
 
             m.x -= V.x;
             m.y -= V.y;
             m.z -= V.z;
 
-            float roll = atan2(a.y, a.z);
+            mutex.lock();
+            roll = atan2(a.y, a.z);
             float By = m.y*cosf(roll) - m.z*sinf(roll);
             float Mz = m.z*cosf(roll) + m.y*sinf(roll);
             float Az = a.y*sinf(roll) + a.z*cosf(roll);
 
-            float pitch = atan2(-a.x, Az);
+            pitch = atan2(-a.x, Az);
             float Bz = m.x*cosf(pitch) + m.z*sinf(pitch);
 
             float yaw = atan2(-By, Bz);
-            float bearing = yaw * (180.0f/M_PI);
+            bearing = yaw * (180.0f/M_PI);
             if (bearing < 0) {
                 bearing += 360.0f;
             }
 
-            dbg.printf("Orientation: (roll=%.4f, pitch=%.4f)\r\n", roll*(180.0f/M_PI), pitch*(180.0f/M_PI));
-            dbg.printf("Yaw (bearing): %.4f\r\n", bearing);
-            dbg.printf("\r\n");
+            roll = roll*(180.0f/M_PI);
+            if (roll < 0) {
+                roll += 360.0f;
+            }
 
-            send_msg(ROLL, imu_counter, roll*(180.0f/M_PI));
-            send_msg(PITCH, imu_counter, pitch*(180.0f/M_PI));
-            send_msg(YAW, imu_counter, bearing);
+            pitch = pitch*(180.0f/M_PI);
+            if (pitch < 0) {
+                pitch += 360.0f;
+            }
+            mutex.unlock();
 
-            ++imu_counter;
-
-            // wait(DT);
+            // dbg.printf("Orientation: (roll=%.4f, pitch=%.4f)\r\n", roll*(180.0f/M_PI), pitch*(180.0f/M_PI));
+            // dbg.printf("Yaw (bearing): %.4f\r\n", bearing);
+            // dbg.printf("\r\n");
         }
+    }
+}
+
+void send_msg() {
+    while(true) {
+        mutex.lock();
+        uint8_t msg[28] = {0};
+        memcpy(&msg[0], ((uint8_t *) &roll), sizeof(float));
+        memcpy(&msg[4], ((uint8_t *) &pitch), sizeof(float));
+        memcpy(&msg[8], ((uint8_t *) &bearing), sizeof(float));
+        memcpy(&msg[12], ((uint8_t *) &LatDeg), sizeof(float));
+        memcpy(&msg[16], ((uint8_t *) &LatMin), sizeof(float));
+        memcpy(&msg[20], ((uint8_t *) &LonDeg), sizeof(float));
+        memcpy(&msg[24], ((uint8_t *) &LonMin), sizeof(float));
+        write_frame(serial, msg, 28);
+        mutex.unlock();
+        wait(0.1);
     }
 }
 
 
 int main() {
 
-    // put your setup code here, to run once:
     RTK rtk;
 
-    thread.start(get_IMU);
+    thread1.start(get_IMU);
+    thread2.start(send_msg);
 
     while(true) {
-        // put your main code here, to run repeatedly:
         while (gps_in.readable()) {
-            // dbg.printf("readable now\r\n");
             char c = gps_in.getc();
             rtk.read_char(c);
-            // wait(DT / 2);
         }
     }
 }
