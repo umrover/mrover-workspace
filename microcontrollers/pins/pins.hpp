a108c59d8b1e618e7ea0e998bc5bd3a1bfa1a249
rayfok <rayfok@umich.edu>
2018-05-04 13:39:36 -0400

    [hwi] Implemented science sensors code

--- microcontrollers/pins/pins.hpp
+++ microcontrollers/pins/pins.hpp
@@ -27,10 +27,16 @@ const PinName UART_TX1_PROD = PinName(PA_9);    // p68
 const PinName UART_RX1_PROD = PinName(PA_10);   // p69
 const PinName UART_TX2_PROD = PinName(PB_10);   // p47
 const PinName UART_RX2_PROD = PinName(PB_11);   // p48
+const PinName UART_TX3_PROD = PinName(PD_5);    // p86
+const PinName UART_RX3_PROD = PinName(PD_6);    // p87
+const PinName UART_TX4_PROD = PinName(PA_0);    // p23
+const PinName UART_RX4_PROD = PinName(PA_1);    // p24
+const PinName UART_TX5_PROD = PinName(PC_12);   // p80
+const PinName UART_RX5_PROD = PinName(PD_2);    // p83
 
-const PinName ANALOGIN1_PROD = PinName();       // p
-const PinName ANALOGIN2_PROD = PinName();       // p
-const PinName ANALOGIN3_PROD = PinName();       // p
+const PinName ANALOGIN1_PROD = PinName(PC_0);   // p15
+const PinName ANALOGIN2_PROD = PinName(PC_1);   // p16
+const PinName ANALOGIN3_PROD = PinName();       // Fill this in
 
 const PinName CAN_TX_PROD = PinName(PA_12);     // p71
 const PinName CAN_RX_PROD = PinName(PA_11);     // p70
--- microcontrollers/science/main.cpp
+++ microcontrollers/science/main.cpp
@@ -2,117 +2,135 @@
 #include "frame_serial.hpp"
 #include "pins.hpp"
 
-Thread thread1, thread2, thread3, thread4, thread5;
+Thread thread1, thread2;
+Mutex mutex;
 
 DigitalOut led(LED1_PROD);
-AnalogIn O2_probe(PA_0);
-AnalogIn CO2_probe(PA_1);
-AnalogIn moisture_probe(PA_2);
-Serial pH_probe(PB_6, PB_7);
-Serial temperature_probe(PB_6, PB_7);
-Serial conductivity_probe(PB_6, PB_7);
-Serial serial(UART_TX1_PROD, UART_RX1_PROD, 115200);
+AnalogIn O2_probe(ANALOGIN1_PROD);
+AnalogIn CO2_probe(ANALOGIN2_PROD);
+AnalogIn moisture_probe(ANALOGIN3_PROD);
+Serial pH_probe(UART_TX2_PROD, UART_RX2_PROD);
+Serial temperature_probe(UART_TX3_PROD, UART_RX3_PROD);
+Serial conductivity_probe(UART_TX1_PROD, UART_RX1_PROD);
+Serial serial(UART_TX5_PROD, UART_RX5_PROD, 115200);
 
-float O2 = 0;
-float CO2 = 0;
-float moisture = 0;
-float pH = 0;
-float temperature = 0;
-float conductivity = 0;
+char temp_buffer[256];
+int temp_write_index = 0;
+bool temp_first = true;
 
-void get_temperature() {
-    char buffer[30];
-    int i = 0;
-    while (true) {
-        char inchar = temperature_probe.getc();
-        if (inchar != '\r') {                    
-            buffer[i] = inchar;
-            i++;
-        } else {
-            temperature = atof(buffer);
-            i = 0;
-            wait(0.5);
-        }
+char cond_buffer[256];
+int cond_write_index = 0;
+bool cond_first = true;
+
+char pH_buffer[256];
+int pH_write_index = 0;
+bool pH_first = true;
+
+struct __attribute__((__packed__)) {
+    float O2;
+    float CO2;
+    float moisture;
+    float pH;
+    float temperature;
+    float conductivity;
+} g_message;
+
+void temperature_callback() {
+    char c = temperature_probe.getc();
+    if (c != '\r' && !temp_first) {
+        temp_buffer[temp_write_index++] = c;
+    } else {
+        mutex.lock();
+        g_message.temperature = atof(temp_buffer);
+        mutex.unlock();
+        temp_write_index = 0;
+        temp_first = false;
     }
 }
 
-void get_pH() {
-    char buffer[30];
-    int i = 0;
-    while (true) {
-        char inchar = pH_probe.getc();
-        if (inchar != '\r') {                    
-            buffer[i] = inchar;
-            i++;
-        } else {
-            pH = atof(buffer);
-            i = 0;
-            wait(0.5);
-        }
+void pH_callback() {
+    char c = pH_probe.getc();
+    if (c != '\r' && !pH_first) {
+        pH_buffer[pH_write_index++] = c;
+    } else {
+        mutex.lock();
+        g_message.pH = atof(pH_buffer);
+        mutex.unlock();
+        pH_write_index = 0;
+        pH_first = false;
     }
 }
 
-void get_conductivity() {
-    char buffer[30];
-    int i = 0;
-    while (true) {
-        char inchar = conductivity_probe.getc();
-        if (inchar != '\r') {                    
-            buffer[i] = inchar;
-            i++;
-        } else {
-            conductivity = atof(buffer);
-            i = 0;
-            wait(0.5);
-        }
+void cond_callback() {
+    char c = conductivity_probe.getc();
+    if (c != '\r' && !cond_first) {
+        cond_buffer[cond_write_index++] = c;
+    } else {
+        mutex.lock();
+        g_message.conductivity = atof(cond_buffer);
+        mutex.unlock();
+        cond_write_index = 0;
+        cond_first = false;
     }
 }
 
 void get_gas() {
-    float O2_slope = 6.5625;
-    float O2_intercept = 0;
-    float CO2_slope = 2500;
+    float O2_slope = 44200.0f;
+    float O2_intercept = -6630.0f;
+    // float CO2_slope = 25000; // HIGH
+    float CO2_slope = 2500.0f;  // LOW
     float CO2_intercept = 0;
 
     while(true) {
         float O2_voltage = O2_probe.read() * 5;
-        O2 = O2_slope * O2_voltage + O2_intercept;
-        
         float CO2_voltage = CO2_probe.read() * 5;
-        CO2 = CO2_slope * CO2_voltage + CO2_intercept;
+
+        mutex.lock();
+        g_message.O2 = O2_voltage;
+        g_message.CO2 = CO2_voltage;
+        /*g_message.O2 = O2_slope * O2_voltage + O2_intercept;
+        g_message.CO2 = CO2_slope * CO2_voltage + CO2_intercept;*/
+        mutex.unlock();
         wait(0.5);
     }
 }
 
 void get_moisture() {
+    // float wet = 0.41;
+    // float dry = 0.83;
     while(true) {
         float raw = moisture_probe.read();
-        // TODO: figure out how to interpret moisture reading
+        mutex.lock();
+        g_message.moisture = raw;
+        mutex.unlock();
         wait(0.5);
     }
 }
 
-void send_msg() {
-    uint8_t msg[24] = {0};
-    memcpy(&msg[0], ((uint8_t *) &O2), sizeof(float));
-    memcpy(&msg[4], ((uint8_t *) &CO2), sizeof(float));
-    memcpy(&msg[8], ((uint8_t *) &moisture), sizeof(float));
-    memcpy(&msg[12], ((uint8_t *) &pH), sizeof(float));
-    memcpy(&msg[16], ((uint8_t *) &temperature), sizeof(float));
-    memcpy(&msg[20], ((uint8_t *) &conductivity), sizeof(float));
-    write_frame(serial, msg, 24);
-    led = !led;
-    wait(1);
-}
-
 int main() {
+    g_message.conductivity = -1023.f;
+    g_message.temperature = -1023.f;
+    g_message.pH = -1023.f;
+    g_message.moisture = -1023.f;
+    g_message.O2 = -1023.f;
+    g_message.CO2 = -1023.f;
+    for (int i = 0; i < 10; i++) {
+        led = !led;
+        wait(0.1);
+    }
+
     thread1.start(get_gas);
-    thread2.start(get_moisture);
-    thread3.start(get_pH);
-    thread4.start(get_temperature);
-    thread5.start(get_conductivity);
+    //thread2.start(get_moisture);
+
+    pH_probe.attach(&pH_callback);
+    temperature_probe.attach(&temperature_callback);
+    conductivity_probe.attach(&cond_callback);
 
     while(true) {
-        send_msg();
+        mutex.lock();
+        write_frame(serial, (uint8_t *) &g_message, sizeof(g_message));
+        mutex.unlock();
+        led = !led;
+        wait(1);
     }
 }
