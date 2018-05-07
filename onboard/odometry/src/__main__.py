16dbdec7bc54826767e14469d6a1f2e991012987
Nathan Moos <moosingin3space@gmail.com>
2018-05-07 16:32:40 -0400

    [hwi] added number of satellites in view reporting

--- onboard/odometry/src/__main__.py
+++ onboard/odometry/src/__main__.py
@@ -11,7 +11,7 @@ odom_port = os.environ.get('MROVER_ODOM_PORT', '/dev/ttyUSB0')
 
 class OdomFrame:
     def __init__(self, buf):
-        data = struct.unpack('<fffifif??', buf)
+        data = struct.unpack('<fffififi??', buf)
         self.roll = data[0]
         self.pitch = data[1]
         self.bearing = data[2]
@@ -19,8 +19,9 @@ class OdomFrame:
         self.lat_min = data[4]
         self.lon_deg = data[5]
         self.lon_min = data[6]
-        self.gps_read = data[7]
-        self.imu_read = data[8]
+        self.num_sats = data[7]
+        self.gps_read = data[8]
+        self.imu_read = data[9]
 
 
 def main():
@@ -46,6 +47,7 @@ def main():
                 msg.longitude_deg = int(frame.lon_deg)
                 msg.longitude_min = frame.lon_min
                 msg.bearing_deg = frame.bearing
+                msg.num_satellites = int(frame.num_sats)
                 lcm_.publish('/odom', msg.encode())
             except struct.error:
                 print("Incomplete data, only {} bytes".format(len(r.buffer)))
