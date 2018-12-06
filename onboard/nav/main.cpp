a2038294a46aba9ce67583f02811a3af7c00c405
elgarber <elgarber@umich.edu>
2018-12-05 19:14:13 -0500

    Search algorithm will now repeat, multiplying the path width by 1/2 each round, until a tennis ball has been found. Path order can be changed from the config file.
    [nav] going through searches seemlessly
    
    Merge branch 'searcher' of https://github.com/elgarber/mrover-workspace into search
    
    [nav] searchOrder added to config
    
    [nav] moved update searcher to StateMachine. Removed undefined behavior
    
    Search algorithm will now repeat, multiplying the path width by 1/2 each round, until a tennis ball has been found. The path order can be updated from the config file
    
    Merged to master

--- onboard/nav/main.cpp
+++ onboard/nav/main.cpp
@@ -10,100 +10,100 @@ using namespace std;
 class LcmHandlers
 {
 public:
-	// Constructs an LcmHandler with the given state machine to work
-	// with.
-	LcmHandlers( StateMachine* stateMachine )
-		: mStateMachine( stateMachine )
-	{}
+    // Constructs an LcmHandler with the given state machine to work
+    // with.
+    LcmHandlers( StateMachine* stateMachine )
+        : mStateMachine( stateMachine )
+    {}
 
-	// Sends the auton state lcm message to the state machine.
-	void autonState(
-		const lcm::ReceiveBuffer* recieveBuffer,
-		const string& channel,
-		const AutonState* autonState
-		)
-	{
-		mStateMachine->updateRoverStatus( *autonState );
-	}
+    // Sends the auton state lcm message to the state machine.
+    void autonState(
+        const lcm::ReceiveBuffer* recieveBuffer,
+        const string& channel,
+        const AutonState* autonState
+        )
+    {
+        mStateMachine->updateRoverStatus( *autonState );
+    }
 
-	// Sends the magnetometer/accelerometer/gyro data to the state machine.
-	void bearing(
-		const lcm::ReceiveBuffer* receiveBuffer,
-		const string& channel,
-		const Bearing* bearing
-		)
-	{
-		mStateMachine->updateRoverStatus( *bearing );
-	}
+    // Sends the magnetometer/accelerometer/gyro data to the state machine.
+    void bearing(
+        const lcm::ReceiveBuffer* receiveBuffer,
+        const string& channel,
+        const Bearing* bearing
+        )
+    {
+        mStateMachine->updateRoverStatus( *bearing );
+    }
 
-	// Sends the course lcm message to the state machine.
-	void course(
-		const lcm::ReceiveBuffer* recieveBuffer,
-		const string& channel,
-		const Course* course
-		)
-	{
-		mStateMachine->updateRoverStatus( *course );
-	}
+    // Sends the course lcm message to the state machine.
+    void course(
+        const lcm::ReceiveBuffer* recieveBuffer,
+        const string& channel,
+        const Course* course
+        )
+    {
+        mStateMachine->updateRoverStatus( *course );
+    }
 
-	// Sends the obstacle lcm message to the state machine.
-	void obstacle(
-		const lcm::ReceiveBuffer* receiveBuffer,
-		const string& channel,
-		const Obstacle* obstacle
-		)
-	{
-		mStateMachine->updateRoverStatus( *obstacle );
-	}
+    // Sends the obstacle lcm message to the state machine.
+    void obstacle(
+        const lcm::ReceiveBuffer* receiveBuffer,
+        const string& channel,
+        const Obstacle* obstacle
+        )
+    {
+        mStateMachine->updateRoverStatus( *obstacle );
+    }
 
-	// Sends the odometry lcm message to the state machine.
-	void odometry(
-		const lcm::ReceiveBuffer* recieveBuffer,
-		const string& channel,
-		const Odometry* odometry
-		)
-	{
-		mStateMachine->updateRoverStatus( *odometry );
-	}
+    // Sends the odometry lcm message to the state machine.
+    void odometry(
+        const lcm::ReceiveBuffer* recieveBuffer,
+        const string& channel,
+        const Odometry* odometry
+        )
+    {
+        mStateMachine->updateRoverStatus( *odometry );
+    }
 
-	// Sends the tennis ball lcm message to the state machine.
-	void tennisBall(
-		const lcm::ReceiveBuffer* receiveBuffer,
-		const string& channel,
-		const TennisBall* tennisBall
-		)
-	{
-		mStateMachine->updateRoverStatus( *tennisBall );
-	}
+    // Sends the tennis ball lcm message to the state machine.
+    void tennisBall(
+        const lcm::ReceiveBuffer* receiveBuffer,
+        const string& channel,
+        const TennisBall* tennisBall
+        )
+    {
+        mStateMachine->updateRoverStatus( *tennisBall );
+    }
 
 private:
-	// The state machine to send the lcm messages to.
-	StateMachine* mStateMachine;
+    // The state machine to send the lcm messages to.
+    StateMachine* mStateMachine;
 };
 
 // Runs the autonomous navigation of the rover.
 int main()
 {
-	lcm::LCM lcmObject;
-	if( !lcmObject.good() )
-	{
-		cerr << "Error: cannot create LCM\n";
-		return 1;
-	}
+    lcm::LCM lcmObject;
+    if( !lcmObject.good() )
+    {
+        cerr << "Error: cannot create LCM\n";
+        return 1;
+    }
 
-	StateMachine roverStateMachine( lcmObject );
-	LcmHandlers lcmHandlers( &roverStateMachine );
+    StateMachine roverStateMachine( lcmObject );
+    LcmHandlers lcmHandlers( &roverStateMachine );
 
-	lcmObject.subscribe( "/auton", &LcmHandlers::autonState, &lcmHandlers );
-	lcmObject.subscribe( "/bearing", &LcmHandlers::bearing, &lcmHandlers );
-	lcmObject.subscribe( "/course", &LcmHandlers::course, &lcmHandlers );
-	lcmObject.subscribe( "/obstacle", &LcmHandlers::obstacle, &lcmHandlers );
-	lcmObject.subscribe( "/odometry", &LcmHandlers::odometry, &lcmHandlers );
-	lcmObject.subscribe( "/tennis_ball", &LcmHandlers::tennisBall, &lcmHandlers );
+    lcmObject.subscribe( "/auton", &LcmHandlers::autonState, &lcmHandlers );
+    lcmObject.subscribe( "/bearing", &LcmHandlers::bearing, &lcmHandlers );
+    lcmObject.subscribe( "/course", &LcmHandlers::course, &lcmHandlers );
+    lcmObject.subscribe( "/obstacle", &LcmHandlers::obstacle, &lcmHandlers );
+    lcmObject.subscribe( "/odometry", &LcmHandlers::odometry, &lcmHandlers );
+    lcmObject.subscribe( "/tennis_ball", &LcmHandlers::tennisBall, &lcmHandlers );
 
-	while( lcmObject.handle() == 0 )
-	{
-		roverStateMachine.run();
-	}
-	return 0;
+    while( lcmObject.handle() == 0 )
+    {
+        roverStateMachine.run();
+    }
+    return 0;
 } // main()
--- onboard/nav/rover.cpp
+++ onboard/nav/rover.cpp
@@ -8,90 +8,90 @@
 // Constructs a rover status object and initializes the navigation
 // state to off.
 Rover::RoverStatus::RoverStatus()
-	: mCurrentState( NavState::Off )
+    : mCurrentState( NavState::Off )
 {
-	mAutonState.is_auton = false;
+    mAutonState.is_auton = false;
 } // RoverStatus()
 
 // Gets a reference to the rover's current navigation state.
 NavState& Rover::RoverStatus::currentState()
 {
-	return mCurrentState;
+    return mCurrentState;
 } // currentState()
 
 // Gets a reference to the rover's current auton state.
 AutonState& Rover::RoverStatus::autonState()
 {
-	return mAutonState;
+    return mAutonState;
 } // autonState()
 
 Bearing& Rover::RoverStatus::bearing()
 {
-	return mBearing;
+    return mBearing;
 }
 
 // Gets a reference to the rover's course.
 Course& Rover::RoverStatus::course()
 {
-	return mCourse;
+    return mCourse;
 } // course()
 
 // Gets a reference to the rover's path.
 queue<Waypoint>& Rover::RoverStatus::path()
 {
-	return mPath;
+    return mPath;
 } // path()
 
 // Gets a reference to the rover's current obstacle information.
 Obstacle& Rover::RoverStatus::obstacle()
 {
-	return mObstacle;
+    return mObstacle;
 } // obstacle()
 
 // Gets a reference to the rover's current odometry information.
 Odometry& Rover::RoverStatus::odometry()
 {
-	return mOdometry;
+    return mOdometry;
 } // odometry()
 
 // Gets a reference to the rover's current tennis ball information.
 TennisBall& Rover::RoverStatus::tennisBall()
 {
-	return mTennisBall;
+    return mTennisBall;
 } // tennisBall()
 
 // Assignment operator for the rover status object. Does a "deep" copy
 // where necessary.
 Rover::RoverStatus& Rover::RoverStatus::operator=( Rover::RoverStatus& newRoverStatus )
 {
-	mAutonState = newRoverStatus.autonState();
-	mCourse = newRoverStatus.course();
-	while( !mPath.empty() )
-	{
-		mPath.pop();
-	}
-	for( int courseIndex = 0; courseIndex < mCourse.num_waypoints; ++courseIndex )
-	{
-		mPath.push( mCourse.waypoints[ courseIndex ] );
-	}
-	mObstacle = newRoverStatus.obstacle();
-	mOdometry = newRoverStatus.odometry();
-	mTennisBall = newRoverStatus.tennisBall();
-	return *this;
+    mAutonState = newRoverStatus.autonState();
+    mCourse = newRoverStatus.course();
+    while( !mPath.empty() )
+    {
+        mPath.pop();
+    }
+    for( int courseIndex = 0; courseIndex < mCourse.num_waypoints; ++courseIndex )
+    {
+        mPath.push( mCourse.waypoints[ courseIndex ] );
+    }
+    mObstacle = newRoverStatus.obstacle();
+    mOdometry = newRoverStatus.odometry();
+    mTennisBall = newRoverStatus.tennisBall();
+    return *this;
 } // operator=
 
 // Constructs a rover object with the given configuration file and lcm
 // object with which to use for communications.
 Rover::Rover( const rapidjson::Document& config, lcm::LCM& lcmObject )
-	: mRoverConfig( config )
-	, mLcmObject( lcmObject )
-	, mDistancePid( config[ "distancePid" ][ "kP" ].GetDouble(),
-					config[ "distancePid" ][ "kI" ].GetDouble(),
-					config[ "distancePid" ][ "kD" ].GetDouble() )
-	, mBearingPid( config[ "bearingPid" ][ "kP" ].GetDouble(),
-				   config[ "bearingPid" ][ "kI" ].GetDouble(),
-				   config[ "bearingPid" ][ "kD" ].GetDouble() )
-	, mLongMeterInMinutes( -1 )
+    : mRoverConfig( config )
+    , mLcmObject( lcmObject )
+    , mDistancePid( config[ "distancePid" ][ "kP" ].GetDouble(),
+                    config[ "distancePid" ][ "kI" ].GetDouble(),
+                    config[ "distancePid" ][ "kD" ].GetDouble() )
+    , mBearingPid( config[ "bearingPid" ][ "kP" ].GetDouble(),
+                   config[ "bearingPid" ][ "kI" ].GetDouble(),
+                   config[ "bearingPid" ][ "kD" ].GetDouble() )
+    , mLongMeterInMinutes( -1 )
 {
 } // Rover()
 
@@ -102,10 +102,10 @@ Rover::Rover( const rapidjson::Document& config, lcm::LCM& lcmObject )
 // on-course or off-course.
 DriveStatus Rover::drive( const Odometry& destination )
 {
-	double distance = estimateNoneuclid( mRoverStatus.odometry(), destination );
-	double bearing = calcBearing( mRoverStatus.odometry(), destination );
+    double distance = estimateNoneuclid( mRoverStatus.odometry(), destination );
+    double bearing = calcBearing( mRoverStatus.odometry(), destination );
 
-	return drive( distance, bearing );
+    return drive( distance, bearing );
 } // drive()
 
 // Sends a joystick command to drive forward from the current odometry
@@ -114,25 +114,25 @@ DriveStatus Rover::drive( const Odometry& destination )
 // rover small amounts as "course corrections".
 // The return value indicates if the rover has arrived or if it is
 // on-course or off-course.
-
 DriveStatus Rover::drive( const double distance, const double bearing )
 {
-	if( distance < mRoverConfig[ "atGoalDistanceThresh" ].GetDouble() )
-	{
-		return DriveStatus::Arrived;
-	}
-	if( fabs( bearing - mRoverStatus.bearing().bearing ) < mRoverConfig[ "drivingBearingThresh" ].GetDouble() )
-	{
-		double distanceEffort = mDistancePid.update( -1 * distance, 0 );
-		double destinationBearing = mod( mRoverStatus.bearing().bearing + bearing, 360);
-		throughZero( destinationBearing, mRoverStatus.bearing().bearing );
-		double turningEffort = mBearingPid.update( mRoverStatus.bearing().bearing, destinationBearing );
-		publishJoystick( distanceEffort, turningEffort, false );
-		return DriveStatus::OnCourse;
-	}
-	
-	printf("offcourse\n");
-	return DriveStatus::OffCourse;
+    if( distance < mRoverConfig[ "atGoalDistanceThresh" ].GetDouble() )
+    {
+        return DriveStatus::Arrived;
+    }
+    
+    double destinationBearing = mod( bearing, 360 );
+    throughZero( destinationBearing, mRoverStatus.bearing().bearing ); // will go off course if inside if because through zero not calculated 
+    
+    if( fabs( destinationBearing - mRoverStatus.bearing().bearing ) < mRoverConfig[ "drivingBearingThresh" ].GetDouble() )
+    {
+        double distanceEffort = mDistancePid.update( -1 * distance, 0 );
+        double turningEffort = mBearingPid.update( mRoverStatus.bearing().bearing, destinationBearing );
+        publishJoystick( distanceEffort, turningEffort, false );
+        return DriveStatus::OnCourse;
+    }
+    printf("offcourse\n");
+    return DriveStatus::OffCourse;
 } // drive()
 
 
@@ -141,8 +141,8 @@ DriveStatus Rover::drive( const double distance, const double bearing )
 // otherwise.
 bool Rover::turn( Odometry& destination )
 {
-	double bearing = calcBearing( mRoverStatus.odometry(), destination );
-	return turn( bearing );
+    double bearing = calcBearing( mRoverStatus.odometry(), destination );
+    return turn( bearing );
 } // turn()
 
 // Sends a joystick command to turn the rover. The bearing is the
@@ -150,20 +150,20 @@ bool Rover::turn( Odometry& destination )
 // otherwise.
 bool Rover::turn( double bearing )
 {
-	if( fabs( bearing - mRoverStatus.bearing().bearing ) < mRoverConfig[ "turningBearingThresh" ].GetDouble() )
-	{
-		return true;
-	}
-	throughZero( bearing, mRoverStatus.bearing().bearing );
-	double turningEffort = mBearingPid.update( mRoverStatus.bearing().bearing, bearing );
-	publishJoystick( 0, turningEffort, false );
-	return false;
+    throughZero( bearing, mRoverStatus.bearing().bearing );
+    if( fabs( bearing - mRoverStatus.bearing().bearing ) < mRoverConfig[ "turningBearingThresh" ].GetDouble() )
+    {
+        return true;
+    }
+    double turningEffort = mBearingPid.update( mRoverStatus.bearing().bearing, bearing );
+    publishJoystick( 0, turningEffort, false );
+    return false;
 } // turn()
 
 // Sends a joystick command to stop the rover.
 void Rover::stop()
 {
-	publishJoystick( 0, 0, false );
+    publishJoystick( 0, 0, false );
 } // stop()
 
 // Checks if the rover should be updated based on what information in
@@ -173,148 +173,153 @@ void Rover::stop()
 // we got rid of NavStates TurnToBall and DriveToBall (oops) fix this soon :P
 bool Rover::updateRover( RoverStatus newRoverStatus )
 {
-	// Rover currently on.
-	if( mRoverStatus.autonState().is_auton )
-	{
-		// Rover turned off
-		if( !newRoverStatus.autonState().is_auton )
-		{
-			mRoverStatus.autonState() = newRoverStatus.autonState();
-			return true;
-		}
-		if( ( mRoverStatus.currentState() == NavState::TurnToBall ||
- 			  mRoverStatus.currentState() == NavState::DriveToBall ) &&
- 			!isEqual( mRoverStatus.tennisBall(), newRoverStatus.tennisBall() ) )
-		{
-			mRoverStatus.bearing() = newRoverStatus.bearing();
-			mRoverStatus.obstacle() = newRoverStatus.obstacle();
-			mRoverStatus.odometry() = newRoverStatus.odometry();
-			mRoverStatus.tennisBall() = newRoverStatus.tennisBall();
-			return true;
-		}
+    // Rover currently on.
+    if( mRoverStatus.autonState().is_auton )
+    {
+        // Rover turned off
+        if( !newRoverStatus.autonState().is_auton )
+        {
+            mRoverStatus.autonState() = newRoverStatus.autonState();
+            return true;
+        }
+        if( ( mRoverStatus.currentState() == NavState::TurnToBall ||
+              mRoverStatus.currentState() == NavState::DriveToBall ) &&
+            !isEqual( mRoverStatus.tennisBall(), newRoverStatus.tennisBall() ) )
+        {
+            mRoverStatus.bearing() = newRoverStatus.bearing();
+            mRoverStatus.obstacle() = newRoverStatus.obstacle();
+            mRoverStatus.odometry() = newRoverStatus.odometry();
+            mRoverStatus.tennisBall() = newRoverStatus.tennisBall();
+            return true;
+        }
 
-		if( ( mRoverStatus.currentState() == NavState::TurnAroundObs ||
-				   mRoverStatus.currentState() == NavState::SearchTurnAroundObs ) &&
-				 ( !isEqual( mRoverStatus.obstacle(), newRoverStatus.obstacle() )
-				 	|| !isEqual(mRoverStatus.bearing(), newRoverStatus.bearing() ) ) )
-		{
-			mRoverStatus.bearing() = newRoverStatus.bearing();
-			mRoverStatus.obstacle() = newRoverStatus.obstacle();
-			mRoverStatus.odometry() = newRoverStatus.odometry();
-			mRoverStatus.tennisBall() = newRoverStatus.tennisBall();
-			return true;
-		}
+        if( ( mRoverStatus.currentState() == NavState::TurnAroundObs ||
+                   mRoverStatus.currentState() == NavState::SearchTurnAroundObs ) &&
+                 ( !isEqual( mRoverStatus.obstacle(), newRoverStatus.obstacle() )
+                    || !isEqual(mRoverStatus.bearing(), newRoverStatus.bearing() ) ) )
+        {
+            mRoverStatus.bearing() = newRoverStatus.bearing();
+            mRoverStatus.obstacle() = newRoverStatus.obstacle();
+            mRoverStatus.odometry() = newRoverStatus.odometry();
+            mRoverStatus.tennisBall() = newRoverStatus.tennisBall();
+            return true;
+        }
 
-		if( !isEqual( mRoverStatus.bearing(), newRoverStatus.bearing() ) ||
-			!isEqual( mRoverStatus.obstacle(), newRoverStatus.obstacle() ) ||
-			!isEqual( mRoverStatus.odometry(), newRoverStatus.odometry() ) ||
-			!isEqual( mRoverStatus.tennisBall(), newRoverStatus.tennisBall() ) )
-		{
-			mRoverStatus.bearing() = newRoverStatus.bearing();
-			mRoverStatus.obstacle() = newRoverStatus.obstacle();
-			mRoverStatus.odometry() = newRoverStatus.odometry();
-			mRoverStatus.tennisBall() = newRoverStatus.tennisBall();
-			return true;
-		}
-		return false;
-	}
+        if( !isEqual( mRoverStatus.bearing(), newRoverStatus.bearing() ) ||
+            !isEqual( mRoverStatus.obstacle(), newRoverStatus.obstacle() ) ||
+            !isEqual( mRoverStatus.odometry(), newRoverStatus.odometry() ) ||
+            !isEqual( mRoverStatus.tennisBall(), newRoverStatus.tennisBall() ) )
+        {
+            mRoverStatus.bearing() = newRoverStatus.bearing();
+            mRoverStatus.obstacle() = newRoverStatus.obstacle();
+            mRoverStatus.odometry() = newRoverStatus.odometry();
+            mRoverStatus.tennisBall() = newRoverStatus.tennisBall();
+            return true;
+        }
+        return false;
+    }
 
-	// Rover currently off.
-	else
-	{
-		// Rover turned on.
-		if( newRoverStatus.autonState().is_auton )
-		{
-			mRoverStatus = newRoverStatus;
-			// Calculate longitude minutes/meter conversion.
-			mLongMeterInMinutes = 60 / ( EARTH_CIRCUM * cos( degreeToRadian(
-				mRoverStatus.odometry().latitude_deg, mRoverStatus.odometry().latitude_min ) ) / 360 );
-			return true;
-		}
-		return false;
-	}
+    // Rover currently off.
+    else
+    {
+        // Rover turned on.
+        if( newRoverStatus.autonState().is_auton )
+        {
+            mRoverStatus = newRoverStatus;
+            // Calculate longitude minutes/meter conversion.
+            mLongMeterInMinutes = 60 / ( EARTH_CIRCUM * cos( degreeToRadian(
+                mRoverStatus.odometry().latitude_deg, mRoverStatus.odometry().latitude_min ) ) / 360 );
+            return true;
+        }
+        return false;
+    }
 } // updateRover()
 
 // Calculates the conversion from minutes to meters based on the
 // rover's current latitude.
 const double Rover::longMeterInMinutes() const
 {
-	return mLongMeterInMinutes;
+    return mLongMeterInMinutes;
 }
 
 // Gets the rover's status object.
 Rover::RoverStatus& Rover::roverStatus()
 {
-	return mRoverStatus;
+    return mRoverStatus;
 } // roverStatus()
 
 // Gets the rover's driving pid object.
 PidLoop& Rover::distancePid()
 {
-	return mDistancePid;
+    return mDistancePid;
 } // distancePid()
 
 // Gets the rover's turning pid object.
 PidLoop& Rover::bearingPid()
 {
-	return mBearingPid;
+    return mBearingPid;
 } // bearingPid()
 
 // Publishes a joystick command with the given forwardBack and
 // leftRight efforts.
 void Rover::publishJoystick( const double forwardBack, const double leftRight, const bool kill )
 {
-	Joystick joystick;
-	joystick.dampen = -1.0; // power limit (0 = 50%, 1 = 0%, -1 = 100% power)
-	joystick.forward_back = -forwardBack;
-	joystick.left_right = leftRight;
-	joystick.kill = kill;
-	string joystickChannel = mRoverConfig[ "joystickChannel" ].GetString();
-	mLcmObject.publish( joystickChannel, &joystick );
+    Joystick joystick;
+    joystick.dampen = -1.0; // power limit (0 = 50%, 1 = 0%, -1 = 100% power)
+    joystick.forward_back = -forwardBack;
+    joystick.left_right = leftRight;
+    joystick.kill = kill;
+    string joystickChannel = mRoverConfig[ "joystickChannel" ].GetString();
+    mLcmObject.publish( joystickChannel, &joystick );
 } // publishJoystick()
 
 // Returns true if the two bearing messages are equal, false
 // otherwise.
 bool Rover::isEqual( const Bearing& bearing1, const Bearing& bearing2 ) const
 {
-	return (bearing1.bearing == bearing2.bearing );
+    return (bearing1.bearing == bearing2.bearing );
 } // isEqual( bearing )
 
 // Returns true if the two obstacle messages are equal, false
 // otherwise.
 bool Rover::isEqual( const Obstacle& obstacle1, const Obstacle& obstacle2 ) const
 {
-	if( obstacle1.detected == obstacle2.detected &&
-		obstacle1.bearing == obstacle2.bearing )
-	{
-		return true;
-	}
-	return false;
+    if( obstacle1.detected == obstacle2.detected &&
+        obstacle1.bearing == obstacle2.bearing )
+    {
+        return true;
+    }
+    return false;
 } // isEqual( Obstacle )
 
 // Returns true if the two odometry messages are equal, false
 // otherwise.
 bool Rover::isEqual( const Odometry& odometry1, const Odometry& odometry2 ) const
 {
-	if( odometry1.latitude_deg == odometry2.latitude_deg &&
-		odometry1.latitude_min == odometry2.latitude_min &&
-		odometry1.longitude_deg == odometry2.longitude_deg &&
-		odometry1.longitude_min == odometry2.longitude_min )
-	{
-		return true;
-	}
-	return false;
+    if( odometry1.latitude_deg == odometry2.latitude_deg &&
+        odometry1.latitude_min == odometry2.latitude_min &&
+        odometry1.longitude_deg == odometry2.longitude_deg &&
+        odometry1.longitude_min == odometry2.longitude_min )
+    {
+        return true;
+    }
+    return false;
 } // isEqual( Odometry )
 
 // Returns true if the two tennis ball messages are equal, false
 // otherwise.
 bool Rover::isEqual( const TennisBall& tennisBall1, const TennisBall& tennisBall2 ) const
 {
-	if( tennisBall1.found == tennisBall2.found &&
-		tennisBall1.bearing == tennisBall2.bearing &&
-		tennisBall1.distance == tennisBall2.distance )
-	{
-		return true;
-	}
-	return false;
+    if( tennisBall1.found == tennisBall2.found &&
+        tennisBall1.bearing == tennisBall2.bearing &&
+        tennisBall1.distance == tennisBall2.distance )
+    {
+        return true;
+    }
+    return false;
 } // isEqual( TennisBall )
+
+/*************************************************************************/
+/* TODOS */
+/*************************************************************************/
+
