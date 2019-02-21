fe6a6aad9f25d61804b1f78500b0b34279af67bb
Jason Whitfield <jtwhit@umich.edu>
2019-02-21 18:34:04 -0500

    [odom] Reworked sensor package code to work with new phone.

--- onboard/sensor_package/src/__main__.py
+++ onboard/sensor_package/src/__main__.py
@@ -4,12 +4,12 @@ import time
 import sys
 import traceback
 from rover_common import aiolcm
-from rover_msgs import Odometry, Bearing
+from rover_msgs import IMU, GPS
 
 from .android_usb_comm import Android
 
 
-GS3_DEVICE_IDS = (0x18d1, 0x4ee7, 0x2d01)
+PHONE_DEVICE_IDS = (0x1004, 0x18d1, 0x633e, 0x2d01)
 
 lcm_ = aiolcm.AsyncLCM()
 
@@ -27,7 +27,7 @@ class AndroidFrame:
 
 def get_android():
     return Android(
-        *GS3_DEVICE_IDS,
+        *PHONE_DEVICE_IDS,
         manufacturer='MRover',
         model='Onboard',
         description='OnboardComputer',
@@ -41,25 +41,25 @@ def main():
         try:
             time.sleep(1)
             with get_android() as android:
-                print('connected to Samsung GS3')
+                print('connected to Sensor Package')
 
                 while True:
                     raw_data = android.read()
                     if raw_data:
                         frame = AndroidFrame(raw_data)
 
-                        odom_msg = Odometry()
-                        odom_msg.latitude_deg = int(frame.lat_deg)
-                        odom_msg.latitude_min = frame.lat_min
-                        odom_msg.longitude_deg = int(frame.lon_deg)
-                        odom_msg.longitude_min = frame.lon_min
-                        odom_msg.bearing_deg = frame.azimuth_deg
-                        odom_msg.speed = 0
-                        lcm_.publish('/odometry', odom_msg.encode())
+                        imu_msg = IMU()
+                        imu_msg.bearing = frame.azimuth_deg
+                        lcm_.publish('/imu', imu_msg.encode())
 
-                        bearing_msg = Bearing()
-                        bearing_msg.bearing = frame.azimuth_deg
-                        lcm_.publish('/bearing', bearing_msg.encode())
+                        gps_msg = GPS()
+                        gps_msg.latitude_deg = int(frame.lat_deg)
+                        gps_msg.latitude_min = frame.lat_min
+                        gps_msg.longitude_deg = int(frame.lon_deg)
+                        gps_msg.longitude_min = frame.lon_min
+                        gps_msg.bearing_deg = frame.azimuth_deg
+                        gps_msg.speed = 0
+                        lcm_.publish('/gps', gps_msg.encode())
                     else:
                         print('read timed out, retrying the connection')
                         break
