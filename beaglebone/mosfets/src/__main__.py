4745d9ffc5b2e4cc87aa573259ab62d579603c79
Justin Beemer <jubeemer@umich.edu>
2019-04-28 13:58:40 -0400

    [hwi] LED and solenoid control

--- beaglebone/mosfets/src/__main__.py
+++ beaglebone/mosfets/src/__main__.py
@@ -4,16 +4,32 @@ import lcm
 
 lcm_ = lcm.LCM()
 
-mosfets = ["P8_14", "P8_16", "P8_18"]
+LED_BACKLIGHTS = "P8_11"
+LED_UV = "P8_12"
+SOLENOID_1 = "P8_14"
+SOLENOID_2 = "P8_16"
+
+mosfets = [LED_BACKLIGHTS, LED_UV, SOLENOID_1, SOLENOID_2]
 
 
 def mosfet_callback(channel, msg):
     cmd = Mosfet.decode(msg)
 
     if cmd.enable:
-        GPIO.output(mosfets[cmd.id], GPIO.HIGH)
+        out = GPIO.HIGH
     else:
-        GPIO.output(mosfets[cmd.id], GPIO.LOW)
+        out = GPIO.LOW
+
+    if cmd.id == "backlights":
+        GPIO.output(mosfets[0], out)
+    elif cmd.id == "uv_leds":
+        GPIO.output(mosfets[1], out)
+    elif cmd.id == "solenoid_1":
+        GPIO.output(mosfets[2], out)
+    elif cmd.id == "solenoid_2":
+        GPIO.output(mosfets[3], out)
+    else:
+        print("Invalid mosfet ID.")
 
 
 def main():
