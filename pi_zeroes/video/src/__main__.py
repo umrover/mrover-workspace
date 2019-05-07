aaf986cdd816755b4689963d2aadbfa38f2458a9
spenfarr <spenfarr@umich.edu>
2019-05-07 16:03:14 -0400

    [pis] automatic quality reduction

--- pi_zeroes/video/src/__main__.py
+++ pi_zeroes/video/src/__main__.py
@@ -1,9 +1,11 @@
 import sys
 import os
 import time
+import asyncio
 from configparser import ConfigParser
 from subprocess import Popen, PIPE
 from rover_common import aiolcm
+from rover_common import heartbeatlib
 from rover_common.aiohelper import run_coroutines
 from rover_msgs import PiCamera, PiSettings, PiPicture
 import gi
@@ -19,19 +21,25 @@ vid_process = None
 pipeline = None
 index = -1
 secondary = False
+last_ping = int(round(time.time() * 1000))
+disconnected = False
+reduced_quality = False
+reconnection = None
 
 
 def start_pipeline():
-    global pipeline, vid_process, settings, secondary
+    global pipeline, vid_process, settings, secondary, reduced_quality
 
     height = str(settings.height)
     width = str(settings.width)
 
-    bitrate = int((settings.height * settings.width) / 0.4608)
+    bitrate = int((480 * 854) / 0.4608)
+    if reduced_quality:
+        bitrate //= 3
     port = '5001' if secondary else '5000'
 
     vid_args = ["raspivid", "-t", "0", "-h", height,
-                "-w", width, "-b", str(bitrate),
+                "-w", width, "-b", str(int(bitrate)),
                 "-ss", str(settings.shutter_speed), "-o", "-"]
     if settings.vflip:
         vid_args.append("-vf")
@@ -75,8 +83,8 @@ def read_settings():
     settings = PiSettings()
     settings.shutter_speed = int(config["cam_settings"]["shutter_speed"])
     settings.vflip = config["cam_settings"].getboolean("vflip")
-    settings.height = int(config["cam_settings"]["height"])
-    settings.width = int(config["cam_settings"]["width"])
+    settings.height = 480
+    settings.width = 854
 
 
 def write_settings():
@@ -139,15 +147,53 @@ def picture_callback(channel, msg):
               .format(index, round(time.time() * 1000)))
 
 
+def connection_state_changed(c, _):
+    global disconnected, last_ping, reconnection
+    if c:
+        print("Connection established.")
+        disconnected = False
+        reconnection = int(round(time.time() * 1000))
+    else:
+        last_ping = int(round(time.time() * 1000))
+        disconnected = True
+        reconnection = float("inf")
+        print("Disconnected.")
+
+
+async def connection_monitor():
+    global disconnected, last_ping, pipeline, settings, reduced_quality
+    while True:
+        if pipeline is not None:
+            if disconnected and not reduced_quality:
+                if int(round(time.time() * 1000)) - last_ping > 3000:
+                    settings.height = 144
+                    settings.width = 256
+                    write_settings()
+                    reduced_quality = True
+                    stop_pipeline()
+                    start_pipeline()
+            if reduced_quality:
+                if int(round(time.time() * 1000)) - reconnection > 5000:
+                    settings.height = 480
+                    settings.width = 854
+                    write_settings()
+                    reduced_quality = False
+                    stop_pipeline()
+                    start_pipeline()
+        await asyncio.sleep(0.1)
+
+
 def main():
     global index
     index = int(sys.argv[1])
+    hb = heartbeatlib.OnboardHeartbeater(connection_state_changed, index)
     Gst.init(None)
 
     read_settings()
+    write_settings()
 
     lcm_.subscribe("/pi_camera", camera_callback)
     lcm_.subscribe("/pi_settings", settings_callback)
     lcm_.subscribe("/pi_picture", picture_callback)
 
-    run_coroutines(lcm_.loop())
+    run_coroutines(lcm_.loop(), hb.loop(), connection_monitor())
