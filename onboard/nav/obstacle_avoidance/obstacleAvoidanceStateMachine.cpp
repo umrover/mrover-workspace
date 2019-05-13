483c5fb7c8415a35ce85b121c6bfe06d806b313f
amverni <amverni@umich.edu>
2019-05-12 19:31:51 -0500

    Fix bugs in obstacle avoidance edge cases

--- onboard/nav/obstacle_avoidance/obstacleAvoidanceStateMachine.cpp
+++ onboard/nav/obstacle_avoidance/obstacleAvoidanceStateMachine.cpp
@@ -13,28 +13,28 @@ ObstacleAvoidanceStateMachine::ObstacleAvoidanceStateMachine( StateMachine* stat
 
 // Allows outside objects to set the original obstacle angle
 // This will allow the variable to be set before the rover turns
-void ObstacleAvoidanceStateMachine::updateObstacleAngle( double bearing ) 
+void ObstacleAvoidanceStateMachine::updateObstacleAngle( double bearing )
 {
     mOriginalObstacleAngle = bearing;
 }
 
 // Allows outside objects to set the original obstacle distance
 // This will allow the variable to be set before the rover turns
-void ObstacleAvoidanceStateMachine::updateObstacleDistance( double distance ) 
+void ObstacleAvoidanceStateMachine::updateObstacleDistance( double distance )
 {
     mOriginalObstacleDistance = distance;
 }
 
 // Allows outside objects to set the original obstacle angle
 // This will allow the variable to be set before the rover turns
-void ObstacleAvoidanceStateMachine::updateObstacleElements( double bearing, double distance )  
+void ObstacleAvoidanceStateMachine::updateObstacleElements( double bearing, double distance )
 {
     updateObstacleAngle( bearing );
     updateObstacleDistance( distance );
 }
 
-// Runs the avoidance state machine through one iteration. This will be called by StateMachine 
-// when NavState is in an obstacle avoidance state. This will call the corresponding function based 
+// Runs the avoidance state machine through one iteration. This will be called by StateMachine
+// when NavState is in an obstacle avoidance state. This will call the corresponding function based
 // on the current state and return the next NavState
 NavState ObstacleAvoidanceStateMachine::run( Rover* phoebe, const rapidjson::Document& roverConfig )
 {
@@ -61,17 +61,17 @@ NavState ObstacleAvoidanceStateMachine::run( Rover* phoebe, const rapidjson::Doc
 }
 
 // Checks that both rover is in search state and that tenis ball is detected
-bool ObstacleAvoidanceStateMachine::isTennisBallDetected ( Rover* phoebe ) 
+bool ObstacleAvoidanceStateMachine::isTennisBallDetected ( Rover* phoebe )
 {
     return ( phoebe->roverStatus().currentState() == NavState::SearchTurnAroundObs &&
              phoebe->roverStatus().tennisBall().found );
 }
 
 // Checks to see if tennis ball is reachable before hitting obstacle
-// Tennis ball must be closer than obstacle and must be in same direction ( in other words, 
+// Tennis ball must be closer than obstacle and must be in same direction ( in other words,
 // the rover should be turning the same way to go around the obstacle as to get to the ball )
-bool ObstacleAvoidanceStateMachine::isTennisBallReachable( Rover* phoebe, double distance, 
-                                                         double bearing ) 
+bool ObstacleAvoidanceStateMachine::isTennisBallReachable( Rover* phoebe, double distance,
+                                                         double bearing )
 {
     return ( ( phoebe->roverStatus().obstacle().distance > distance - 1 ) ||
            ( bearing < 0 && phoebe->roverStatus().obstacle().bearing < 0 ) ||
