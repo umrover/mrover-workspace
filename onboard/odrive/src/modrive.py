223481a9b00c176a3a4f4748f54ccc3c4abfa20c
jjtom <jjtom@umich.edu>
2019-10-30 20:53:03 -0400

    Class Wrapper and Syntax errors

---
+++ onboard/odrive/src/modrive.py
@@ -0,0 +1,113 @@
+import odrive as odv
+from odrive.enums import *
+
+
+class Modrive:
+  left_axis;
+  right_axis;
+  odrive;
+  
+  def __init__(self, odr):
+    self.odrive = odr;
+   	self.left_axis = self.odrive.axis0
+    self.right_axis = self.odrive.axis1
+    
+    #viable to set initial state to idle? 
+      
+      
+  def __getattr__(self, attr):
+      if attr in self.__dict__:
+          return getattr(self, attr)
+      return getattr(self.odrive, attr)
+    
+    
+  def set_current_lim(self, axis, lim):
+    if (axis == "LEFT"):
+      self.left_axis.motor.config.current_lim = lim
+    elif (axis == "RIGHT"):
+      self.right_axis.motor.config.current_lim = lim
+    else:
+      self.left_axis.motor.config.current_lim = lim
+      self.right_axis.motor.config.current_lim = lim
+    
+	#odrive.axis0.controller.config.control_mode = CTRL_MODE_VELOCITY_CONTROL
+
+	def set_control_mode(self,axis,mode):
+    if (axis == "LEFT"):
+      self.left_axis.controller.config.control_mode = mode
+    elif (axis == "RIGHT"):
+      self.right_axis.controller.config.control_mode = mode
+    elif (axis == "BOTH"):
+      self.left_axis.controller.config.control_mode = mode
+      self.right_axis.controller.config.control_mode = mode
+
+	#odrive.axis0.motor.current_control.Iq_measured
+	def get_iq_measured(axis):
+    if (axis == "LEFT"):
+      return self.left_axis.motor.current_control.Iq_measured
+    elif(axis == "RIGHT":
+      return self.right_axis.motor.current_control.Iq_measured
+    else:
+    	print("ERROR: cant get the measured iq for both motors at once")
+      return 0
+
+	#odrive.axis0.encoder.vel_estimate
+  def get_vel_estimate(axis):
+    if (axis == "LEFT"):
+      return self.left_axis.encoder.vel_estimate
+    elif(axis == "RIGHT":
+      return self.right_axis.encoder.vel_estimate
+    else:
+    	print("ERROR: cant get the velocity estimate for both motors at once")
+      return 0
+
+	#odrive.axis0.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
+	#odrive.axis0.requested_state = AXIS_STATE_IDLE
+	#odrive.axis0.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
+         
+	def requested_state(self, axis, state):
+		if (axis == "LEFT"):
+         self.left_axis.requested_state = state
+    elif (axis == "RIGHT"):
+         self.right_axis.requested_state = state
+    else:
+        self.right_axis.requested_state = state
+        self.left_axis.requested_state = state
+         
+ 	# odrive.axis0.encoder.vel_estimate == 0
+  
+	def est_vel(self,axis):
+  	if (axis == "LEFT"):
+      return self.left_axis.encoder.vel_estimate
+    elif (axis == "RIGHT"):
+      return self.right_axis.encoder.vel_estimate
+  
+  
+	def set_vel(axis, vel):
+  	if (axis == "LEFT"):
+    	self.left_axis.controller.vel_setpoint = vel
+  	elif axis == "RIGHT":
+    	self.right_axis.controller.vel_setpoint = vel
+  	elif axis == "BOTH":
+    	self.left_axis.controller.vel_setpoint = vel
+    	self.right_axis.controller.vel_setpoint = vel
+  	else:
+    	print("ERROR, unknown axis")
+         
+         
+  def get_current_state(axis):
+    if (axis == "LEFT"):
+         return self.left_axis.current_state
+  	elif(axis == "RIGHT"):
+         return self.right_axis.current_state
+    else:
+         print("cant get current state of both axes at once")
+         return 0
+         
+         
+         
+         
+         
+         
+         
+         
--- onboard/odrive/src/odriver.py
+++ onboard/odrive/src/odriver.py
@@ -3,6 +3,9 @@ import sys
 import time as t
 import odrive as odv
 from rover_msgs import ODriver_Req_State, ODriver_Req_Vel, ODriver_Pub_State, ODriver_Pub_Encoders
+from odrive.enums import *
+
+import Modrive
 
 def main():
 	lcm_ = lcm.LCM()
@@ -10,6 +13,8 @@ def main():
 	#insert lcm subscribe statements here
 	
 	#These have been mapped
+  
+  
 	lcm_.subscribe("/odriver_req_state", odriver_req_state_callback)
 	
 	lcm_.subscribe("/odrive_req_vel", odriver_req_vel_callback)
@@ -23,7 +28,7 @@ def main():
 	exit()
 
 #Program states possible - BOOT, DISARMED, ARMED, ERROR, CALIBRATING, EXIT
-#							1		2		3		4		5         6
+#							               1		  2	      	3		  4		      5         6
 currentState = "BOOT" #starting state
 requestedState = "DISARMED" #starting requested state
 odrive = None #starting odrive
@@ -34,31 +39,42 @@ def nextState():
 	global requestedState
 	global odrive
 	global encoderTime
+  
+  global states = ["BOOT", "DISARMED", "ARMED", "ERROR", "CALIBRATING", "EXIT"]
+  
 	msg = odriver_pub_encoders()
 	msg1 = odriver_pub_state()
 
 	if (currentState == "BOOT"):
 		#attempt to connect to odrive
 		odrive = odv.find_any(serial_number=sys.argv[1])
+    modrive = Modrive(odrive) #arguments = odr
+    
 		#Block until odrive is connected
 		#set current limit on odrive to 50
-		odrive.axis0.motor.config.current_lim = 50
+		modrive.set_current_lim(legalAxis, 50)
 		#set controller's control mode to velocity control
-		odrive.axis0.controller.config.control_mode = CTRL_MODE_VELOCITY_CONTROL
+		modrive.set_control_mode(legalAxis, CTRL_MODE_VELOCITY_CONTROL)
 		#set currentState to DISARMED
 		currentState = "DISARMED"
 		msg1.state = 2
 		msg1.serialid = sys.argv[1]
 		lcm_.publish("/odriver_pub_state", msg.encode())
 
-	else if (currentState == "DISARMED"):
+	elif (currentState == "DISARMED"):
 		#if 100 ms have passed since last time data was published
 		#	publish an odrive_data lcm message with measured current and estimated velocity
 		
 		#Unsure if using correct timestamp
 		if (encoderTime - t.time() > 0.1):
-			msg.measuredCurrent = odrive.axis0.motor.current_control.Iq_measured
+      #change ODrive_Pub_Encoders.LCM
+      #msg.measuredCurrentLeft = modrive.get_iq_measured(LEFT)
+      #msg.measuredCurrentRight = modrive.get_iq_measured(RIGHT)
+
+      #Same for vel, velRight and velLeft
+      msg.measuredCurrent = odrive.axis0.motor.current_control.Iq_measured
 			msg.estVel = odrive.axis0.encoder.vel_estimate
+      
 			msg.serialid = sys.argv[1]
 			lcm_.publish("/odriver_pub_encoders", msg)
 			encoderTime = t.time()
@@ -84,22 +100,22 @@ def nextState():
 			msg1.serialid = sys.argv[1]
 			lcm_.publish("/odriver_pub_state", msg1.encode())
 			encoderTime = t.time()
-		else if requestedState == "ARMED":
-			odrive.axis0.controller.config.control_mode = CTRL_MODE_VELOCITY_CONTROL
+		elif requestedState == "ARMED":
+      modrive.set_control_mode(legalAxis, CTRL_MODE_VELOCITY_CONTROL)
 			currentState = "ARMED"
 			msg1.state = 3
 			msg1.serialid = sys.argv[1]
 			lcm_.publish("/odriver_pub_state", msg1.encode())
 			encoderTime = t.time()
-		else if requestedState == "BOOT":
+		elif requestedState == "BOOT":
 			odrive.reboot()
 			currentState = "BOOT"
 			msg1.state = 1
 			msg1.serialid = sys.argv[1]
 			lcm_.publish("/odriver_pub_state", msg1.encode())
 			encoderTime = t.time()
-		else if requestedState == "CALIBRATING":
-			odrive.axis0.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
+		elif requestedState == "CALIBRATING":
+      modrive.requested_state(legalAxis, AXIS_STATE_FULL_CALIBRATION_SEQUENCE)
 			currentState == "CALIBRATING"
 			msg1.state = 5
 			msg1.serialid = sys.argv[1]
@@ -108,7 +124,7 @@ def nextState():
 
 
 
-	else if (currentState == "ARMED"):
+	elif (currentState == "ARMED"):
 		#check odrive's dump errors functionality
 		#if there are errors
 		#	set currentState to ERROR
@@ -124,7 +140,7 @@ def nextState():
 
 		if (encoderTime - t.time() > 0.1):
 			msg.measuredCurrent = odrive.axis0.motor.current_control.Iq_measured
-			msg.estVel = odrive.axis0.encoder.vel_estimate
+			msg.estVel = modrive.axis0.encoder.vel_estimate
 			msg.serialid = sys.argv[1]
 			lcm_.publish("/odriver_pub_encoders", msg)
 			encoderTime = t.time()
@@ -135,49 +151,49 @@ def nextState():
 			msg1.serialid = sys.argv[1]
 			lcm_.publish("/odriver_pub_state", msg1.encode())
 			encoderTime = t.time()
-		else if requestedState == "DISARMED":
-			odrive.axis0.requested_state = AXIS_STATE_IDLE
+		elif requestedState == "DISARMED":
+			modrive.set_control_mode(legalAxis,AXIS_STATE_IDLE)
 			currentState = "DISARMED"
 			msg1.state = 2
 			msg1.serialid = sys.argv[1]
 			lcm_.publish("/odriver_pub_state", msg1.encode())
 			encoderTime = t.time()
-		else if requestedState == "BOOT":
+		elif requestedState == "BOOT":
 			odrive.reboot()
 			currentState = "BOOT"
 			msg1.state = 1
 			msg1.serialid = sys.argv[1]
 			lcm_.publish("/odriver_pub_state", msg1.encode())
 			encoderTime = t.time()
-		else if requestedState == "CALIBRATING":
-			odrive.axis0.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
-			currentState == "CALIBRATING"
+		elif requestedState == "CALIBRATING":
+			modrive.requested_state(legalAxis, AXIS_STATE_FULL_CALIBRATION_SEQUENCE)
+			currentState = "CALIBRATING"
 			msg1.state = 5
 			msg1.serialid = sys.argv[1]
 			lcm_.publish("/odriver_pub_state", msg1.encode())
 			encoderTime = t.time()
-		else if (currentState == "ERROR"):
+		elif (currentState == "ERROR"):
 		#if requestedState is BOOT
 		#	reboot the odrive
 		#	set currentState to BOOT
 		#else if requested state is CALIBRATING
 		#	have the odrive recalibrate
 		#	set currentState to CALIBRATING
-		if requestedState == "BOOT":
-			odrive.reboot()
-			currentState = "BOOT"
-			msg1.state = 1
-			msg1.serialid = sys.argv[1]
-			lcm_.publish("/odriver_pub_state", msg1.encode())
-			encoderTime = t.time()
-		else if requestedState == "CALIBRATING":
-			odrive.axis0.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
-			currentState == "CALIBRATING"
-			msg1.state = 5
-			msg1.serialid = sys.argv[1]
-			lcm_.publish("/odriver_pub_state", msg1.encode())
-			encoderTime = t.time()
-	else if (currentState == "CALIBRATING"):
+      if requestedState == "BOOT":
+        odrive.reboot()
+        currentState = "BOOT"
+        msg1.state = 1
+        msg1.serialid = sys.argv[1]
+        lcm_.publish("/odriver_pub_state", msg1.encode())
+        encoderTime = t.time()
+      elif requestedState == "CALIBRATING":
+        modrive.requested_state(legalAxis, AXIS_STATE_FULL_CALIBRATION_SEQUENCE)
+        currentState = "CALIBRATING"
+        msg1.state = 5
+        msg1.serialid = sys.argv[1]
+        lcm_.publish("/odriver_pub_state", msg1.encode())
+        encoderTime = t.time()
+	elif (currentState == "CALIBRATING"):
 		#if odrive is done calibrating
 		#	set current limit on odrive to 50
 		#	set controller's control mode to velocity control
@@ -186,8 +202,8 @@ def nextState():
 		#We don't know how to check if done calibrating
 		#if odrive.
 
-		odrive.axis0.motor.config.current_lim = 50
-		odrive.axis0.controller.config.control_mode = CTRL_MODE_VELOCITY_CONTROL
+		modrive.set_current_lim(legalAxis, 50)
+		modrive.set_control_mode(legalAxis, CTRL_MODE_VELOCITY_CONTROL)
 		currentState = "DISARMED"
 		msg1.state = 2
 		msg1.serialid = sys.argv[1]
@@ -199,37 +215,28 @@ def nextState():
 
 def odriver_req_state_callback(channel,msg):
 	message = ODriver_Req_State.decode(msg)
-	if message.serialid = sys.argv[1] :
-		if message.requestState = 2:
-			requestedState = "ARMED"
-		else if message.requestState = 3:
-			requestedState = "DISARMED"
-		else if message.requestState = 5:
-			requestedState = "CALIBRATING"
-		else if message.requestState = 1:
-			requestedState = "BOOT"
-		else if message.requestState = 6:
-			requestedState = "EXIT"
-
-			if odrive.axis0.encoder.vel_estimate = 0:
-				if axis0.current_state = AXIS_STATE_IDLE
+	if message.serialid == sys.argv[1]:
+    requestedState = states[message.requestState - 1]
+    if requestedState == "EXIT":
+			if modrive.get_vel_estimate("LEFT") == 0 and modrive.get_current_state("LEFT") == AXIS_STATE_IDLE and 
+      										modrive.get_vel_estimate("RIGHT") == 0 and modrive.get_current_state("RIGHT") == AXIS_STATE_IDLE:
 					sys.exit()
-				else:
-					axis0.requested_state= AXIS_STATE_IDLE
+			else:
+          modrive.set_vel(legalAxis, 0)		
+          modrive.requested_state(legalAxis, AXIS_STATE_IDLE)
 					sys.exit()
 
-
+    
 	
 def odriver_req_vel_callback(channel, msg):
 	#if the program is in an ARMED state
 	#	set the odrive's velocity to the float specified in the message
 	#no state change
 	message = ODriver_Req_Vel.decode(msg)
-	if message.serialid = sys.argv[1] :
+	if message.serialid == sys.argv[1] :
 		if(currentState == "ARMED"):
-			odrive.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
-			odrive.axis0.controller.vel_setpoint = message.vel
-			
+      modrive.requested_state(legalAxis, AXIS_STATE_CLOSED_LOOP_CONTROL)
+      modrive.set_vel(legalAxis, message.vel)			
 
 if __name__ == "__main__":
 	main()
