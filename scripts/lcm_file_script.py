2fa0041f025bbdc5153c6619c6441acfabc5fbd3
acfang <acfang@umich.edu>
2019-11-12 19:36:31 -0500

    [teleop] Added comms radio setup script and script to convert all .lcm files into a single .json file.
    Added radio ip argument, new script for comms
    
    Fixed file destination for radio setup script
    
    Removed new_script.tcl

---
+++ scripts/lcm_file_script.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+import os
+import json
+
+lcm_dict = {}
+lcm_dict['message_types'] = []
+
+json_file = open('../base_station/gui/src/rover_msgs.json', 'w+')
+
+for filename in os.listdir('../rover_msgs'):
+    if filename.endswith('.lcm'):
+        with open(os.path.join('../rover_msgs/' + filename), 'r') as f:
+            f.readline()
+            f.readline()
+            message_type_name = f.readline()[7:-3]
+
+            lcm_dict['message_types'].append(message_type_name)
+            message_type_dict = []
+            line = f.readline()
+
+            while line.find('}') == -1:
+                line = line.replace('  ', '')
+                line = line.replace(';', '')
+                line = line.replace("\n", '')
+                line = line.replace("\r", '')
+                line = line.replace("\t", '')
+                line = line.split("//", 1)[0]
+                
+                #find out later how to process const variables with set values
+                #ex. (in NavStatus.lcm)
+                #const int8_t OFF = 0;
+
+                if (line.find('=') == -1):
+                    message_type_dict.append(line.split(' '))
+
+                line = f.readline()
+
+            lcm_dict[message_type_name] = message_type_dict
+            
+json.dump(lcm_dict, json_file)
