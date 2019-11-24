c3f31ea73ee93d26148cc2668b86ff5fdd77b2e7
Michael Jeffrey Wolf <mjwolf@umich.edu>
2019-11-24 14:42:21 -0500

    Implemented graphial LCM Send form that populates with fields for each message type
    Populating message type dropdown based on rover_msgs.json
    
    Populating window with types, labels, and textboxes corresponding to selected message type
    
    Now converting LCMSend inputs to corresponding types
    
    Finished implmenting graphical LCM Send page

--- base_station/gui/src/components/LCMSend.vue
+++ base_station/gui/src/components/LCMSend.vue
@@ -2,7 +2,7 @@
   <div class="wrapper">
     <div class="box header">
       <img src="/static/mrover.png" alt="MRover" title="MRover" width="48" height="48" />
-      <h1>LCM Echo</h1>
+      <h1>LCM Send</h1>
       <div class="spacer"></div>
       <div class="comms">
         <ul id="vitals">
@@ -12,12 +12,40 @@
       </div>
       <div class="spacer"></div>
     </div>
-    <div class="box"> 
-      <input type="text" :id="'Channel'" class ="lcm_send_textbox_input" v-model="channel_name">
-      <label :for="'Channel'">Channel</label>
-      <textarea class="lcm_send_textbox_input message_textarea" :id="'Message'" v-model="message_text"></textarea>
-      <label :for="'Message'">Message</label>
-      <br>
+    <div class="box">
+
+      <label style="float: left" :for="'channel'">Channel:  </label>
+      <input id='channel' type="text" v-model='channel_name' />
+
+      <br/>
+      <br/>
+
+      <label style="float: left" :for="'message_type'">Message Type</label>
+      <!-- <input type="text" :id="'Channel'" class ="lcm_send_input" v-model="channel_name">-->
+      <select class="lcm_send_input" id="message_type" v-model="selectedOption" @change="switch_message_type()" required> 
+        <option value="" selected>select a message type</option>
+        <option v-for="option in options" v-bind:value="option.text">
+          {{ option.text }}
+        </option>
+      </select>
+
+
+
+      <br/>
+      <br/>
+      
+      <div class="lcm_send_input"> 
+        <div class="param" v-for="datum in commandParams"> 
+          <label class="param_type" > {{ datum.type }} </label>
+          <label class="param_label"> {{ datum.label }} </label>
+          <input type="text" v-model="datum.value"/>
+        </div>
+      </div>     
+
+      <!--<textarea class="lcm_send_input message_textarea" :id="'Message'" v-model="message_text"></textarea> -->
+
+      <br/>
+
       <button type="button" v-on:click="send_message()">Send</button>
     </div>
 
@@ -29,9 +57,13 @@
 <script>
   import LCMBridge from 'lcm_bridge_client/dist/bridge.js';
   import CommIndicator from './CommIndicator.vue'
+  import msgs from '../static/rover_msgs.json'
 
   export default {
     name: 'LCMSend',
+    mounted() {
+      this.populate();
+    },
     data () {
       return {
         lcm_: null,
@@ -40,14 +72,81 @@
           lcm: false
         },
         channel_name: "",
-        message_text: ""
+        message_text: "",
+        options: [],
+        selectedOption: '',
+        commandParams: []
       }
     },
 
     methods: {
       send_message: function () {
-      console.log(this.channel_name);
-      this.lcm_.publish(this.channel_name, JSON.parse(this.message_text))  
+
+        var msg = {type: this.selectedOption};
+
+        for(var i = 0; i < this.commandParams.length; ++i) {
+          var param = this.commandParams[i];
+
+          //Convert to boolean, 
+          if(param.type == "boolean") {
+            if(param.value.toLowerCase() == "true") {
+              msg[param.label] = true;
+            }
+
+            else if(param.value.toLowerCase() == "false") {
+              msg[param.label] = false;
+            }
+
+            else {
+              console.error("Input for boolean parameter '" + param.label + "' is not 'true' or 'false'");
+              return;
+            }
+          }
+
+          //convert to number
+          else if(param.type == "double" || param.type == "float" || param.type.startsWith("int")) {
+            msg[param.label] = Number(param.value);
+          }
+
+          //leave as string
+          else if(param.type == "string") {
+            msg[param.label] = param.value;
+          }
+          
+          //otherwise, convert to JS object
+          else {
+            msg[param.label] = JSON.parse(param.value);
+          }
+        }
+
+        
+        console.log("Sending message:", msg);
+
+        this.lcm_.publish(this.channel_name, msg);
+
+        //Example:
+        //this.lcm_.publish('/auton', {type: 'AutonState', is_auton: this.auton_enabled})
+      },
+
+      switch_message_type: function() {
+
+        //Clear commandParams array
+        this.commandParams.length = 0;
+
+        //Populate commandParams arary
+        for(var i = 0; i < msgs[this.selectedOption].length; ++i)
+        {
+          this.commandParams.push({ type: msgs[this.selectedOption][i][0], label: msgs[this.selectedOption][i][1] });
+        }
+      },
+
+      populate() {
+        
+        for(var m in msgs)
+         {
+           this.options.push({text: m, value: m });
+         }
+        
       }
     },
 
@@ -128,12 +227,25 @@
     overflow: auto;
   }
 
-  .lcm_send_textbox_input {
+  .lcm_send_input {
     width: 100%;
   }
 
   .message_textarea {
     height: 500px;
+  }
+
+  .param {
+    margin-bottom: 5px
+  }
+
+  .param_type {
+    color: grey;
+    margin-right: 2px;
+  }
+
+  .param_label {
+    margin-right: 2px;
   }
 
   p {
