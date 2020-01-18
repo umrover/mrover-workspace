75f436dbddc44ead08872f47d8c5462dcba54850
Murali Mohan <y2kmurali@gmail.com>
2020-01-18 12:31:59 -0500

    [cv] Integrate AR Tag detection into CV's main
    Now detects AR Tags and pushes them to LCM

---
+++ onboard/cv/artag_detector.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <vector>
+#include "perception.hpp"
+
+using namespace std;
+using namespace cv;
+
+struct Tag {
+    Point2f loc;
+    int id;
+};
+
+class TagDetector {
+   private:
+    Ptr<cv::aruco::Dictionary> alvarDict;
+    Ptr<cv::aruco::DetectorParameters> alvarParams;
+    std::vector<int> ids;
+    std::vector<std::vector<cv::Point2f> > corners;
+    cv::Mat rgb;
+
+   public:
+    TagDetector();                                                        //constructor loads dictionary data from file
+    Point2f getAverageTagCoordinateFromCorners(const vector<Point2f> &corners);  //takes detected AR tag and finds center coordinate for use with ZED
+    pair<Tag, Tag> findARTags(Mat &src, Mat &depth_src);                  //detects AR tags in a given Mat
+};
