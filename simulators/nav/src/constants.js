3515ddddc690b64b9e9de1df0714420f7a7d7db2
Andrew Vernier <amverni@umich.edu>
2020-01-27 19:57:04 -0500

    [nav-sim] Add radio signal strength to simulator

--- simulators/nav/src/constants.js
+++ simulators/nav/src/constants.js
@@ -2,3 +2,6 @@ export const WAYPOINT_MODE = 0;
 export const ROCK_MODE = 1;
 export const AR_TAG_MODE = 2;
 export const GATE_MODE = 3;
+export const RADIO_REPEATER_THRESHOLD = 30;
+export const MIN_RADIO_SIGNAL_STRENGTH = 0;
+export const MAX_RADIO_SIGNAL_STRENGTH = 100;
