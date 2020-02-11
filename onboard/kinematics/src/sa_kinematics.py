aab03bd9c34e52d26536c635695b47009e050ee2
skorman <skorman@umich.edu>
2020-02-11 00:06:06 -0500

    added sa config json and started making sa return to base

---
+++ onboard/kinematics/src/sa_kinematics.py
@@ -0,0 +1,25 @@
+import numpy as np
+from scipy.interpolate import CubicSpline
+
+
+class SAKinematics:
+    def __init__(self, lcm):
+        self.lcm_ = lcm
+
+    def plan_return_to_origin(self, cur_pos):
+        translator = cur_pos[0]
+        joint_a = cur_pos[1]
+        joint_b = cur_pos[2]
+        path = [[translator, joint_a, joint_b]]
+        path.append([0, joint_a, joint_b])
+        path.append([0, 0, 0])
+
+        cs = self.spline_fitting(path)
+        print(path)
+        print(cs)
+
+        return cs
+
+    def spline_fitting(self, path):
+        x_ = np.linspace(0, 1, len(path))
+        return CubicSpline(x_, path)
