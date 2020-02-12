272a02e0f0f21afe3a4ea5c46acf299ed9660fb2
skorman <skorman@umich.edu>
2020-02-11 20:22:39 -0500

    writing sa communication over lcm

--- onboard/kinematics/src/sa_kinematics.py
+++ onboard/kinematics/src/sa_kinematics.py
@@ -1,5 +1,8 @@
 import numpy as np
 from scipy.interpolate import CubicSpline
+from rover_msgs import SAClosedLoopCmd
+import asyncio
+from .logger import logger
 
 from numpy import linalg as LA
 from.utils import calculate_torque
@@ -8,6 +11,9 @@ class SAKinematics:
     def __init__(self, lcm, arm):
         self.lcm_ = lcm
         self.arm_ = arm
+        self.spline_t = 0
+        self.current_spline = []
+        self.enable_execute = False
 
     def FK(self, cur_robot):
         joints = cur_robot.all_joints
@@ -97,8 +103,41 @@ class SAKinematics:
         print(path)
         print(cs)
 
+        self.current_spline = cs
+
         return cs
 
     def spline_fitting(self, path):
         x_ = np.linspace(0, 1, len(path))
         return CubicSpline(x_, path)
+
+    def execute_callback(self, channel, msg):
+        print("is this being called?")
+        self.enable_execute = True
+
+    def publish_config(self, angles, torques, channel):
+        sa_clsd_loop = SAClosedLoopCmd()
+        sa_clsd_loop.angle = angles
+        sa_clsd_loop.torque = torques
+
+        self.lcm_.publish(channel, sa_clsd_loop.encode())
+
+    async def execute_spline(self):
+        logger.info("Executing path on SA Arm")
+        self.spline_t = 0
+        while True:
+            if self.enable_execute:
+
+                logger.info('spline time: {}'.format(self.spline_t))
+
+                target_angs = self.current_spline(self.spline_t)
+                torques = [0, 0, 0]
+
+                self.publish_config(target_angs, torques, '/sa_closedloop_cmd')
+                print(target_angs)
+                self.spline_t += 0.01
+
+                if self.spline_t >= 1:
+                    self.enable_execute = False
+            await asyncio.sleep(0.001)
+        return
