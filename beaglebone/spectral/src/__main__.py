facf8aef410f28c032c48c84274ffc32053a646e
Carmen Giger <cgiger00@gmail.com>
2020-02-17 04:05:34 +0000

    spectral commits i forgot to push the other day...lcm publishing

--- beaglebone/spectral/src/__main__.py
+++ beaglebone/spectral/src/__main__.py
@@ -1,36 +1,38 @@
-from math import ceil
 import smbus
-import time as t
-import numpy as np
-import spectral
+from . import spectral
 
 bus = smbus.SMBus(2)
 
 I2C_MUX_ADDRESS = 0x70
 MUX_READ_WRITE_REG = 0xCC  # filler address
 DEVICE_SLAVE_ADDRESS = 0x49
+ADDRESSES = {'triad': 0x1, 'single_1': 0x2, 'single_2': 0x4, 'single_3': 0x8}
+
 
 def read(channel):
     bus.read_byte_data(I2C_MUX_ADDRESS, MUX_READ_WRITE_REG, channel)
 
-def write(channel):
-    bus.write_byte_data(I2C_MUX_ADDRESS, MUX_READ_WRITE_REG, channel)    
 
-ADDRESSES = { 't': 0x1, '0': 0x2, '1': 0x4, '2': 0x8 }
+def write(channel):
+    bus.write_byte_data(I2C_MUX_ADDRESS, MUX_READ_WRITE_REG, channel)
+
 
 def enable():
-    for i in addresses:
+    for i in ADDRESSES:
         write(i[0])
         spectral.enable_spectral()
 
-def main():
-    while 1:
-        mode = input("select mode: t (triad), 0 (normal 0), 1 (normal 1), 2 (normal 2)")
-        write(ADDRESSES[mode])
-        spectral.get_spectral_data(mode)
 
+def main():
+    enable()
+    spectral.lcm_.subscribe("/spectral_cmd", spectral.spectral_cmd_callback)
+    while 1:
+        spectral.lcm_.handle()
+        if (spectral.SENSOR != 'none'):
+            write(ADDRESSES[spectral.SENSOR])
+            spectral.publish_spectral_data()
+        spectral.SENSOR = 'none'
 
 
 if __name__ == "__main__":
     main()
-
--- beaglebone/spectral/src/spectral.py
+++ beaglebone/spectral/src/spectral.py
@@ -1,17 +1,17 @@
 import smbus
 import numpy as np
 import time as t
-# from rover_msgs import SpectralData
-# will add another soon 
-# import lcm
+from rover_msgs import SpectralData
+from rover_msgs import SpectralCmd
+import lcm
 
 # 0x00: Select master data -- AS72651
 # 0x01: Select first slave data -- AS72652
 # 0x02: Select second slave data -- AS72653
-DEV_SEL  = 0x4F
+DEV_SEL = 0x4F
 
 bus = smbus.SMBus(2)
-# lcm_ = lcm.LCM()
+lcm_ = lcm.LCM()
 
 I2C_AS72XX_SLAVE_STATUS_REG = 0x00
 I2C_AS72XX_SLAVE_WRITE_REG = 0x01
@@ -23,7 +23,7 @@ DEVICE_SLAVE_ADDRESS_READ = 0x93
 DEVICE_SLAVE_ADDRESS_WRITE = 0x92
 DEVICE_SLAVE_ADDRESS = 0x49
 
-# registers for triad 
+# registers for triad
 RAW_VALUE_RGA_HIGH = 0x08
 RAW_VALUE_RGA_LOW = 0x09
 
@@ -42,100 +42,86 @@ RAW_VALUE_VKE_LOW = 0x11
 RAW_VALUE_WLF_HIGH = 0x12
 RAW_VALUE_WLF_LOW = 0x13
 
-# non-triad registers
-R_HIGH = 0x08  # Channel R High Data Byte
-R_LOW = 0x09  # Channel R Low Data Byte
-S_HIGH = 0x0A  # Channel S High Data Byte
-S_LOW = 0x0B  # Channel S Low Data Byte
-T_HIGH = 0x0C  # Channel T High Data Byte
-T_LOW = 0x0D  # Channel T Low Data Byte
-U_HIGH = 0x0E  # Channel U High Data Byte
-U_LOW = 0x0F  # Channel U Low Data Byte
-V_HIGH = 0x10  # Channel V High Data Byte
-V_LOW = 0x11  # Channel V Low Data Byte
-W_HIGH = 0x12  # Channel W High Data Byte
-W_LOW = 0x13  # Channel W Low Data Byte
+global SENSOR
+SENSOR = 'none'  # triad = 0, single = 1 - 3
 
-#combines MSB and LSB data
-def get_decimal(virtual_reg_L, virtual_reg_H):  
+
+# combines MSB and LSB data
+def get_decimal(virtual_reg_L, virtual_reg_H):
     high = virtual_read(virtual_reg_H) << 8
-    return np.int16((high | virtual_read(virtual_reg_L))) 
+    return np.int16((high | virtual_read(virtual_reg_L)))
+
 
 def virtual_read(virtual_reg):
-    status = None 
+    status = None
     d = None
 
-    status = bus.read_byte_data(DEVICE_SLAVE_ADDRESS, 
-                I2C_AS72XX_SLAVE_STATUS_REG)
+    status = bus.read_byte_data(DEVICE_SLAVE_ADDRESS,
+                                I2C_AS72XX_SLAVE_STATUS_REG)
 
     if ((status & I2C_AS72XX_SLAVE_RX_VALID) != 0):
         d = bus.read_byte_data(DEVICE_SLAVE_ADDRESS, I2C_AS72XX_SLAVE_READ_REG)
 
     while(1):
-        status = bus.read_byte_data(DEVICE_SLAVE_ADDRESS, 
-                I2C_AS72XX_SLAVE_STATUS_REG)
+        status = bus.read_byte_data(DEVICE_SLAVE_ADDRESS, I2C_AS72XX_SLAVE_STATUS_REG)
 
         if ((status & I2C_AS72XX_SLAVE_TX_VALID) == 0):
-            #no inbound TX pending at slave 
+            # no inbound TX pending at slave
             break
         t.sleep(0.005)
-    
+
     bus.write_byte_data(DEVICE_SLAVE_ADDRESS, I2C_AS72XX_SLAVE_WRITE_REG, virtual_reg)
 
     while(1):
-        status = bus.read_byte_data(DEVICE_SLAVE_ADDRESS, 
-                I2C_AS72XX_SLAVE_STATUS_REG)
-        
+        status = bus.read_byte_data(DEVICE_SLAVE_ADDRESS, I2C_AS72XX_SLAVE_STATUS_REG)
+
         if((status & I2C_AS72XX_SLAVE_RX_VALID) != 0):
-            #read data is ready
+            # read data is ready
             break
         t.sleep(0.005)
-    
-    d = bus.read_byte_data(DEVICE_SLAVE_ADDRESS, 
-                    I2C_AS72XX_SLAVE_READ_REG)
+
+    d = bus.read_byte_data(DEVICE_SLAVE_ADDRESS, I2C_AS72XX_SLAVE_READ_REG)
     return d
 
 
 def virtual_write(virtual_reg, data):
-    print ("data sent:", data)
+    print("data sent:", data)
     status = None
     while(1):
-        status = bus.read_byte_data(DEVICE_SLAVE_ADDRESS, 
-                    I2C_AS72XX_SLAVE_STATUS_REG)
+        status = bus.read_byte_data(DEVICE_SLAVE_ADDRESS, I2C_AS72XX_SLAVE_STATUS_REG)
         if((status & I2C_AS72XX_SLAVE_TX_VALID) == 0):
             break
         t.sleep(0.005)
 
-    bus.write_byte_data(DEVICE_SLAVE_ADDRESS, 
-                    I2C_AS72XX_SLAVE_WRITE_REG, (virtual_reg | 1<<7))
+    bus.write_byte_data(DEVICE_SLAVE_ADDRESS,
+                        I2C_AS72XX_SLAVE_WRITE_REG, (virtual_reg | 1 << 7))
     while(1):
-        status = bus.read_byte_data(DEVICE_SLAVE_ADDRESS, 
-                    I2C_AS72XX_SLAVE_STATUS_REG)
+        status = bus.read_byte_data(DEVICE_SLAVE_ADDRESS,
+                                    I2C_AS72XX_SLAVE_STATUS_REG)
         if ((status & I2C_AS72XX_SLAVE_TX_VALID) == 0):
             break
-        t.sleep(0.005) #amount of ms to wait between checking virtual register changes
+        t.sleep(0.005)  # amount of ms to wait between checking virtual register changes
 
-    bus.write_byte_data(DEVICE_SLAVE_ADDRESS, 
-                I2C_AS72XX_SLAVE_WRITE_REG, data)
-    
+    bus.write_byte_data(DEVICE_SLAVE_ADDRESS,
+                        I2C_AS72XX_SLAVE_WRITE_REG, data)
 
-def get_triad_data():
-    #colors = SpectralData.decode(msg)
-    # dev_channels = {
-    #                0x00: {"R": colors.r, "S": colors.s, "T": colors.t, "U": colors.u, "V": colors.v, "W": colors.w },
-    #                0x01: {"G": colors.g, "H": colors.h, "I": colors.i, "J": colors.j, "K": colors.k, "L": colors.l },
-    #                0x02: {"A": colors.a, "B": colors.b, "C": colors.c, "D": colors.d, "E": colors.e, "F": colors.f }
-    #                }
 
+def get_data():
+    colors = SpectralData()
     dev_channels = {
-                   0x00: {"R": 0, "S": 0, "T": 0, "U": 0, "V": 0, "W": 0 },
-                   0x01: {"G": 0, "H": 0, "I": 0, "J": 0, "K": 0, "L": 0 },
-                   0x02: {"A": 0, "B": 0, "C": 0, "D": 0, "E": 0, "F": 0 }
+                   0x00: {"R": colors.r, "S": colors.s, "T": colors.t, "U": colors.u, "V": colors.v, "W": colors.w},
+                   0x01: {"G": colors.g, "H": colors.h, "I": colors.i, "J": colors.j, "K": colors.k, "L": colors.l},
+                   0x02: {"A": colors.a, "B": colors.b, "C": colors.c, "D": colors.d, "E": colors.e, "F": colors.f}
                    }
 
-    channel_registers = [[RAW_VALUE_RGA_LOW, RAW_VALUE_RGA_HIGH], [RAW_VALUE_SHB_LOW, RAW_VALUE_SHB_HIGH],
-                     [RAW_VALUE_TIC_LOW, RAW_VALUE_TIC_HIGH], [RAW_VALUE_UJD_LOW, RAW_VALUE_UJD_HIGH],
-                     [RAW_VALUE_VKE_LOW, RAW_VALUE_VKE_HIGH], [RAW_VALUE_WLF_LOW, RAW_VALUE_WLF_HIGH]]
+    channel_registers = [
+                        [RAW_VALUE_RGA_LOW, RAW_VALUE_RGA_HIGH],
+                        [RAW_VALUE_SHB_LOW, RAW_VALUE_SHB_HIGH],
+                        [RAW_VALUE_TIC_LOW, RAW_VALUE_TIC_HIGH],
+                        [RAW_VALUE_UJD_LOW, RAW_VALUE_UJD_HIGH],
+                        [RAW_VALUE_VKE_LOW, RAW_VALUE_VKE_HIGH],
+                        [RAW_VALUE_WLF_LOW, RAW_VALUE_WLF_HIGH]
+                        ]
 
     for i in dev_channels:
         virtual_write(DEV_SEL, i)
@@ -144,68 +130,61 @@ def get_triad_data():
         for j in dev_channels[i]:
             dev_channels[i][j] = get_decimal(channel_registers[channel][0], channel_registers[channel][1])
             channel += 1
-    return dev_channels
 
-def get_data():
-    # colors = SpectralData.decode(msg)
-    # dev_channels = {"R": colors.r, "S": colors.s, "T": colors.t, "U": colors.u, "V": colors.v, "W": colors.w }
-    dev_channels = {"R": 0, "S": 0, "T": 0, "U": 0, "V": 0, "W": 0 }
-
-    channel_registers = [ 
-                    [ R_LOW, R_HIGH ], [ S_LOW, S_HIGH ],
-                    [ T_LOW, T_HIGH ], [ U_LOW, U_HIGH ],
-                    [ V_LOW, V_HIGH ], [ W_LOW, W_HIGH ] 
-                    ]
-    channel = 0
-    for i in dev_channels:
-        dev_channels[i] = get_decimal(channel_registers[channel][0], channel_registers[channel][1])
-        channel += 1
-    return dev_channels
+        global SENSOR
+        if (SENSOR != 'triad'):
+            break
+    return colors
 
 
-def spectral_callback(msg):
+def spectral_cmd_callback(channel, msg):
+    sensors = ['triad', 'single_0', 'single_1', 'single_2']
+    cmd = SpectralCmd.decode(msg)
+
+    global SENSOR
+    SENSOR = sensors[cmd.sensor]
+
+
+def publish_spectral_data():
     if ((virtual_read(0x04) & 0x02) == 2):
-        get_data(msg)
+        lcm_.publish('/spectral_data_publish', get_data().encode())
+
 
 def enableLED():
     virtual_write(0x07, virtual_read(0x07) | ~0xF7)
 
+
 def disableLED():
     virtual_write(0x07, virtual_read(0x07) & 0xF7)
 
-def enable_spectral():
-    virtual_write(0x04, 0x28) #runs twice to account for status miss
-    virtual_write(0x04, 0x28) #converts data bank to 2
-    virtual_write(0x05, 0xFF) #increases integration time
 
-def get_spectral_data(mode):
-    if ((virtual_read(0x04) & 0x02) == 2):
-        print("DATA READY TO READ")
-        if (mode == 't'):
-            data = get_triad_data()
-            for i in range(0, 3):
-                [print(key, value) for key, value in data[i].items()] 
-        else:
-            data = get_data()
-            [print(key, value) for key, value in data.items()]    
-    
+def enable_spectral():
+    virtual_write(0x04, 0x28)  # runs twice to account for status miss
+    virtual_write(0x04, 0x28)  # converts data bank to 2
+    virtual_write(0x05, 0xFF)  # increases integration time
+
+
+# def get_spectral_data():
+#     if ((virtual_read(0x04) & 0x02) == 2):
+#         print("DATA READY TO READ")
+#         for i in range(0, 3):
+#             [print(key, value) for key, value in data[i].items()]
+
 
 # def main():
 #    mode = input("select mode: s (spectral) or n (normal)")
 
 #    while True:
-#        # lcm_.handle() 
+#        # lcm_.handle()
 #        if ((virtual_read(0x04) & 0x02) == 2):
 #            print("DATA READY TO READ")
 #            if (mode == 't'):
 #                data = get_spectral_data()
 #                for i in range(0, 3):
-#                    [print(key, value) for key, value in data[i].items()] 
+#                    [print(key, value) for key, value in data[i].items()]
 #            else:
 #                data = get_data()
 #                [print(key, value) for key, value in data.items()]
 
 # if __name__ == "__main__":
 #    main()
-
-        
