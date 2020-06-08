f72c96c1b704994ba92749a71378eeb9111ec6d2
Andrew Vernier <amverni@umich.edu>
2020-06-07 20:38:25 -0400

    [gui] Add links on Menu page
    Add links for the remaining GUI pages on the Menu page

--- base_station/gui/src/components/Menu.vue
+++ base_station/gui/src/components/Menu.vue
@@ -3,11 +3,15 @@
     <a href="#/RATask">Robotic Arm Setup</a>
     <a href="#/SATask">Soil Acquisition Setup</a>
     <a href="#/AutonTask">Autonomy Setup</a>
+    <a href="#/LcmEcho">LCM Echo</a>
+    <a href="#/LcmSend">LCM Send</a>
+    <a href="#/PidTune">PID Tuning</a>
+    <a href="#/EscTest">ESC Testing</a>
   </div>
 </template>
 
 <style scoped>
-  
+
 .wrapper {
   font-size: 3em;
 }
