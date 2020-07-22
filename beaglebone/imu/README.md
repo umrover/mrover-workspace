7b41edd9296ef10ae5d235146a68e98865380c81
jjtom <jjtom@umich.edu>
2020-07-21 19:38:32 -0500

    Initial commit for IMU Code

---
+++ beaglebone/imu/README.md
@@ -0,0 +1,55 @@
+﻿Code for measuring pitch, roll and yaw through the ICM 20948(IMU)
+ ---
+### About
+
+The IMU is intended to read out magnetometer, accelerometer and magnetometer readings and translate them into pitch, yaw and roll for use in Auton's systems. First, the calibration script is run while the system is stable, followed by the main driver which utilizes the calibrated values in order to produce accurate data.
+
+### LCM Channels Publishing/Subscribed To
+
+Currently, no official LCM channel/struct has been created but the device should publish the current roll, pitch and yaw calculated from the accelerometer, gyroscope and magnetometer readings. The raw values given may also be passed along. 
+
+### Usage
+#### Wiring
+When wiring, only the set of pins related to i2c should be used. The top left corner should indicate the correct side and the pins below should be used.
+[https://cdn.sparkfun.com/r/600-600/assets/learn_tutorials/8/9/3/15335-GPIO_I2C.jpg](https://cdn.sparkfun.com/r/600-600/assets/learn_tutorials/8/9/3/15335-GPIO_I2C.jpg)
+VIN,GND and DA, CL should be wired to your power, ground, SDA, and SCL respectively but AD0 and NC should not be connected to anything.
+#### Running
+Before running, the calibration script should be run in order to obtain proper calibration values before normal operation. This should be done while stable and level.
+
+Implementation into Jarvis hasn't been tested but should be similar to other sensors using
+`$ /jarvis build`
+`$ /jarvis exec`
+in the proper directory.
+
+Current usage is to run the calibration script followed by the driver script
+  
+
+#### LCM Commands
+
+LCM is currently unimplemented.
+  
+
+### Off Nominal Behavior Handling
+
+Repeated attempts at startup should warrant checks to wiring
+
+### To Do
+
+This section should have a list of the remaining items needed to be finished for the code. The todo (and later on completed todo) items should be listed using this markup syntax:
+
+-   [x] Proper readings for the Accelerometer and Gyroscope
+    
+-   [ ] Proper readings for the magnetometer (Testing Needed)
+    
+-   [ ] Calibration for all three sensors needs more testing
+
+-   [ ] LCM implementation
+
+### Notes
+
+Due to the timing of when this project was being worked on much of the code is still rough and shallow in depth. The magnetometer was tested very lightly before quarantine but its unknown if the values given were correct. Come September, more of this should be polished, tested and integrated into our systems.
+
+--
+
+  
+
---
+++ beaglebone/imu/src/quaternion.py
@@ -0,0 +1,129 @@
+import numpy as np
+import numbers
+
+
+class Quaternion:
+    """
+    A simple class implementing basic quaternion arithmetic.
+    """
+    def __init__(self, w_or_q, x=None, y=None, z=None):
+        """
+        Initializes a Quaternion object
+        :param w_or_q: A scalar representing the real part of the quaternion, another Quaternion object or a
+                    four-element array containing the quaternion values
+        :param x: The first imaginary part if w_or_q is a scalar
+        :param y: The second imaginary part if w_or_q is a scalar
+        :param z: The third imaginary part if w_or_q is a scalar
+        """
+        self._q = np.array([1, 0, 0, 0])
+
+        if x is not None and y is not None and z is not None:
+            w = w_or_q
+            q = np.array([w, x, y, z])
+        elif isinstance(w_or_q, Quaternion):
+            q = np.array(w_or_q.q)
+        else:
+            q = np.array(w_or_q)
+            if len(q) != 4:
+                raise ValueError("Expecting a 4-element array or w x y z as parameters")
+
+        self._set_q(q)
+
+    # Quaternion specific interfaces
+
+    def conj(self):
+        """
+        Returns the conjugate of the quaternion
+        :rtype : Quaternion
+        :return: the conjugate of the quaternion
+        """
+        return Quaternion(self._q[0], -self._q[1], -self._q[2], -self._q[3])
+
+    def to_angle_axis(self):
+        """
+        Returns the quaternion's rotation represented by an Euler angle and axis.
+        If the quaternion is the identity quaternion (1, 0, 0, 0), a rotation along the x axis with angle 0 is returned.
+        :return: rad, x, y, z
+        """
+        if self[0] == 1 and self[1] == 0 and self[2] == 0 and self[3] == 0:
+            return 0, 1, 0, 0
+        rad = np.arccos(self[0]) * 2
+        imaginary_factor = np.sin(rad / 2)
+        if abs(imaginary_factor) < 1e-8:
+            return 0, 1, 0, 0
+        x = self._q[1] / imaginary_factor
+        y = self._q[2] / imaginary_factor
+        z = self._q[3] / imaginary_factor
+        return rad, x, y, z
+
+    @staticmethod
+    def from_angle_axis(rad, x, y, z):
+        s = np.sin(rad / 2)
+        return Quaternion(np.cos(rad / 2), x*s, y*s, z*s)
+
+    def to_euler_angles(self):
+        pitch = np.arcsin(2 * self[1] * self[2] + 2 * self[0] * self[3])
+        if np.abs(self[1] * self[2] + self[3] * self[0] - 0.5) < 1e-8:
+            roll = 0
+            yaw = 2 * np.arctan2(self[1], self[0])
+        elif np.abs(self[1] * self[2] + self[3] * self[0] + 0.5) < 1e-8:
+            roll = -2 * np.arctan2(self[1], self[0])
+            yaw = 0
+        else:
+            roll = np.arctan2(2 * self[0] * self[1] - 2 * self[2] * self[3], 1 - 2 * self[1] ** 2 - 2 * self[3] ** 2)
+            yaw = np.arctan2(2 * self[0] * self[2] - 2 * self[1] * self[3], 1 - 2 * self[2] ** 2 - 2 * self[3] ** 2)
+        return roll, pitch, yaw
+
+    def to_euler123(self):
+        roll = np.arctan2(-2*(self[2]*self[3] - self[0]*self[1]), self[0]**2 - self[1]**2 - self[2]**2 + self[3]**2)
+        pitch = np.arcsin(2*(self[1]*self[3] + self[0]*self[1]))
+        yaw = np.arctan2(-2*(self[1]*self[2] - self[0]*self[3]), self[0]**2 + self[1]**2 - self[2]**2 - self[3]**2)
+        return roll, pitch, yaw
+
+    def __mul__(self, other):
+        """
+        multiply the given quaternion with another quaternion or a scalar
+        :param other: a Quaternion object or a number
+        :return:
+        """
+        if isinstance(other, Quaternion):
+            w = self._q[0]*other._q[0] - self._q[1]*other._q[1] - self._q[2]*other._q[2] - self._q[3]*other._q[3]
+            x = self._q[0]*other._q[1] + self._q[1]*other._q[0] + self._q[2]*other._q[3] - self._q[3]*other._q[2]
+            y = self._q[0]*other._q[2] - self._q[1]*other._q[3] + self._q[2]*other._q[0] + self._q[3]*other._q[1]
+            z = self._q[0]*other._q[3] + self._q[1]*other._q[2] - self._q[2]*other._q[1] + self._q[3]*other._q[0]
+
+            return Quaternion(w, x, y, z)
+        elif isinstance(other, numbers.Number):
+            q = self._q * other
+            return Quaternion(q)
+
+    def __add__(self, other):
+        """
+        add two quaternions element-wise or add a scalar to each element of the quaternion
+        :param other:
+        :return:
+        """
+        if not isinstance(other, Quaternion):
+            if len(other) != 4:
+                raise TypeError("Quaternions must be added to other quaternions or a 4-element array")
+            q = self.q + other
+        else:
+            q = self.q + other.q
+
+        return Quaternion(q)
+
+    # Implementing other interfaces to ease working with the class
+
+    def _set_q(self, q):
+        self._q = q
+
+    def _get_q(self):
+        return self._q
+
+    q = property(_get_q, _set_q)
+
+    def __getitem__(self, item):
+        return self._q[item]
+
+    def __array__(self):
+        return self._q
