25d95a828e4f03432c306259f1fd72f36add0a66
david <david.l.smith166@gmail.com>
2020-08-23 23:30:08 -0400

    [Localization] Achieved RTK Float without relying on external casting server

--- base_station/gps/src/__main__.py
+++ base_station/gps/src/__main__.py
@@ -1,23 +1,22 @@
-import socket
+import serial
 import time
 from rover_common import aiolcm
 from rover_msgs import RTCM
 
 # To be moved to config
-_TCP_IP = '127.0.0.1'
-_TCP_PORT = 2222
-_BUFFER_SIZE = 4096
+_BAUDRATE = 115200
+_FILENAME = '/dev/ttyACM0'
+_BUFFSIZE = 1024
 
 
 def main():
     lcm = aiolcm.AsyncLCM()
     rtcm = RTCM()
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((_TCP_IP, _TCP_PORT))
-        while (s):
+    with serial.Serial(_FILENAME, _BAUDRATE) as s:
+        while (s.is_open):
             try:
-                byte_string = s.recv(_BUFFER_SIZE)
-                print(byte_string, len(byte_string))
+                byte_string = s.read(_BUFFSIZE)
+                print(byte_string)
             except Exception as e:
                 print(e)
                 time.sleep(1)
