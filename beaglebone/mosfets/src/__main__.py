5fc775588a8ba1e84a6366cdb1879ea7bb81c3a0
Polishdudealan <atondryk@umich.edu>
2020-09-16 20:52:11 -0400

    refactored mosfet variables and changed number of devices/ pins to 6

--- beaglebone/mosfets/src/__main__.py
+++ beaglebone/mosfets/src/__main__.py
@@ -4,18 +4,13 @@ import lcm
 
 lcm_ = lcm.LCM()
 
-# TODO: Update device names accordng to LCM constants
 pins = {
-    MosfetCmd.DEV0: "P8_7",
-    MosfetCmd.DEV1: "P8_8",
-    MosfetCmd.DEV2: "P8_9",
-    MosfetCmd.DEV3: "P8_10",
-    MosfetCmd.DEV4: "P8_11",
-    MosfetCmd.DEV5: "P8_12",
-    MosfetCmd.DEV6: "P8_15",
-    MosfetCmd.DEV7: "P8_16",
-    MosfetCmd.DEV8: "P8_17",
-    MosfetCmd.DEV9: "P8_18",
+    MosfetCmd.HEAT0_12V: "P8_7",
+    MosfetCmd.HEAT1_12V: "P8_8",
+    MosfetCmd.HEAT2_12V: "P8_9",
+    MosfetCmd.UV_LED_REP_5V: "P8_10",
+    MosfetCmd.LED_24V: "P8_11",
+    MosfetCmd.RA_LASER_3_3V: "P8_12",
 }
 
 
