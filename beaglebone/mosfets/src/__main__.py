ac9e01d30858c9e5d9f0a23824185408f2153c94
Carmen Giger <cgiger@umich.edu>
2020-09-27 10:22:19 -0400

    linter

--- beaglebone/mosfets/src/__main__.py
+++ beaglebone/mosfets/src/__main__.py
@@ -5,12 +5,12 @@ import lcm
 lcm_ = lcm.LCM()
 
 pins = {
-    MosfetCmd.HEAT_W_12V    : "P8_7",
-    MosfetCmd.UV_LED_REP_5V : "P8_8",
-    MosfetCmd.LED_24V       : "P8_9",
-    MosfetCmd.HEAT_B_12V    : "P8_10",
-    MosfetCmd.HEAT_Y_12V    : "P8_11",
-    MosfetCmd.RA_LASER_3_3V : "P8_12"
+    MosfetCmd.HEAT_W_12V:    "P8_7",
+    MosfetCmd.UV_LED_REP_5V: "P8_8",
+    MosfetCmd.LED_24V:       "P8_9",
+    MosfetCmd.HEAT_B_12V:    "P8_10",
+    MosfetCmd.HEAT_Y_12V:    "P8_11",
+    MosfetCmd.RA_LASER_3_3V: "P8_12"
 }
 
 
