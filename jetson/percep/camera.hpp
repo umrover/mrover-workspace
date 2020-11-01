4a2b37a82b0b9f5ca5773419c8adee3d0cde7e20
JayeshM29 <71610772+JayeshM29@users.noreply.github.com>
2020-10-31 21:36:37 -0400

    [percep] Added frame saving functions to camera class resolves #433 #434 (#466)
    Co-authored-by: mmartin4972 <mmartin4972@gmail.com>

--- jetson/percep/camera.hpp
+++ jetson/percep/camera.hpp
@@ -2,6 +2,7 @@
 #include <opencv2/opencv.hpp>
 #include "perception.hpp"
 
+
 #if OBSTACLE_DETECTION
 	#include <pcl/common/common_headers.h>
 #endif
@@ -23,7 +24,13 @@ public:
 	#if OBSTACLE_DETECTION
 	void getDataCloud(pcl::PointCloud<pcl::PointXYZRGB>::Ptr &p_pcl_point_cloud);
 	#endif
-	
+
+	void disk_record_init();
+	void write_curr_frame_to_disk(cv::Mat rgb, cv::Mat depth, int counter);
+
 private:
 	Impl *impl_;
+	std::string rgb_foldername;
+	std::string depth_foldername;
 };
+
