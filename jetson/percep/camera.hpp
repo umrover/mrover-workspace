485588785bd60e7916c75067ae7643702888690d
Ashwin Gupta <ashwingupta2000@gmail.com>
2020-11-08 00:57:32 -0500

    [percep] Organized AR tag record (#472)

--- jetson/percep/camera.hpp
+++ jetson/percep/camera.hpp
@@ -2,10 +2,13 @@
 #include <opencv2/opencv.hpp>
 #include "perception.hpp"
 
+//#include "artag_detector.hpp"
+
 
 #if OBSTACLE_DETECTION
 	#include <pcl/common/common_headers.h>
 #endif
+
 
 class Camera {
 private:
@@ -13,6 +16,10 @@ private:
 	Impl *impl_;
 	std::string rgb_foldername;
 	std::string depth_foldername;
+	//Video Stuff
+  	//std::pair<Tag, Tag> tp;
+  	//TagDetector d1;
+	cv::VideoWriter vidWrite;
 	
 public:
 	Camera();
@@ -26,6 +33,10 @@ public:
 	#if OBSTACLE_DETECTION
 	void getDataCloud(pcl::PointCloud<pcl::PointXYZRGB>::Ptr &p_pcl_point_cloud);
 	#endif
+
+	void record_ar_init();
+	void record_ar(cv::Mat rgb);
+	void record_ar_finish();
 
 	void disk_record_init();
 	void write_curr_frame_to_disk(cv::Mat rgb, cv::Mat depth, int counter);
