3220c6385d60d719b1f04ec43615dc5b03aa5c4e
raytitan <raytitan@umich.edu>
2020-11-11 19:24:54 -0500

    Updated to version that was tested/verified on the jetson

--- onboard/nucleo_bridge/LCMHandler.h
+++ onboard/nucleo_bridge/LCMHandler.h
@@ -1,8 +1,8 @@
 #ifndef LCMHANDLER_H
 #define LCMHANDLER_H
 
-#include "controller.h"
-#include "hardware.h"
+#include "Controller.h"
+#include "Hardware.h"
 
 #include <unordered_map>
 #include <string>
@@ -35,10 +35,42 @@ class LCMHandler
 private:
     inline static std::chrono::high_resolution_clock::time_point last_output_time = NOW;
 
-    inline static lcm::LCM lcm_bus = 0;
+    inline static lcm::LCM *lcm_bus = nullptr;
 
+    
     //Empty object to pass to lcm subscribe
-    inline static LCMHandler *internal_object = nullptr;
+    class InternalHandler {
+    public: 
+    //The following functions are handlers for the corresponding lcm messages
+        void ra_closed_loop_cmd(LCM_INPUT, const ArmPosition *msg);
+
+        void sa_closed_loop_cmd(LCM_INPUT, const SAClosedLoopCmd *msg);
+
+        void ra_open_loop_cmd(LCM_INPUT, const RAOpenLoopCmd *msg);
+
+        void sa_open_loop_cmd(LCM_INPUT, const SAOpenLoopCmd *msg);
+
+        /*
+	void ra_config_cmd(LCM_INPUT, const RAConfigCmd *msg);
+        void sa_config_cmd(LCM_INPUT, const SAConfigCmd *msg);
+        void sa_zero_trigger(LCM_INPUT, const SAZeroTrigger *msg);
+        void ra_zero_trigger(LCM_INPUT, const RAZeroTrigger *msg);
+        */ 
+        void hand_openloop_cmd(LCM_INPUT, const HandCmd *msg);
+
+        void foot_openloop_cmd(LCM_INPUT, const FootCmd *msg);
+        
+	void gimbal_cmd(LCM_INPUT, const GimbalCmd *msg);
+
+        void refreshAngles();
+
+        void ra_pos_data();
+
+        void sa_pos_data();
+
+    };
+
+    inline static InternalHandler *internal_object = nullptr;
 
 public:
     //Initialize the lcm bus and subscribe to relevant channels with message handlers defined below
@@ -50,37 +82,6 @@ public:
     //Decide whether outgoing messages need to be sent, and if so, send them
     static void handle_outgoing();
 
-    //Empty object to pass to lcm subscribe
-    LCMHandler();
-
-    //The following functions are handlers for the corresponding lcm messages
-    void ra_closed_loop_cmd(LCM_INPUT, const ArmPosition *msg);
-
-    void sa_closed_loop_cmd(LCM_INPUT, const SAClosedLoopCmd *msg);
-
-    void ra_open_loop_cmd(LCM_INPUT, const RAOpenLoopCmd *msg);
-
-    void sa_open_loop_cmd(LCM_INPUT, const SAOpenLoopCmd *msg);
-
-    void ra_config_cmd(LCM_INPUT, const RAConfigCmd *msg);
-
-    void sa_config_cmd(LCM_INPUT, const SAConfigCmd *msg);
-
-    void hand_openloop_cmd(LCM_INPUT, const HandCmd *msg);
-
-    void gimbal_cmd(LCM_INPUT, const GimbalCmd *msg);
-
-    void refreshAngles();
-
-    void ra_pos_data();
-
-    void sa_pos_data();
-
-    void sa_zero_trigger(LCM_INPUT, const SAZeroTrigger *msg);
-
-    //void ra_zero_trigger(LCM_INPUT, const RAZeroTrigger *msg);
-
-    void foot_openloop_cmd(LCM_INPUT, const FootCmd *msg);
 };
 
 #endif
