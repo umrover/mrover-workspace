b46a24fc0e5e416265b59a89e9c5eb7f0050608d
Rushil Talla <rushilt@umich.edu>
2020-11-19 21:19:03 -0500

    Edited IMU

--- beaglebone/imu/src/__main__.py
+++ beaglebone/imu/src/__main__.py
@@ -1,5 +1,7 @@
 import Adafruit_BBIO.UART as UART
-import serial as se
+import serial
+
+baud = 115200
 
 #https://learn.adafruit.com/setting-up-io-python-library-on-beaglebone-black/uart
 #UART1 RX:P9_26 TX:P9_24 CTS:P9_20 RTS:P9_19
@@ -10,7 +12,36 @@ UART.setup("UART2")
 #Binary packets Pg. 31
 #https://www.pololu.com/file/0J1556/UM7%20Datasheet_v1-8_30.07.2018.pdf
 
+#All SPI operations begin when the master writes two control bytes to the bus. The first byte
+#indicates whether the operation is a register read (0x00) or a write (0x01). The second byte is
+#the address of the register being accessed. 
+#first byte read or write, second is address
 
+# use with serial.Serial(port="/dev/ttyS4", baudrate=baud) as ser
+#pg 15 for talking about packets
+ser.write(0x00)
+
+
+
+
+#Wants Gyroscopic accelerometer and magnetometer data
+#Data Registers
+XGyro = 0x61  # Processed x-axis rate gyro data
+YGyro = 0x62  # Processed y-axis rate gyro data
+ZGyro = 0x63  # Processed z-axis rate gyro data 
+XAccel = 0x65  # Processed x-axis accel data
+YAccel = 0x66  # Processed y-axis accel data
+ZAccel = 0x67  # Processed z-axis accel data 
+XMag = 0x69  # Processed x-axis magnetometer data
+YMag = 0x6A  # Processed y-axis magnetometer data
+ZMag = 0x6B  # Processed z-axis magnetometer data 
+
+
+
+
+
+
+#Old Code
 
 # Command flag
 COMMAND = (1 << 7)
@@ -18,7 +49,7 @@ COMMAND = (1 << 7)
 #LightSensor I2C_Address
 I2C_ADDRESS = 0x29
 
-#Relevant Registers (From .h sheet)
+#Data Registers (From .h sheet)
 CD_LS = 0x14  # Clear channel data low byte 
 CD_MS = 0x15  # Clear channel data high byte
 R_LS = 0x16  # Red channel data low byte 
