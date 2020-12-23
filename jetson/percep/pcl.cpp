7abd7303a2c4fc3b46741577741c7fe254dcd55b
Kevin X Ji <kevinji@gl-login1.arc-ts.umich.edu>
2020-12-23 15:53:48 -0500

    Finds closest distance of last obstacle. Needs to be tested

--- jetson/percep/pcl.cpp
+++ jetson/percep/pcl.cpp
@@ -151,13 +151,17 @@ void PCL::FindInterestPoints(std::vector<pcl::PointIndices> &cluster_indices,
         pcl::ScopeTime t ("Find Interest Points");
     #endif
 
+    //double previousDistance = DBL_MAX;
+    
     for (int i = 0; i < (int)cluster_indices.size(); ++i)
     {
         std::vector<int>* curr_cluster = &interest_points[i];
-
+        //double currentDistance = 0;
+        
         //Initialize interest points
         std::fill(curr_cluster->begin(), curr_cluster->end(), cluster_indices[i].indices[0]);
         
+        //double sizeOfCluster = 0;
         //Order of interest points: 0=Up Left 1=Up Right 2=Low Right 3=Low Left
         for (auto index : cluster_indices[i].indices)
         {
@@ -175,15 +179,26 @@ void PCL::FindInterestPoints(std::vector<pcl::PointIndices> &cluster_indices,
             //Low Right
             else if(curr_point.x > pt_cloud_ptr->points[curr_cluster->at(3)].x && curr_point.y < pt_cloud_ptr->points[curr_cluster->at(3)].y)
                 curr_cluster->at(3) = index;
+
+            //currentDistance += curr_point.z;
+            //sizeOfCluster++;
         }
-    #if PERCEPTION_DEBUG
-        for(auto interest_point : *curr_cluster)
-        {
-            pt_cloud_ptr->points[interest_point].r = 255;
-            pt_cloud_ptr->points[interest_point].g = 255;
-            pt_cloud_ptr->points[interest_point].b = 255;
-        }
-    #endif
+        /*currentDistance = 1.0 * currentDistance / sizeOfCluster;
+        if(currentDistance < previousDistance) {
+            previousDistance = currentDistance;
+            std::cout << "Distance " << previousDistance << ", Cluster Size " << sizeOfCluster << std::endl;
+            distance = previousDistance;
+            std::cout << "Distance to closestObstacle "  << distance << std::endl;
+        }*/
+
+        #if PERCEPTION_DEBUG
+            for(auto interest_point : *curr_cluster)
+            {
+                pt_cloud_ptr->points[interest_point].r = 255;
+                pt_cloud_ptr->points[interest_point].g = 255;
+                pt_cloud_ptr->points[interest_point].b = 255;
+            }
+        #endif
     }
 
 }
@@ -279,8 +294,12 @@ bool PCL::CheckPath(std::vector<std::vector<int>> interest_points,
 
     bool end = true;
     
+    double previousDistance = DBL_MAX;
+    
     //Iterate through interest points
     for(auto cluster : interest_points) {
+        double sizeOfCluster = 0;
+        double currentDistance = 0;
         for (auto index : cluster) {
             //Check if the obstacle interest point is to the right of the left projected path of the rover 
             //and to the left of the right projected path of the rover
@@ -306,6 +325,16 @@ bool PCL::CheckPath(std::vector<std::vector<int>> interest_points,
                 pt_cloud_ptr->points[index].g = 69;
                 pt_cloud_ptr->points[index].b = 0;
                 #endif
+
+                currentDistance += pt_cloud_ptr->points[index].z;
+                sizeOfCluster++; 
+            }
+            currentDistance = 1.0 * currentDistance / sizeOfCluster;
+            if(currentDistance < previousDistance) {
+            previousDistance = currentDistance;
+            std::cout << "Distance " << previousDistance << ", Cluster Size " << sizeOfCluster << std::endl;
+            distance = previousDistance;
+            std::cout << "Distance to closestObstacle "  << distance << std::endl;
             }
         }
     }
