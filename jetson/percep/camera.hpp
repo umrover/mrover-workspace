b82a8f49611cc130eadde8caefa7db840531e50e
Kevin X Ji <kevinji@gl-login1.arc-ts.umich.edu>
2021-01-21 19:56:35 -0500

    Clean up syntax

--- jetson/percep/camera.hpp
+++ jetson/percep/camera.hpp
@@ -7,34 +7,34 @@
 #endif
 
 class Camera {
-private:
-	class Impl;
-	Impl *impl_;
-	std::string rgb_foldername;
-	std::string depth_foldername;
-	std::string pcl_foldername;
+	private:
+		class Impl;
+		Impl *impl_;
+		std::string rgb_foldername;
+		std::string depth_foldername;
+		std::string pcl_foldername;
 
-    cv::VideoWriter vidWrite;
-	
-public:
-	Camera();
-	~Camera();
+		cv::VideoWriter vidWrite;
+		
+	public:
+		Camera();
+		~Camera();
 
-	bool grab();
+		bool grab();
 
-	cv::Mat image();
-	cv::Mat depth();
-	
-	#if OBSTACLE_DETECTION
-	void getDataCloud(pcl::PointCloud<pcl::PointXYZRGB>::Ptr &p_pcl_point_cloud);
-	#endif
+		cv::Mat image();
+		cv::Mat depth();
+		
+		#if OBSTACLE_DETECTION
+			void getDataCloud(pcl::PointCloud<pcl::PointXYZRGB>::Ptr &p_pcl_point_cloud);
+		#endif
 
-	#if WRITE_CURR_FRAME_TO_DISK && AR_DETECTION && OBSTACLE_DETECTION
-	void disk_record_init();
-	void write_curr_frame_to_disk(cv::Mat rgb, cv::Mat depth, pcl::PointCloud<pcl::PointXYZRGB>::Ptr &p_pcl_point_cloud, int counter);
-	#endif
+		#if WRITE_CURR_FRAME_TO_DISK && AR_DETECTION && OBSTACLE_DETECTION
+			void disk_record_init();
+			void write_curr_frame_to_disk(cv::Mat rgb, cv::Mat depth, pcl::PointCloud<pcl::PointXYZRGB>::Ptr &p_pcl_point_cloud, int counter);
+		#endif
 
-	void record_ar_init();
-	void record_ar(cv::Mat rgb);
-	void record_ar_finish();
+		void record_ar_init();
+		void record_ar(cv::Mat rgb);
+		void record_ar_finish();
 };
