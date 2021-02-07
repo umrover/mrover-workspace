a3b903a7e5f5ac831c4ad186181b1283fff99db5
rzfeng <ryan.feng16@gmail.com>
2021-02-06 23:04:12 -0500

    Merge beaglebone/imu from Jonathan

---
+++ beaglebone/imu/src/madgwickahrs.py
@@ -0,0 +1,123 @@
+import warnings
+import numpy as np
+from numpy.linalg import norm
+from . import quaternion as Quaternion
+
+
+class MadgwickAHRS:
+    samplePeriod = 1/256
+    quaternion = Quaternion.Quaternion(1, 0, 0, 0)
+    beta = 1
+
+    def __init__(self, sampleperiod=None, quaternion=None, beta=None):
+        """
+        Initialize the class with the given parameters.
+        :param sampleperiod: The sample period
+        :param quaternion: Initial quaternion
+        :param beta: Algorithm gain beta
+        :return:
+        """
+        if sampleperiod is not None:
+            self.samplePeriod = sampleperiod
+        if quaternion is not None:
+            self.quaternion = quaternion
+        if beta is not None:
+            self.beta = beta
+
+    def update(self, gyroscope, accelerometer, magnetometer):
+        """
+        Perform one update step with data from a AHRS sensor array
+        :param gyroscope: A three-element array containing the gyroscope data in radians per second.
+        :param accelerometer: A three-element array containing the
+        accelerometer data. Can be any unit since a normalized value is used.
+        :param magnetometer: A three-element array containing the magnetometer
+        data. Can be any unit since a normalized value is used.
+        :return:
+        """
+        q = self.quaternion
+
+        gyroscope = np.array(gyroscope, dtype=float).flatten()
+        accelerometer = np.array(accelerometer, dtype=float).flatten()
+        magnetometer = np.array(magnetometer, dtype=float).flatten()
+
+        # Normalise accelerometer measurement
+        if norm(accelerometer) is 0:
+            warnings.warn("accelerometer is zero")
+            return
+        accelerometer /= norm(accelerometer)
+
+        # Normalise magnetometer measurement
+        if norm(magnetometer) is 0:
+            warnings.warn("magnetometer is zero")
+            return
+        magnetometer /= norm(magnetometer)
+
+        h = q * (Quaternion.Quaternion(0, magnetometer[0], magnetometer[1], magnetometer[2]) * q.conj())
+        b = np.array([0, norm(h[1:3]), 0, h[3]])
+
+        # Gradient descent algorithm corrective step
+        f = np.array([
+            2*(q[1]*q[3] - q[0]*q[2]) - accelerometer[0],
+            2*(q[0]*q[1] + q[2]*q[3]) - accelerometer[1],
+            2*(0.5 - q[1]**2 - q[2]**2) - accelerometer[2],
+            2*b[1]*(0.5 - q[2]**2 - q[3]**2) + 2*b[3]*(q[1]*q[3] - q[0]*q[2]) - magnetometer[0],
+            2*b[1]*(q[1]*q[2] - q[0]*q[3]) + 2*b[3]*(q[0]*q[1] + q[2]*q[3]) - magnetometer[1],
+            2*b[1]*(q[0]*q[2] + q[1]*q[3]) + 2*b[3]*(0.5 - q[1]**2 - q[2]**2) - magnetometer[2]
+        ])
+        j = np.array([
+            [-2*q[2],                  2*q[3],                  -2*q[0],                  2*q[1]],
+            [2*q[1],                   2*q[0],                  2*q[3],                   2*q[2]],
+            [0,                        -4*q[1],                 -4*q[2],                  0],
+            [-2*b[3]*q[2],             2*b[3]*q[3],             -4*b[1]*q[2]-2*b[3]*q[0], -4*b[1]*q[3]+2*b[3]*q[1]],
+            [-2*b[1]*q[3]+2*b[3]*q[1], 2*b[1]*q[2]+2*b[3]*q[0], 2*b[1]*q[1]+2*b[3]*q[3],  -2*b[1]*q[0]+2*b[3]*q[2]],
+            [2*b[1]*q[2],              2*b[1]*q[3]-4*b[3]*q[1], 2*b[1]*q[0]-4*b[3]*q[2],  2*b[1]*q[1]]
+        ])
+        step = j.T.dot(f)
+        step /= norm(step)  # normalise step magnitude
+
+        # Compute rate of change of quaternion
+        qdot = (q * Quaternion.Quaternion(0, gyroscope[0], gyroscope[1], gyroscope[2])) * 0.5 - self.beta * step.T
+
+        # Integrate to yield quaternion
+        q += qdot * self.samplePeriod
+        self.quaternion = Quaternion.Quaternion(q / norm(q))  # normalise quaternion
+
+    def update_imu(self, gyroscope, accelerometer):
+        """
+        Perform one update step with data from a IMU sensor array
+        :param gyroscope: A three-element array containing the gyroscope data
+        in radians per second.
+        :param accelerometer: A three-element array containing the
+        accelerometer data. Can be any unit since a normalized value is used.
+        """
+        q = self.quaternion
+
+        gyroscope = np.array(gyroscope, dtype=float).flatten()
+        accelerometer = np.array(accelerometer, dtype=float).flatten()
+
+        # Normalise accelerometer measurement
+        if norm(accelerometer) is 0:
+            warnings.warn("accelerometer is zero")
+            return
+        accelerometer /= norm(accelerometer)
+
+        # Gradient descent algorithm corrective step
+        f = np.array([
+            2*(q[1]*q[3] - q[0]*q[2]) - accelerometer[0],
+            2*(q[0]*q[1] + q[2]*q[3]) - accelerometer[1],
+            2*(0.5 - q[1]**2 - q[2]**2) - accelerometer[2]
+        ])
+        j = np.array([
+            [-2*q[2], 2*q[3], -2*q[0], 2*q[1]],
+            [2*q[1], 2*q[0], 2*q[3], 2*q[2]],
+            [0, -4*q[1], -4*q[2], 0]
+        ])
+        step = j.T.dot(f)
+        step /= norm(step)  # normalise step magnitude
+
+        # Compute rate of change of quaternion
+        qdot = (q * Quaternion.Quaternion(0, gyroscope[0], gyroscope[1], gyroscope[2])) * 0.5 - self.beta * step.T
+
+        # Integrate to yield quaternion
+        q += qdot * self.samplePeriod
+        self.quaternion = Quaternion.Quaternion(q / norm(q))  # normalise quaternion
