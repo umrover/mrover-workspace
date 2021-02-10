d8017d1f0c923cfe83d3ed7aaee92f7397943008
Carmen Giger <cgiger00@gmail.com>
2021-02-10 14:55:44 -0500

    added functionality of the pervious imu w filtering + calibration

--- beaglebone/snafoo/src/__main__.py
+++ beaglebone/snafoo/src/__main__.py
@@ -1,68 +1,73 @@
 # Simple demo of the FXAS21002C gyroscope.
 # Will print the gyroscope values every second.
 import time
-import board
-import busio
-import adafruit_fxas21002c
-import adafruit_fxos8700
 import lcm
 import numpy
 from rover_msgs import IMUData
-# Initialize I2C bus and device.
-i2c = busio.I2C(board.SCL, board.SDA)
-print("sees board")
-sensor = adafruit_fxas21002c.FXAS21002C(i2c)
-print("sees gyro")
-sensor2 = adafruit_fxos8700.FXOS8700(i2c)
-print("sees a/m")
-# Optionally create the sensor with a different gyroscope range (the
-# default is 250 DPS, but you can use 500, 1000, or 2000 DPS values):
-# sensor = adafruit_fxas21002c.FXAS21002C(i2c, gyro_range=adafruit_fxas21002c.GYRO_RANGE_500DPS)
-# sensor = adafruit_fxas21002c.FXAS21002C(i2c, gyro_range=adafruit_fxas21002c.GYRO_RANGE_1000DPS)
-# sensor = adafruit_fxas21002c.FXAS21002C(i2c, gyro_range=adafruit_fxas21002c.GYRO_RANGE_2000DPS)
-# Main loop will read the gyroscope values every second and print them out.
+from . import madgwickahrs as MadgwickAHRS
+from . import imu
+
+
+# Offsets applied to raw x/y/z mag values
+gyro_offsets = [0, 0, 0]
+accel_offsets = [0, 0, 0]
+mag_offsets = [0, 0, 0]
+
+# Soft iron error compensation matrix
+mag_softiron_matrix = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+mag_field_strength = 0
+
+filter = MadgwickAHRS.MadgwickAHRS()
 
 
 def main():
     global lcm_
     lcm_ = lcm.LCM()
     imudata = IMUData()
+
+    with open('mag_calibration.txt', 'r') as mag_calibration:
+        lines = mag_calibration.readlines()
+        mag_offsets = [float(x) for x in lines[0].split()]
+        mag_softiron_matrix = np.reshape([float(x) for x in lines[1].split()], (3, 3))
+        # mag_field_strength = [float(x) for x in lines[3].split()][0]
+
+    with open('ag_calibration.txt', 'r') as ag_calibration:
+        lines = ag_calibration.readlines()
+        accel_offsets = [float(x) for x in lines[0].split()]
+        gyro_offsets = [float(x) for x in lines[1].split()]
+
     while True:
         print("start while")
         # Read gyroscope.
-        gyro_x, gyro_y, gyro_z = sensor.gyroscope
-        # Print values.
-        print(
-            "Gyroscope (radians/s): ({0:0.3f}, {1:0.3f}, {2:0.3f})".format(
-                gyro_x, gyro_y, gyro_z
-            )
-        )
-        imudata.gyro_x_dps = gyro_x
-        imudata.gyro_y_dps = gyro_y
-        imudata.gyro_z_dps = gyro_z
 
-        # Read acceleration & magnetometer.
-        accel_x, accel_y, accel_z = sensor2.accelerometer
-        mag_x, mag_y, mag_z = sensor2.magnetometer
-        # Print values.
-        print(
-            "Acceleration (m/s^2): ({0:0.3f}, {1:0.3f}, {2:0.3f})".format(
-                accel_x, accel_y, accel_z
-            )
-        )
-        print(
-            "Magnetometer (uTesla): ({0:0.3f}, {1:0.3f}, {2:0.3f})".format(
-                mag_x, mag_y, mag_z
-            )
-        )
+        data = imu.get_data()
 
-        imudata.accel_x_g = accel_x
-        imudata.accel_y_g = accel_y
-        imudata.accel_z_g = accel_z
+        for i in range(6):
+            if (i < 3):
+                data[i] += accel_offsets[i]
+            else:
+                data[i] += gyro_offsets[i - 3]
 
-        imudata.mag_x_uT = mag_x
-        imudata.mag_y_uT = mag_y
-        imudata.mag_z_uT = mag_z
+        imudata.gyro_x_dps = data[imu.XGYRO]
+        imudata.gyro_y_dps = data[imu.YGYRO]
+        imudata.gyro_z_dps = data[imu.ZGYRO]
+
+        imudata.accel_x_g = data[imu.XACCEL]
+        imudata.accel_y_g = data[imu.YACCEL]
+        imudata.accel_z_g = data[imu.ZACCEL]
+
+        mag_x = (data[imu.XMAG] * 0.15) - mag_offsets[0]
+        mag_y = (data[imu.YMAG] * 0.15) - mag_offsets[1]
+        mag_z = (data[imu.ZMAG] * 0.15) - mag_offsets[2]
+
+        imudata.mag_x_uT = mag_x * mag_softiron_matrix[0][0] + \
+            mag_y * mag_softiron_matrix[0][1] + mag_z * mag_softiron_matrix[0][2]
+
+        imudata.mag_y_uT = mag_x * mag_softiron_matrix[1][0] + \
+            mag_y * mag_softiron_matrix[1][1] + mag_z * mag_softiron_matrix[1][2]
+
+        imudata.mag_z_uT = mag_x * mag_softiron_matrix[2][0] + \
+            mag_y * mag_softiron_matrix[2][1] + mag_z * mag_softiron_matrix[2][2]
 
         # Bearing calculation
         bearing = -(numpy.arctan2(imudata.mag_y_uT, imudata.mag_x_uT) * (180.0/numpy.pi))
@@ -72,8 +77,35 @@ def main():
         imudata.bearing_deg = bearing
         print("bearing: ", imudata.bearing_deg)
 
+        # Roll, Pitch, yaw calc
+        # Dividing the mag data by 100000 to get units to be Teslas
+        acc = numpy.array([imudata.accel_x_g, imudata.accel_y_g, imudata.accel_z_g])
+        gyr = numpy.array([imudata.gyro_x_dps, imudata.gyro_y_dps, imudata.gyro_z_dps])
+        mag = numpy.array([imudata.mag_x_uT / 1000000.0, imudata.mag_y_uT / 1000000.0, imudata.mag_z_uT / 1000000.0])
+        gyr_rad = gyr * (numpy.pi/180)
+
+        # Everything Past here is Auton stuff
+        filter.update(gyr_rad, acc, mag)
+        # aboves update method can be run instead of update_imu
+        # if the magnetometer problem is fixed
+        # filter.update_imu(gyr_rad,acc)
+        # updates the filter and returns roll,pitch,and yaw in quaternion form
+        ahrs = filter.quaternion.to_euler_angles()
+
+        # values are between -pi and pi
+        curRoll = ahrs[0]
+        curPitch = ahrs[2]
+        curYaw = ahrs[1]
+
+        # Remove prints after testing
+        print("Roll: ", curRoll, " Pitch: ", curPitch, " Yaw: ", curYaw)
+
+        imudata.roll_rad = curRoll
+        imudata.pitch_rad = curPitch
+        imudata.yaw_rad = curYaw
+
         # Delay for a second.
-        time.sleep(1.0)
+        time.sleep(0.01)
 
         lcm_.publish("/imu_data", imudata.encode())
 
---
+++ beaglebone/snafoo/src/calibrate.py
@@ -0,0 +1,38 @@
+from . import imu
+import time as t
+
+def main():
+
+    offsets = [0, 0, 0, 0, 0, 0, 0]
+
+    # averages first 50 samples of the accel and gyro data
+    for _ in range(50):
+        try:
+            data = imu.get_data()
+
+            # could just be a for loop, using constants for readbility
+            offsets[imu.XACCEL] += data[imu.XACCEL]
+            offsets[imu.YACCEL] += data[imu.YACCEL]
+
+            # in the z direction acceleration should be 1, so the offset is the difference
+            # accel units at 2048 LSB/g
+            offsets[imu.ZACCEL] += (data[imu.ZACCEL] - 1)
+
+            offsets[imu.XGYRO] += data[imu.XGYRO]
+            offsets[imu.YGYRO] += data[imu.YGYRO]
+            offsets[imu.ZGYRO] += data[imu.ZGYRO]
+
+        except Exception:
+            print("Connection Lost")
+            t.sleep(1)
+
+    for i in range(len(offsets)):
+        offsets[i] = offsets[i] / -50
+
+    with open('ag_calibration.txt', 'w') as f:
+        f.write("{} {} {} \n".format(offsets[imu.XACCEL], offsets[imu.YACCEL], offsets[imu.ZACCEL]))
+        f.write("{} {} {} \n".format(offsets[imu.XGYRO], offsets[imu.YGYRO], offsets[imu.ZGYRO]))
+
+
+if(__name__ == '__main__'):
+    main()
