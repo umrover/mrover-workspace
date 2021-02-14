3f336cfb61b74cee52b8d51d0d78a7033902c91b
vagrant <vagrant@mrover-devbox>
2021-02-14 19:17:16 +0000

    Added Chlorphyll UV button, moved zero encoders button. Removed waypoint editor.

---
+++ base_station/gui/src/components/Ammonia.vue
@@ -0,0 +1,43 @@
+<template>
+<div class="wrap">
+    <div>
+        <h3> Ammonia Controls </h3>
+    </div>
+    <div class="box">
+        <button>
+            Up
+        </button>
+        <button>
+            Down
+        </button>
+        <button>
+            Stop
+        </button>
+    </div>
+</div>
+</template>
+
+<style scoped>
+    .wrap {
+        display: inline-block;
+        align-content: center;
+        /* height: 300px; */
+    }
+    .box {
+        border-radius: 5px;
+        padding: 10px;
+        border: 1px solid black;
+        text-align: right;
+        vertical-align: top;
+    }
+</style>
+
+<script>
+export default {
+    data () {
+        return {
+
+        }
+    }
+}
+</script>
