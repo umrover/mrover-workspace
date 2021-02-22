9f59b8256423c945fe36ab2b441bc6ad443e8ef2
Jetson Bogie <jetsonbogie@mrover.edu>
2021-02-21 21:40:35 -0500

    config changes

--- jetson/odrive_bridge/src/__main__.py
+++ jetson/odrive_bridge/src/__main__.py
@@ -197,7 +197,7 @@ class OdriveBridge(object):
         global modrive
         global legal_controller
         print("looking for odrive")
-        odrives = ["2091358E524B", "20563591524B"]
+        odrives = ["205A377D5753", "2091358E524B", "207C39A14D4D"]
         id = odrives[legal_controller]
 
         print(id)
