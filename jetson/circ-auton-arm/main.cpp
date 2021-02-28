098212fa1cb8fdb67077b1a02d5b0ca47cce27c5
Michelle Baojia Li <michmli@gl3026.arc-ts.umich.edu>
2021-02-28 15:00:22 -0500

    code is kinda compiling

---
+++ jetson/circ-auton-arm/main.cpp
@@ -0,0 +1,134 @@
+#include <iostream>
+#include <lcm/lcm-cpp.hpp>
+#include "rover_msgs/CircAutonTarget.hpp"
+#include "rover_msgs/TargetList.hpp"
+#include "rover_msgs/TargetOrientation.hpp"
+#include <unistd.h>
+#include "perception.hpp"
+
+using namespace std;
+
+/* void outgoing() {
+    while(true) {
+        lcm.handle();
+        LCMHandler::time_outgoing();
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    }
+} */
+
+int main() {
+    cout<<"Hello world"<<endl;
+    cout<<ZED_SDK_PRESENT<<endl;
+/*
+    //create new lcm object
+    lcm::LCM lcmObj;
+    if( !lcmObject.good() )
+    {
+        cerr << "Error: cannot create LCM\n";
+        return 1;
+    }
+
+    LCMHandler lcmHandler(&roverStateMachine);
+
+    lcmObj.subscribe( "/circ_target", &LcmHandlers::CircTarget, &lcmHandler );
+
+    */
+
+    /* --- Camera Initializations --- */
+  Camera cam;
+  int iterations = 0;
+  cam.grab();
+
+  #if PERCEPTION_DEBUG
+    namedWindow("depth", 2);
+  #endif
+  
+  #if AR_DETECTION
+  Mat rgb;
+  Mat src = cam.image();
+  #endif
+
+  /*#if WRITE_CURR_FRAME_TO_DISK && AR_DETECTION && OBSTACLE_DETECTION
+    cam.disk_record_init();
+  #endif */
+
+  /* -- LCM Messages Initializations -- */
+  lcm::LCM lcm_;
+  rover_msgs::TargetList arTagsMessage;
+  //rover_msgs::Target* arTags = arTagsMessage.CircTarget;
+  rover_msgs::CircAutonTarget* arTags = arTagsMessage.targetList;
+  arTags[0].z = -1;
+  arTags[1].z = -1;
+
+  /* --- AR Tag Initializations --- */
+  TagDetector detector;
+  pair<Tag, Tag> tagPair;
+  
+   /* --- AR Recording Initializations and Implementation--- */ 
+  
+  time_t now = time(0);
+  char* ltm = ctime(&now);
+  string timeStamp(ltm);
+
+  #if AR_RECORD
+  //initializing ar tag videostream object
+  cam.record_ar_init();
+  #endif
+
+/* --- Main Processing Stuff --- */
+  while (true) {
+    //Check to see if we were able to grab the frame
+    if (!cam.grab()) break;
+
+    #if AR_DETECTION
+    //Grab initial images from cameras
+    Mat rgb;
+    Mat src = cam.image();
+    Mat depth_img = cam.depth();
+    #endif
+
+   /* #if WRITE_CURR_FRAME_TO_DISK && AR_DETECTION && OBSTACLE_DETECTION
+      if (iterations % FRAME_WRITE_INTERVAL == 0) {
+        Mat rgb_copy = src.clone(), depth_copy = depth_img.clone();
+        cerr << "Copied correctly" << endl;
+        cam.write_curr_frame_to_disk(rgb_copy, depth_copy, pointcloud.pt_cloud_ptr, iterations);
+      }
+    #endif*/
+
+/* --- AR Tag Processing --- */
+    arTags[0].z = -1;
+    arTags[1].z = -1;
+    #if AR_DETECTION
+    cerr<<"Finding AR Tags"<<endl;
+      tagPair = detector.findARTags(src, depth_img, rgb);
+      #if AR_RECORD
+        cam.record_ar(rgb);
+      #endif
+
+      detector.updateDetectedTagInfo(arTags, tagPair, depth_img, src);
+      cerr<<arTags[0].x<<" "<<arTags[0].y<<" "<<arTags[0].z<<" "<<arTags[0].id<<endl;
+
+    #if PERCEPTION_DEBUG && AR_DETECTION
+      imshow("depth", src);
+      waitKey(1);  
+    #endif
+
+    #endif
+
+/* --- Publish LCMs --- */
+  lcm_.publish("/target_list", &arTagsMessage);
+
+    #if !ZED_SDK_PRESENT
+    std::this_thread::sleep_for(0.2s); // Iteration speed control not needed when using camera 
+    #endif
+    cerr<<"LCM sent"<<endl;
+  }
+  cerr<<"process completed"<<endl;
+/* --- Wrap Things Up --- */
+  #if AR_RECORD
+    cam.record_ar_finish();
+  #endif
+
+ return 0;
+
+}
