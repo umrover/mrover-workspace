79801598978e59086f92198b4c108bb945cd6d41
jubileew <jubileew@gl-login1.arc-ts.umich.edu>
2021-03-06 21:55:07 -0500

    styling fix

--- jetson/percep/camera.cpp
+++ jetson/percep/camera.cpp
@@ -383,7 +383,7 @@ void Camera::Impl::dataCloud(pcl::PointCloud<pcl::PointXYZRGB>::Ptr &p_pcl_point
 #endif
 
 Camera::Camera(const rapidjson::Document &config) : 
-FRAME_WRITE_INTERVAL(mRoverConfig["camera"]["frame_write_interval"].GetInt()), impl_{new Camera::Impl(config)}, rgb_foldername{""},
+FRAME_WRITE_INTERVAL{mRoverConfig["camera"]["frame_write_interval"].GetInt()}, impl_{new Camera::Impl(config)}, rgb_foldername{""},
                          depth_foldername{""}, pcl_foldername{""} , mRoverConfig( config ) {}
 
 Camera::~Camera() {
