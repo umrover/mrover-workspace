a55dba5bd15d2361eeb21968d96777e56ee37170
Rushil Talla <rushilt@umich.edu>
2021-03-21 15:09:56 -0400

    Fixed linting

--- beaglebone/imu/src/__main__.py
+++ beaglebone/imu/src/__main__.py
@@ -1,12 +1,13 @@
 import Adafruit_BBIO.UART as UART
 import serial
-import struct
-import time
+# import struct
+# import time
 import asyncio
-from os import getenv
+# from os import getenv
 from rover_common.aiohelper import run_coroutines
 from rover_common import aiolcm
 from rover_msgs import IMUData
+
 
 class IMU_Manager():
 
@@ -39,7 +40,7 @@ class IMU_Manager():
 
     def pchrs_handler(self, msg, imu_struct):
         arr = msg.split(",")
-        
+
         # packet type can be either 0: gyro, 1: accel, 2: magnetometer
         # mag data is unit-nrom (unitless)
         try:
@@ -89,7 +90,6 @@ class IMU_Manager():
             imu_struct.yaw_rad = 0
             imu_struct.bearing_deg = 0
 
-
     async def recieve(self, lcm):
             '''
             Reads from the rover IMU over serial connection.
@@ -102,7 +102,7 @@ class IMU_Manager():
             error_counter = 0
             # Mark TXT as always seen because they are not necessary
             seen_tags = {tag: False if not tag == 'TXT' else True
-                        for tag in self.NMEA_TAGS_MAPPER.keys()}
+                         for tag in self.NMEA_TAGS_MAPPER.keys()}
             while True:
                 # Wait for all tags to be seen
                 while (not all(seen_tags.values())):
@@ -135,7 +135,7 @@ class IMU_Manager():
 
                 lcm.publish('/imu_data', imu_struct.encode())
                 seen_tags = {tag: False if not tag == 'TXT' else True
-                            for tag in self.NMEA_TAGS_MAPPER.keys()}
+                             for tag in self.NMEA_TAGS_MAPPER.keys()}
                 await asyncio.sleep(self.sleep)
 
     # turns off registers that are outputting non-NMEA data
@@ -143,22 +143,24 @@ class IMU_Manager():
         checksum = ord('s') + ord('n') + ord('p') + register + 0x80
 
         cmd_buffer = {ord('s'), ord('n'), ord('p'), 0x80, register,
-                     0x00, 0x00, 0x00, 0x00, checksum >> 8, checksum & 0xff}
-        
+                      0x00, 0x00, 0x00, 0x00, checksum >> 8, checksum & 0xff}
+
         self.ser.write(bytes(cmd_buffer))
-    
+
     # turns on the attidude and sensor nmea sentences to 1Hz
     def enable_nmea(self, register):
         checksum = ord('s') + ord('n') + ord('p') + register + 0x80 + 0x01
-        
+
         cmd_buffer = {ord('s'), ord('n'), ord('p'), 0x80, register,
-                         0 , 0x11, 0, 0, checksum >> 8, checksum & 0xff}
-        
+                      0, 0x11, 0, 0, checksum >> 8, checksum & 0xff}
+
         self.ser.write(bytes(cmd_buffer))
 
 # end of class
 
+
 def main():
+
     # Uses a context manager to ensure serial port released
     NMEA_RATE_REG = 0x07
 
@@ -167,7 +169,7 @@ def main():
         l = {0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07}
         for reg in l:
             manager.turnOffRegister(reg)
-        
+
         manager.enable_nmea(NMEA_RATE_REG)
 
         lcm = aiolcm.AsyncLCM()
@@ -177,144 +179,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-
-
-#https://learn.adafruit.com/setting-up-io-python-library-on-beaglebone-black/uart
-#UART1 RX:P9_26 TX:P9_24 CTS:P9_20 RTS:P9_19
-#UART2 RX:P9_22 TX:P9_21
-
-
-
-#Binary packets Pg. 31
-#https://www.pololu.com/file/0J1556/UM7%20Datasheet_v1-8_30.07.2018.pdf
-
-#All SPI operations begin when the master writes two control bytes to the bus. The first byte
-#indicates whether the operation is a register read (0x00) or a write (0x01). The second byte is
-#the address of the register being accessed. 
-#first byte read or write, second is address
-
-
-#pg 15 for talking about packets    This is spi though, Not UART?
-#ser.write(0x00)
-
-#pip install
-
-#Wants Gyroscopic accelerometer and magnetometer data
-#Data Registers
-# XGyro = 0x61  # Processed x-axis rate gyro data
-# YGyro = 0x62  # Processed y-axis rate gyro data
-# ZGyro = 0x63  # Processed z-axis rate gyro data 
-# XAccel = 0x65  # Processed x-axis accel data
-# YAccel = 0x66  # Processed y-axis accel data
-# ZAccel = 0x67  # Processed z-axis accel data 
-# XMag = 0x69  # Processed x-axis magnetometer data
-# YMag = 0x6A  # Processed y-axis magnetometer data
-# ZMag = 0x6B  # Processed z-axis magnetometer data 
-
-# #https://stackoverflow.com/questions/57982782/type-codes-in-python-for-array this is int
-# type = "<b"
-
-# ser = serial.Serial(port="/dev/ttyS4", baudrate=baud)
-# ser.close()
-# ser.open()
-
-# cmd_buffer = [0,0,0,0,0,0,0,0,0,0,0]
-# sending_buffer = [0,0,0,0,0,0,0]
-
-#Sets desired transmission rates for CHR NMEA-style packets (31 bits) Pg. 55
-#CREG_COM_RATES7 = 0x07
-#packetRates = 0xf0000
-
-
-
-
-
-
-
-# def turnOffRegister(register):
-#     cmd_buffer[0] = ord('s') 
-#     cmd_buffer[1] = ord('n')
-#     cmd_buffer[2] = ord('p')
-#     cmd_buffer[3] = 0x80
-#     cmd_buffer[4] = register
-#     #Data that is non zero
-#     cmd_buffer[5] = 0x00
-#     cmd_buffer[6] = 0x00
-#     cmd_buffer[7] = 0x00
-#     cmd_buffer[8] = 0x00
-#     checksum = ord('s') + ord('n') + ord('p') + register + 0x80
-#     cmd_buffer[9] = checksum >> 8     #0x02
-#     cmd_buffer[10] = checksum & 0xff  #0x1C
-    
-#     ser.write(bytes(cmd_buffer))
-   
-
-# def enable_nmea(register):
-#     cmd_buffer[0] = ord('s') 
-#     cmd_buffer[1] = ord('n')
-#     cmd_buffer[2] = ord('p')
-#     cmd_buffer[3] = 0x80
-#     cmd_buffer[4] = register #Processed Data
-
-#     #Data that is non zero
-#     cmd_buffer[5] = 0
-#     cmd_buffer[6] = 0x01
-#     cmd_buffer[7] = 0
-#     cmd_buffer[8] = 0
-
-#     checksum = ord('s') + ord('n') + ord('p') + register + 0x80 + 0x01
-
-#     cmd_buffer[9] = checksum >> 8     #0x02
-#     cmd_buffer[10] = checksum & 0xff  #0x1C
-
-#     ser.write(cmd_buffer)
-#     print(cmd_buffer)
-
-#     return 0
-
-
-#checkfirmware()
-#resetEKF()
-#attempting switch between 3 and 7
-
-
-
-
-
-
-#b = ser.read()
-#print(b) 
-#print(bytes('s', 'utf-8'))
-#while ser.isOpen():
-    
-    #read_data(0x5f)
-
-    # b = ser.read()
-    # if (b == bytes('s', 'utf-8')):
-    #     b = ser.read()
-    #     if (b == bytes('n', 'utf-8')):
-    #         b = ser.read()
-    #         if (b == bytes('p', 'utf-8')):
-    #             break
-
-
-
-#print("We got stuff!")
-    #Using xGyro gets four bytes of data
-    #GyroX = read_data(XGyro)
-    #GyroY = read_data(YGyro)
-    #GyroZ = read_data(ZGyro)
-    #AccelX = read_data(XAccel)
-    #AccelY = read_data(YAccel)
-    #AccelZ = read_data(ZAccel)
-    #MagX = read_data(XMag)
-    #MagY = read_data(YMag)
-    #MagZ = read_data(ZMag)
-    
-
-    
-
-    #print("Gyro:      X: " + GyroX + "Y: " + GyroY + "Z: " + GyroZ + "\n         Accel:     X: " + AccelX + "Y: " + AccelY + "Z: " + AccelZ + "\n         Mag:       X: " + MagX + "Y: " + MagY + "Z: " + MagZ)
-#ser.close()
