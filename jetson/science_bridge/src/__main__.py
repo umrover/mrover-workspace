23daea1e331e2b5fd14c0be355665d6e2bd38e75
jjtom <jjtom@umich.edu>
2021-04-04 13:56:19 -0400

    Started Linting

--- jetson/science_bridge/src/__main__.py
+++ jetson/science_bridge/src/__main__.py
@@ -12,7 +12,7 @@ from rover_common import aiolcm
 from rover_msgs import ThermistorData, MosfetCmd, RepeaterDrop, SpectralData, NavStatus, AmmoniaCmd
 class ScienceBridge():
     def __init__(self):
-        UART.setup("UART4") #  Specific to beaglebone
+        UART.setup("UART4")  #  Specific to beaglebone
         # maps NMEA msgs to their handler
         # mosfet, ammonia, and pump only send msgs
         self.NMEA_HANDLE_MAPPER = {
@@ -22,12 +22,13 @@ class ScienceBridge():
             "REPEATER": self.repeater_handler
         }
         self.NMEA_TRANSMIT_MAPPER = {
-            #"MOSFET" : self.mosfet_transmit,
-            #"AMMONIA" : self.ammonia_transmit,
-            #"PUMP" : self.pump_transmit
+            # "MOSFET" : self.mosfet_transmit,
+            # "AMMONIA" : self.ammonia_transmit,
+            # "PUMP" : self.pump_transmit
         }
         self.max_error_count = 20
         self.sleep = 1
+
     def __enter__(self):
         '''
         Opens a serial connection to the nucleo
@@ -42,16 +43,19 @@ class ScienceBridge():
             timeout=0
         )
         return self
+
     def __exit__(self, exc_type, exc_value, traceback):
         '''
         Closes serial connection to nucleo
         '''
         self.ser.close()
+
     def spectral_handler(self, msg, spectral_struct):
         try:
             arr = msg.split(",")
-            struct_variables = ["d0_1", "d0_2", "d0_3", "d0_4", "d0_5", "d0_6", "d1_1", "d1_2",
-                    "d1_3", "d1_4", "d1_5", "d1_6", "d2_1", "d2_2", "d2_3", "d2_4", "d2_5", "d2_6"]
+            struct_variables = ["d0_1", "d0_2", "d0_3", "d0_4", "d0_5", "d0_6",
+                                "d1_1", "d1_2", "d1_3", "d1_4", "d1_5", "d1_6",
+                                "d2_1", "d2_2", "d2_3", "d2_4", "d2_5", "d2_6"]
             count = 1
             for var in struct_variables:
                 setattr(spectral_struct, var, ((int16(arr[count]) << 8) | (int16(arr[count + 1]))))
@@ -75,15 +79,18 @@ class ScienceBridge():
         # parse the thermistor UART msg
         # adding relevant error handling
         # set the struct variables
+
     def txt_handler(self, msg, struct):
         '''
         Prints info messages revieved from nucleo to screen
         '''
         print(msg)
-    def repeater_handler(self,msg,struct):
+
+    def repeater_handler(self, msg, struct):
         # Expected to be an empty message so nothing is done
         # Only included to fit struct
         pass
+
     def mosfet_transmit(self, channel, msg):
         # get cmd lcm and send to nucleo
         print("Mosfet callback")
@@ -92,62 +99,63 @@ class ScienceBridge():
         # Currently expects mosfet, device number, and enable bit along
         # with padding to reach 30 bytes
         message = "$Mosfet,{device},{enable},1"
-        message = message.format(device = struct.device, enable = int(struct.enable))
+        message = message.format(device=struct.device,
+                                 enable=int(struct.enable))
         print(message)
         self.ser.close()
         self.ser.open()
         if self.ser.isOpen():
-            self.ser.write(bytes(message,encoding='utf8'))
+            self.ser.write(bytes(message, encoding='utf8'))
         print("Mosfet Received")
         pass
-    def rr_drop(self,channel,msg):
+
+    def rr_drop(self, channel, msg):
         print("Received rr_drop req")
         # Struct is expected to be empty so no need for decoding
         message = "$Mosfet,{device},{enable},1"
         # This is always an enable
         # Should be tied to SA UV = 4
-        message = message.format(device = 4, enable = 1)
-        self.ser.write(bytes(message,encoding='utf8'))
+        message = message.format(device=4, enable=1)
+        self.ser.write(bytes(message, encoding='utf8'))
         # Publish to drop complete after sending the message.
-    def nav_status(self,channel,msg):
+
+    def nav_status(self, channel, msg):
         print("Received nav req")
         # Want the name of the status I guess?
         # Off, Done, Else
         # Off = Bluee
         struct = NavStatus.decode(msg)
         message = "$Mosfet,{device},{enable},1"
-        # time.sleep(1)
-        # self.ser.write(bytes(message.format(device=prev, enable = 1),encoding='utf8'))
         if struct.nav_state_name == "Off":
             print("navstatus off")
-            message = message.format(device = 2, enable = 1)
-            self.ser.write(bytes(message,encoding='utf8'))
+            message = message.format(device=2, enable=1)
+            self.ser.write(bytes(message, encoding='utf8'))
             prev = 2
         # Done = Flashing green
         elif struct.nav_state_name == "Done":
             print("navstatus Done")
             # Flashing by turning on and off for 1 second intervals
             # Maybe change to 
-            for i in range (0,6):
-                self.ser.write(bytes(message.format(device = 1, enable = 1),encoding='utf8'))
+            for i in range(0, 6):
+                self.ser.write(bytes(message.format(device=1, enable=1), encoding='utf8'))
                 time.sleep(1)
-                self.ser.write(bytes(message.format(device = 1, enable = 0),encoding='utf8'))
+                self.ser.write(bytes(message.format(device=1, enable=0), encoding='utf8'))
                 time.sleep(1)
-                prev=1
+                prev = 1
         # Everytime else = Red
         else:
             print("navstatus else")
-            messageon = message.format(device = 0, enable = 1)
-            self.ser.write(bytes(messageon,encoding='utf8'))
+            messageon = message.format(device=0, enable=1)
+            self.ser.write(bytes(messageon, encoding='utf8'))
             prev = 0
         time.sleep(1)
         if (prev != 2):
-            self.ser.write(bytes(message.format(device=2, enable = 0),encoding='utf8'))
-        #time.sleep(1)
+            self.ser.write(bytes(message.format(device=2, enable=0),
+                                 encoding='utf8'))
         if (prev != 0):
-            self.ser.write(bytes(message.format(device=0,enable=0),encoding='utf8'))
+            self.ser.write(bytes(message.format(device=0, enable=0),
+                                 encoding='utf8'))
 
-        pass
     def ammonia_transmit(self, channel, msg):
         # get cmd lcm and send to nucleo
         struct = AmmoniaCmd.decode(msg)
@@ -155,7 +163,7 @@ class ScienceBridge():
         # parse data into expected format
         # self.ser.write(bytes(struct.data))
         message = "$AMMONIA,{speed}"
-        message = message.format(speed = struct.speed)
+        message = message.format(speed=struct.speed)
         print(len(message))
         while(len(message) < 13):
             message += ","
@@ -163,8 +171,8 @@ class ScienceBridge():
         self.ser.close()
         self.ser.open()
         if self.ser.isOpen():
-            self.ser.write(bytes(message,encoding='utf-8'))
-        pass
+            self.ser.write(bytes(message, encoding='utf-8'))
+
     # #if ser.isOpen():
     # while(1):
     #     print("Serial is open!")
@@ -177,7 +185,7 @@ class ScienceBridge():
         spectral = SpectralData()
         thermistor = ThermistorData()
         rr_drop = RepeaterDrop()
-         # Mark TXT as always seen because they are not necessary
+        # Mark TXT as always seen because they are not necessary
         seen_tags = {tag: False if not tag == 'TXT' else True
                      for tag in self.NMEA_HANDLE_MAPPER.keys()}
         while True:
@@ -186,7 +194,7 @@ class ScienceBridge():
                 try:
                     error_counter = 0
                     tx = self.ser.readline()
-                    msg =  str(tx)
+                    msg = str(tx)
                     print(msg)
 
                 except Exception as e:
@@ -223,6 +231,8 @@ class ScienceBridge():
             seen_tags = {tag: False if not tag == 'TXT' else True
                          for tag in self.NMEA_HANDLE_MAPPER.keys()}
             await asyncio.sleep(self.sleep)
+
+
 def main():
     # Uses a context manager to ensure serial port released
     with ScienceBridge() as bridge:
@@ -234,6 +244,7 @@ def main():
         print("properly started")
         # lcm.subscribe("/pump_cmd", bridge.pump_transmit)
         run_coroutines(_lcm.loop(), bridge.recieve(_lcm))
+
+
 if __name__ == "__main__":
     main()
-    
