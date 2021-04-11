01d81879e1d457c5b5885cf78b88d5dc7b71902d
Ashwin Gupta <ashmg@umich.edu>
2021-04-11 14:17:12 -0400

    percep camera driver

---
+++ jetson/percep_camera_driver/driver.cu
@@ -0,0 +1,69 @@
+#include <sl/Camera.hpp>
+#include <boost/interprocess/shared_memory_object.hpp>
+#include <boost/interprocess/mapped_region.hpp>
+using namespace boost::interprocess;
+
+bool checkStatus(cudaError_t status) {
+	if (status != cudaSuccess) {
+		printf("%s \n", cudaGetErrorString(status));
+		return true;
+	}
+    return false;
+}
+
+int main() {
+    // Open camera
+    sl::Camera zed;
+    sl::InitParameters init_params;
+    init_params.coordinate_units = sl::UNIT::MILLIMETER;
+    init_params.camera_resolution = sl::RESOLUTION::VGA; 
+    init_params.camera_fps = 100;
+    zed.open(init_params); 
+    sl::Resolution cloud_res(320/2, 180/2);
+
+    cudaIpcMemHandle_t my_handle;
+    sl::Mat operationalFrame, curFrame; 
+
+    // Create shared memory region used for passing the GPU data handle between programs
+    shared_memory_object::remove("gpuhandle");
+    shared_memory_object shm (create_only, "gpuhandle", read_write);
+    shm.truncate(1000);
+    mapped_region region(shm, read_write);
+    unsigned char *sharedMemData = static_cast<unsigned char*>(region.get_address());
+    sharedMemData[sizeof(my_handle)+1] = 0;
+
+    bool gotFrame = false;
+
+    while(true) {
+
+        
+        //Mutex lock and move frame to shared memory 
+        if(sharedMemData[sizeof(my_handle)] == 0 && gotFrame) {
+            //Set the obs detector's operational frame to the current frame, so that it doesn't get destructed/overwritten
+            //operational frame needs to take OWNERSHIP of cur frame's memory
+            curFrame.move(operationalFrame);
+            sl::float4* dataGPU = curFrame.getPtr<sl::float4>(sl::MEM::GPU);
+            
+            //create the cudaIpcMemHandle_t
+            checkStatus(cudaIpcGetMemHandle(&my_handle, dataGPU));
+            //convert the cudaIpcMemHandle_t into a primative char array
+            unsigned char handle_buffer[sizeof(my_handle)];
+            memcpy(handle_buffer, (unsigned char *)(&my_handle), sizeof(my_handle));
+            //copy the primative char array bytes of the handle into shared memory
+            memcpy(sharedMemData, handle_buffer, sizeof(my_handle));
+            //write parity bit to high
+            sharedMemData[sizeof(my_handle)] = 1;
+            std::cout << "writing to shared" << std::endl;
+
+        } else { 
+            //Read a new frame 
+            //curFrame = Mat(...)
+            zed.grab();
+            zed.retrieveMeasure(curFrame, sl::MEASURE::XYZRGBA, sl::MEM::GPU, cloud_res); 
+            gotFrame = true;
+            std::cout << "grabbing" << std::endl;
+        }
+    }
+
+    return 0;
+}
