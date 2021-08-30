633934460e4bf6640b054e408392c25d8a7debb1
danieljiang520 <danieldj@umich.edu>
2021-08-30 19:42:44 -0400

    initial commit for map classs

---
+++ simulators/nav/mini_map/map.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+class Map  {
+    public:
+        vector<uint8_t> ReadInputImage(){
+            
+        }
+
+
+
+    private:
+
+};
