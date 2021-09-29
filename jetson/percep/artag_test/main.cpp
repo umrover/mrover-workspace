12ff502eac19c57104f1e1ab9ef70d08e1023ed6
nishukothari <nishuk@umich.edu>
2021-09-29 19:24:02 -0400

    Switched back to a basic Makefile build system

--- jetson/percep/artag_test/main.cpp
+++ jetson/percep/artag_test/main.cpp
@@ -1,7 +1,7 @@
-#include "../artag_detector.hpp"
+//#include "../artag_detector.hpp"
 #include "test_framework.hpp"
 #include <iostream>
 
 int main() {
-    cout << "Testing Build Setup\n";
+    std::cout << "Testing Build Setup\n";
 }
--- jetson/percep/artag_test/test_framework.hpp
+++ jetson/percep/artag_test/test_framework.hpp
@@ -2,4 +2,4 @@
 
 class ArtagTest {
     
-}
+};
