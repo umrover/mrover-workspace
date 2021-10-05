c0ce322fcf388d8cd87a3f9709e54cce935cc21f
michmli <michmli@umich.edu>
2021-10-05 19:54:38 -0400

    cleaned up map.cpp

--- jetson/nav/map.h
+++ jetson/nav/map.h
@@ -3,22 +3,18 @@
 #include <vector>
 #include <string>
 #include <utility>
+#include "rover_msgs/Joystick.hpp"
 
 class Map  {
     public:
-        Map(std::string inputPath); // populates inputMap and navigationMap
+        // populates inputMap and navigationMap
+        Map(const vector< vector <vector<uint8_t> > > &inputMap, uint32_t row_start, uint32_t col_start); 
 
-        std::vector<uint8_t> ReadInputImage(std::string inputPath);
-
-        void updatePosition(double time, double joystickMagnitude, double joystickHeading);
+        vector< vector <vector<uint8_t> > >& updateNavMap(double time, Joystick joystick);
 
         double updateHeading(const double &oldHeading, const double &joystickHeading);
 
         void displayLCM();
-
-        void colorMap();
-
-        void saveMapToImage();
 
         double getHeading() {
             return currentHeading; 
@@ -26,25 +22,34 @@ class Map  {
 
     private:
         struct Square {
-            int x, y; 
-            uint32_t color; 
-        };
+            int32_t x; 
+            int32_t y; 
 
-        std::string inputPath;
+            Square(int32_t x, int32_t y) : x(x), y(y) {}
+        };
+        struct Color {
+            uint8_t r = 128;
+            uint8_t g = 128;
+            uint8_t b = 128;
+        };
         //RGB vector of squares
-        std::vector< std::vector <std::vector<Square> > > inputMap;
-        std::vector< std::vector <std::vector<Square> > > navigationMap;
-        std::pair <int,int> startIndices;
-        std::pair <int,int> currentIndices;
+        std::vector< std::vector <std::vector<uint8_t> > > inputMap;
+        std::vector< std::vector <std::vector<uint8_t> > > navigationMap;
+        Square startIndices;
+        Square currentIndices;
         double xTotalDisplacement;
         double yTotalDisplacement;
         double currentHeading;
         const static double cellLength = 0.4;
         const static double widthTriangle = 10;
+        const static double speed = 1.6; 
 
-        const static int dimension = 1000; 
+        Joystick joystick;
+
+        const static int dimension = 10000; 
         int ContourX[dimension][2];
-        void SetPixel(int x, int y, uint32_t color);
+
+        void SetPixel(int x, int y, Color color);
 
         void ScanLine(int x1, int x2, int y1, int y2); 
 
