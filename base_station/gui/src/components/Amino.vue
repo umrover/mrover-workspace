649bfd13b328c1716e06ea44cd562fcabb575475
ErikLytle <elytle@umich.edu>
2021-10-31 14:11:21 -0400

    Changed amino to remove pump data and switch Heater to toggle switch

--- base_station/gui/src/components/Amino.vue
+++ base_station/gui/src/components/Amino.vue
@@ -1,60 +1,69 @@
 <template>
 <div class="wrap">
     <div>
-      <h3>Amino Testing Controls</h3>
+      <h3>Amino Test Controls</h3>
     </div>
     <div class="box1">
         <label for="site">Choose a site:</label>
         <select v-model = "site" name="site" id="site">
-            <option value="0">Site 0</option>
-            <option value="1">Site 1</option>
-            <option value="2">Site 2</option>
+            <option value="0">Site A</option>
+            <option value="1">Site B</option>
+            <option value="2">Site C</option>
         </select>
     </div>
     <div class="box1" v-if="site == 0">
-        <button v-if="pump0==0" v-on:click="pump0 = 1, setPart(mosfetIDs.perPump0,true)">
-            Pump 0 on
-        </button>
-        <button v-if="pump0==1" v-on:click="pump0 = 0, setPart(mosfetIDs.perPump0,false)">
-            Pump 0 off
-        </button>
-        <button v-if="heater0==0" v-on:click="heater0 = 1, setPart(mosfetIDs.heater0,true)" v-bind:style="{color: color0}">
-            Heater 0 on
+        <label for="toggle_button" :class="{'active': heater0 == 0}" class="toggle__button">
+            <span v-if="heater0 == 0" class="toggle__label" >Heater A On</span>
+            <span v-if="heater0 == 1" class="toggle__label" >Heater A Off</span>
+
+            <input type="checkbox" id="toggle_button" v-model="checkedValue">
+            <span class="toggle__switch" v-if="heater0 == 1" v-on:click="heater0=0,setPart(mosfetIDs.heater0,false)"></span>
+            <span class="toggle__switch" v-if="heater0 == 0" v-on:click="heater0=1,setPart(mosfetIDs.heater0,true)"></span>
+        </label>
+
+        <!-- button v-if="heater0==0" v-on:click="heater0 = 1, setPart(mosfetIDs.heater0,true)" v-bind:style="{color: color0}">
+            Heater A on
         </button>
         <button v-if="heater0==1" v-on:click="heater0 = 0, setPart(mosfetIDs.heater0,false)" v-bind:style="{color: color0}">
-            Heater 0 off
-        </button>
-        <p>Thermistor 0: {{temp0}} C°</p>
+            Heater A off
+        </button -->
+        <p>Thermistor A: {{temp0}} C°</p>
     </div>
     <div class="box1" v-if="site == 1">
-        <button v-if="pump1==0" v-on:click="pump1 = 1, setPart(mosfetIDs.perPump1,true)">
-            Pump 1 on
-        </button>
-        <button v-if="pump1==1" v-on:click="pump1 = 0, setPart(mosfetIDs.perPump1,false)">
-            Pump 1 off
-        </button>
-        <button v-if="heater1==0" v-on:click="heater1 = 1, setPart(mosfetIDs.heater1,true)" v-bind:style="{color: color1}">
-            Heater 1 on
+        <label for="toggle_button" :class="{'active': heater1 == 0}" class="toggle__button">
+            <span v-if="heater1 == 0" class="toggle__label" >Heater B On</span>
+            <span v-if="heater1 == 1" class="toggle__label" >Heater B Off</span>
+
+            <input type="checkbox" id="toggle_button" v-model="checkedValue">
+            <span class="toggle__switch" v-if="heater1 == 1" v-on:click="heater1=0,setPart(mosfetIDs.heater1,false)"></span>
+            <span class="toggle__switch" v-if="heater1 == 0" v-on:click="heater1=1,setPart(mosfetIDs.heater1,true)"></span>
+        </label>
+
+        <!-- button v-if="heater1==0" v-on:click="heater1 = 1, setPart(mosfetIDs.heater1,true)" v-bind:style="{color: color1}">
+            Heater B on
         </button>
         <button v-if="heater1==1" v-on:click="heater1 = 0, setPart(mosfetIDs.heater1,false)" v-bind:style="{color: color1}">
-            Heater 1 off
-        </button>
-        <p>Thermistor 1: {{temp1}} C°</p>
+            Heater B off
+        </button -->
+        <p>Thermistor B: {{temp1}} C°</p>
     </div>
     <div class="box1" v-if="site == 2">
-        <button v-if="pump2==0" v-on:click="pump2 = 1, setPart(mosfetIDs.perPump2,true)">
-            Pump 2 on
-        </button>
-        <button v-if="pump2==1" v-on:click="pump2 = 0, setPart(mosfetIDs.perPump2,false)">
-            Pump 2 off
-        </button>
-        <button v-if="heater2==0" v-on:click="heater2 = 1, setPart(mosfetIDs.heater2,true)" v-bind:style="{color: color2}">
-            Heater 2 on
+        <label for="toggle_button" :class="{'active': heater2 == 0}" class="toggle__button">
+            <span v-if="heater2 == 0" class="toggle__label" >Heater C On</span>
+            <span v-if="heater2 == 1" class="toggle__label" >Heater C Off</span>
+
+            <input type="checkbox" id="toggle_button" v-model="checkedValue">
+            <span class="toggle__switch" v-if="heater2 == 1" v-on:click="heater2=0,setPart(mosfetIDs.heater2,false)"></span>
+            <span class="toggle__switch" v-if="heater2 == 0" v-on:click="heater2=1,setPart(mosfetIDs.heater2,true)"></span>
+        </label>
+
+        <!-- button v-if="heater2==0" v-on:click="heater2 = 1, setPart(mosfetIDs.heater2,true)" v-bind:style="{color: color2}">
+            Heater C on
         </button>
         <button v-if="heater2==1" v-on:click="heater2 = 0, setPart(mosfetIDs.heater2,false)" v-bind:style="{color: color2}">
-            Heater 2 off
-        </button>
-        <p>Thermistor 2: {{temp2}} C°</p>
+            Heater C off
+        </button -->
+        <p>Thermistor C: {{temp2}} C°</p>
     </div>
     <div>
         <input type="checkbox" id="autoShutdown" name="autoShutdown" v-model="autoShutdown">
@@ -76,6 +85,66 @@
     text-align: left;
     vertical-align: top;
     display: inline-block;
+  }
+
+  .toggle__button {
+      vertical-align: middle;
+      user-select: none;
+      cursor: pointer;
+  }
+  .toggle__button input[type="checkbox"] {
+      opacity: 0;
+      position: absolute;
+      width: 1px;
+      height: 1px;
+  }
+  .toggle__button .toggle__switch {
+      display:inline-block;
+      height:12px;
+      border-radius:6px;
+      width:40px;
+      background: #BFCBD9;
+      box-shadow: inset 0 0 1px #BFCBD9;
+      position:relative;
+      margin-left: 10px;
+      transition: all .25s;
+  }
+
+  .toggle__button .toggle__switch::after, 
+  .toggle__button .toggle__switch::before {
+      content: "";
+      position: absolute;
+      display: block;
+      height: 18px;
+      width: 18px;
+      border-radius: 50%;
+      left: 0;
+      top: -3px;
+      transform: translateX(0);
+      transition: all .25s cubic-bezier(.5, -.6, .5, 1.6);
+  }
+
+  .toggle__button .toggle__switch::after {
+      background: #4D4D4D;
+      box-shadow: 0 0 1px #666;
+  }
+  .toggle__button .toggle__switch::before {
+      background: #4D4D4D;
+      box-shadow: 0 0 0 3px rgba(0,0,0,0.1);
+      opacity:0;
+  }
+  .active .toggle__switch {
+    background: #FFEA9B;
+    box-shadow: inset 0 0 1px #FFEA9B;
+  }
+  .active .toggle__switch::after,
+  .active .toggle__switch::before{
+      transform:translateX(40px - 18px);
+  }
+  .active .toggle__switch::after {
+      left: 23px;
+      background: #FFCB05;
+      box-shadow: 0 0 1px #FFCB05;
   }
 </style>
 
