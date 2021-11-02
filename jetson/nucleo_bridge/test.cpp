83fc72d84c64236a78d882fc5b3d43676c08c34f
Quintin <qhdwight@gmail.com>
2021-11-02 19:01:19 -0400

    Make open PLUS return raw angle

--- jetson/nucleo_bridge/test.cpp
+++ jetson/nucleo_bridge/test.cpp
@@ -3,19 +3,19 @@
 #include <vector>
 #include <thread>
 
-#define OFF 0x00, 0, 0
-#define ON 0x0F, 0, 0
-#define OPEN 0x10, 2, 0
-#define OPEN_PLUS 0x1F, 4, 4
-#define CLOSED 0x20, 8, 0
-#define CLOSED_PLUS 0x2F, 8, 4
-#define CONFIG_PWM 0x30, 2, 0
-#define CONFIG_K 0x3F, 12, 0 // maybe rename to CONFIG_KPID since its all 3
-#define GET_K 0x5F, 0, 12
-#define QUAD 0x40, 0, 4
-#define ADJUST 0x4F, 4, 0
-#define ABS_ENC 0x50, 0, 2
-#define LIMIT 0x60, 0, 1
+#define OFF         0x00,   0,  0
+#define ON          0x0F,   0,  0
+#define OPEN        0x10,   2,  0
+#define OPEN_PLUS   0x1F,   4,  4
+#define CLOSED      0x20,   8,  0
+#define CLOSED_PLUS 0x2F,   8,  4
+#define CONFIG_PWM  0x30,   2,  0
+#define CONFIG_K    0x3F,   12, 0 // maybe rename to CONFIG_KPID since its all 3
+#define GET_K       0x5F,   0,  12
+#define QUAD        0x40,   0,  4
+#define ADJUST      0x4F,   4,  0
+#define ABS_ENC     0x50,   0,  2
+#define LIMIT       0x60,   0,  1
 
 #define UINT8_POINTER_T reinterpret_cast<uint8_t *>
 
@@ -84,30 +84,12 @@ uint32_t openPlus(int addr, float speed)
         memcpy(buffer, UINT8_POINTER_T(&speed), 4);
         uint32_t raw_angle;
         I2C::transact(addr, OPEN_PLUS, buffer, UINT8_POINTER_T(&raw_angle));
-        printf("test open plus transaction successful on slave %i, %d \n", addr, raw_angle);
+        printf("test open plus transaction successful on slave %i \n", addr);
         return raw_angle;
     }
     catch (IOFailure &e)
     {
-        fprintf(stderr, "test open plus failed on slave %i \n", addr);
-        return 0;
-    }
-}
-
-uint32_t closedPlus(int addr, float target)
-{
-    try
-    {
-        uint8_t buffer[4];
-        memcpy(buffer, UINT8_POINTER_T(&target), 4);
-        uint32_t raw_angle;
-        I2C::transact(addr, OPEN_PLUS, buffer, UINT8_POINTER_T(&raw_angle));
-        printf("test closed plus transaction successful on slave %i, %d \n", addr, raw_angle);
-        return raw_angle;
-    }
-    catch (IOFailure &e)
-    {
-        fprintf(stderr, "test closed plus failed on slave %i \n", addr);
+        printf("test open plus failed on slave %i \n", addr);
         return 0;
     }
 }
@@ -118,7 +100,6 @@ void configPWM(int addr, int max_speed)
     {
         uint8_t buffer[2];
         memcpy(buffer, UINT8_POINTER_T(&(max_speed)), 2);
-
         I2C::transact(addr, CONFIG_PWM, buffer, nullptr);
         printf("test config pwm transaction successful on slave %i \n", addr);
     }
@@ -298,28 +279,10 @@ void testOpenPlus()
     PRINT_TEST_START
     for (auto address : i2c_address)
     {
-        // openPlus(address, 0.5);
-        // sleep(300);
-        // openPlus(address, 0);
-        // sleep(300);
-        float speed = 0.5f;
-        if (address == 16)
-        {
-            speed = 0.25f;
-        }
-
-        for (int i = 0; i < 3; i++) {
-            openPlus(address, speed);
-            sleep(200);
-        }
-        for (int i = 0; i < 3; i++) {
-            openPlus(address, -speed);
-            sleep(200);
-        }
-        for (int i = 0; i < 5; i++) {
-            openPlus(address, 0.0f);
-            sleep(200);
-        }
+        // sets speed to half speed 
+        uint32_t raw_angle = openPlus(address, 0.5);
+        printf("raw abs angle: %i \n", raw_angle);
+        std::this_thread::sleep_for(std::chrono::milliseconds(5000));
     }
     PRINT_TEST_END
 }
