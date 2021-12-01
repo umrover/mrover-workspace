5a49e75663da1a856a6388ecccd1c2301834cb72
Seanathan546 <seanaa@umich.edu>
2021-12-01 18:09:04 -0500

    changed obstacle size

--- jetson/percep_obs_detect/tools/SimpleSceneGenerator.cpp
+++ jetson/percep_obs_detect/tools/SimpleSceneGenerator.cpp
@@ -232,7 +232,7 @@ int main() {
 
     //Init all obstacles to be added to the scene and push them to ObsReturn.obs
     //Obstacle <name> = {minX, maxX, minY, maxY, minZ, maxZ};
-    Obstacle one = { 0, 10, 0, 10, 0, 10 };
+    Obstacle one = { 0, 50, 0, 50, 0, 50 };
     objects.obs.push_back(one);
 
     //Init an object of PcGenerator Class using one of the following constructors 
@@ -248,6 +248,6 @@ int main() {
 
 
     PCDWriter writer;
-    writer.writeCloud("pc.pcd", cloud, 1, 1);
+    writer.writeCloud("pc.pcd", cloud, 100, 100);
 
 }
