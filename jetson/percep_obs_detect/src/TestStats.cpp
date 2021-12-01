14a31a84ed4d60520799a78aeaa562cbab97bf71
chrsdavis <84039785+chrsdavis@users.noreply.github.com>
2021-12-01 18:04:17 -0500

    added file

--- jetson/percep_obs_detect/src/TestStats.cpp
+++ jetson/percep_obs_detect/src/TestStats.cpp
@@ -4,6 +4,7 @@
 #include <numeric>
 #include <iomanip>
 #include <algorithm>
+#include <fstream>
 
 TestStats::TestStats()
 {
@@ -33,31 +34,32 @@ void TestStats::analyze_test()
 
 void TestStats::print() //prints out all info
 {
+  std::ofstream of("jetson/percep_obs_detect/unit_test.txt");
     /* Output Log Format */
-    //std::cout.setprecision(4);
-    std::cout << "Evaluating GPU Cloud #[NUMBER]\n";
-    std::cout << "GPU Obstacle Detection Runtime: [TIME]\n";
-    std::cout << "Number of Detected Obstacles: [NUMDET]\n";
-    std::cout << "Number of True Obstacles: [NUMTRUE]\n";
-    std::cout << "\t(What Should be detected)\n";
-    std::cout << "Percent Truth Detected: [TRUE]\n";
-    std::cout << "\t(Total intersection divided by total true area)\n\tShould be close to 1\n";
-    std::cout << "False Positive over True Volume: [FALSE]\n";
-    std::cout << "\t(Total detected volume not contained in set of true obstacles)\n\t(Divided by total true volume)\n";
-    std::cout << "Mean % Truth: [TRUEPCT]\n";
-    std::cout << "Average volume of a truth detected\n";
-    std::cout << "Obstacle #[NUMBER] % detected: [PERCENT]\n";
+    //of.setprecision(4);
+    of << "Evaluating GPU Cloud #[NUMBER]\n";
+    of << "GPU Obstacle Detection Runtime: [TIME]\n";
+    of << "Number of Detected Obstacles: [NUMDET]\n";
+    of << "Number of True Obstacles: [NUMTRUE]\n";
+    of << "\t(What Should be detected)\n";
+    of << "Percent Truth Detected: [TRUE]\n";
+    of << "\t(Total intersection divided by total true area)\n\tShould be close to 1\n";
+    of << "False Positive over True Volume: [FALSE]\n";
+    of << "\t(Total detected volume not contained in set of true obstacles)\n\t(Divided by total true volume)\n";
+    of << "Mean % Truth: [TRUEPCT]\n";
+    of << "Average volume of a truth detected\n";
+    of << "Obstacle #[NUMBER] % detected: [PERCENT]\n";
 
     for (size_t i = 0; i < iot.size(); i++) // Loop through each cloud
     {
-        std::cout << "\n–––––––––––––––––––––––\n–––––––––––––––––––––––\n\n";
-        std::cout << "Evaluating GPU Cloud #" << i << "\n";
-        std::cout << "GPU Obstacle Detection Runtime: " << times[i] << "\n";
-        std::cout << "Number of Detected Obstacles: " << num_det_obs[i] << "\n";
-        std::cout << "Number of True Obstacles: " << num_true_obs[i] << "\n";
-        std::cout << "Percent Truth Detected: " << iot[i] * 100 << "\n";
-        std::cout << "False Positive over True Volume: " << fot[i] << "\n";
-        std::cout << "Mean % Truth: " <<
+        of << "\n–––––––––––––––––––––––\n–––––––––––––––––––––––\n\n";
+        of << "Evaluating GPU Cloud #" << i << "\n";
+        of << "GPU Obstacle Detection Runtime: " << times[i] << "\n";
+        of << "Number of Detected Obstacles: " << num_det_obs[i] << "\n";
+        of << "Number of True Obstacles: " << num_true_obs[i] << "\n";
+        of << "Percent Truth Detected: " << iot[i] * 100 << "\n";
+        of << "False Positive over True Volume: " << fot[i] << "\n";
+        of << "Mean % Truth: " <<
             std::accumulate(discrete_truths[i].begin(),
                 discrete_truths[i].end(),
                 static_cast<float>(0),
@@ -65,7 +67,7 @@ void TestStats::print() //prints out all info
                     return a + b / discrete_truths[i].size();
                 }) * 100 << "\n";
         for (size_t j = 0; j < discrete_truths[i].size(); ++j) {
-            std::cout << "Obstacle #" << j << "% detected: " << discrete_truths[i][j] * static_cast<float>(100) << "\n";
+            of << "Obstacle #" << j << "% detected: " << discrete_truths[i][j] * static_cast<float>(100) << "\n";
         }
     }
 }//End print()
