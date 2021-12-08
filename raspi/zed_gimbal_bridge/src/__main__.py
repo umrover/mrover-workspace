21896f3e56d98c14de4740e751f90c958e1f34db
Guthrie Tabios <gutstabios@gmail.com>
2021-12-08 11:22:34 -0500

    Change from Python 3.6 to 3.7 and pass linter, but still not tested on pi

--- raspi/zed_gimbal_bridge/src/__main__.py
+++ raspi/zed_gimbal_bridge/src/__main__.py
@@ -3,23 +3,24 @@ import asyncio
 import math
 import moteus
 import moteus_pi3hat
+from rover_common.aiohelper import run_coroutines
 from rover_common import aiolcm
 from rover_msgs import ZedGimbalPosition
 
 lcm_ = aiolcm.AsyncLCM()
 
-
-f = moteus_pi3hat.Pi3HatRouter( 
+f = moteus_pi3hat.Pi3HatRouter(
     servo_bus_map={...},
 )
+
 c = moteus.Controller(transport=f, id=1)
-await c.set_stop()
 
 
 def zed_gimbal_position_callback(channel, msg):
     desired_position_degrees = ZedGimbalPosition.decode(msg)
     desired_position_revolutions = desired_position_degrees / 360.0
-    c.make_position(position=math.nan, velocity=0.2, maximum_torque=0.3, stop_position=desired_position_revolutions, watchdog_timeout=10, query=True)
+    c.make_position(position=math.nan, velocity=0.2, maximum_torque=0.3,
+                    stop_position=desired_position_revolutions, watchdog_timeout=10, query=True)
 
 
 async def publish_zed_gimbal_position():
@@ -34,7 +35,10 @@ async def publish_zed_gimbal_position():
 
         await asyncio.sleep(0.5)
 
-def main():
+
+async def main():
+    await c.set_stop()
+
     lcm_.subscribe("/zed_gimbal_data", zed_gimbal_position_callback)
 
     run_coroutines(lcm_.loop(), publish_zed_gimbal_position())
