63e0f7980d8aa1814929a1009c35fa6f3ebcce3e
Seanathan546 <seanaa@umich.edu>
2022-01-09 13:35:45 -0500

    edited obstacle dimensions

--- jetson/percep_obs_detect/tools/SimpleSceneGenerator.cpp
+++ jetson/percep_obs_detect/tools/SimpleSceneGenerator.cpp
@@ -232,7 +232,7 @@ int main() {
 
     //Init all obstacles to be added to the scene and push them to ObsReturn.obs
     //Obstacle <name> = {minX, maxX, minY, maxY, minZ, maxZ};
-    Obstacle one = { 0, 20, 0, 20, 0, 20 };
+    Obstacle one = { 0, 80, 0, 80, 0, 80 };
     objects.obs.push_back(one);
 
     //Init an object of PcGenerator Class using one of the following constructors 
@@ -248,6 +248,6 @@ int main() {
 
 
     PCDWriter writer;
-    writer.writeCloud("pc.pcd", cloud, 50, 50);
+    writer.writeCloud("pc.pcd", cloud, 100, 100);
 
 }
