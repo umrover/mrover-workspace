c103b2ea12c06233a340609070519b5bcae928bf
Seanathan546 <seanaa@umich.edu>
2022-01-09 13:59:22 -0500

    changed dimensions again

--- jetson/percep_obs_detect/tools/SimpleSceneGenerator.cpp
+++ jetson/percep_obs_detect/tools/SimpleSceneGenerator.cpp
@@ -232,7 +232,7 @@ int main() {
 
     //Init all obstacles to be added to the scene and push them to ObsReturn.obs
     //Obstacle <name> = {minX, maxX, minY, maxY, minZ, maxZ};
-    Obstacle one = { 0, 30, 0, 30, 0, 30 };
+    Obstacle one = { 0, 1000, 0, 1000, 0, 1000 };
     objects.obs.push_back(one);
 
     //Init an object of PcGenerator Class using one of the following constructors 
