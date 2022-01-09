708b38c79f2ab10769655c258de4fdb963c7e55b
Seanathan546 <seanaa@umich.edu>
2022-01-09 13:29:39 -0500

    changed dimensions of cloud

--- jetson/percep_obs_detect/src/obs-detector.cu
+++ jetson/percep_obs_detect/src/obs-detector.cu
@@ -170,7 +170,7 @@ void ObsDetector::test_input_file()
   EuclideanClusterExtractor::ObsReturn objects;
   //Init all obstacles to be added to the scene and push them to ObsReturn.obs
   //Obstacle <name> = {minX, maxX, minY, maxY, minZ, maxZ};
-  EuclideanClusterExtractor::Obstacle one = { 0, 50, 0, 50, 0, 50 };
+  EuclideanClusterExtractor::Obstacle one = { 0, 20, 0, 20, 0, 20 };
   objects.obs.push_back(one);
   truths.push_back(objects);
   test(raw_data, truths);
