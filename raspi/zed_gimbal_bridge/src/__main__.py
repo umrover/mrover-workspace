fdadcdbc56fe267498573278ea9948fe0fe733c3
Guthrie Tabios <gutstabios@gmail.com>
2022-01-10 12:22:03 -0500

    [Untested] Add code to stop after setting position

--- raspi/zed_gimbal_bridge/src/__main__.py
+++ raspi/zed_gimbal_bridge/src/__main__.py
@@ -17,7 +17,7 @@ c = moteus.Controller(transport=f, id=1)
 desired_position_revolutions = 0
 
 
-def zed_gimbal_position_callback(channel, msg):
+async def zed_gimbal_position_callback(channel, msg):
     zed_struct = ZedGimbalPosition.decode(msg)
     desired_position_degrees = zed_struct.angle
 
@@ -28,8 +28,10 @@ def zed_gimbal_position_callback(channel, msg):
 
     global desired_position_revolutions
     desired_position_revolutions = desired_position_degrees / 360.0
-    c.make_position(position=math.nan, velocity=0.2, maximum_torque=0.3,
-                    stop_position=desired_position_revolutions, watchdog_timeout=10, query=True)
+    await c.set_position(position=math.nan, velocity=0.2, maximum_torque=0.3,
+                         stop_position=desired_position_revolutions, watchdog_timeout=70, query=False)
+
+    await c.set_stop()
 
 
 async def publish_zed_gimbal_position():
@@ -40,10 +42,12 @@ async def publish_zed_gimbal_position():
 
         position_revolutions = state.values[moteus.Register.POSITION]
         position_degrees = 360.0 * position_revolutions
+
         if position_degrees > 180:
             position_degrees = 180
         elif position_degrees < -180:
             position_degrees = -180
+
         zed_struct = ZedGimbalPosition()
         zed_struct.angle = position_degrees
         lcm_.publish('/zed_gimbal_data', zed_struct.encode())
@@ -51,7 +55,9 @@ async def publish_zed_gimbal_position():
         await asyncio.sleep(0.5)
 
 
-def main():
+async def main():
+
+    await c.set_stop()
 
     lcm_.subscribe("/zed_gimbal_cmd", zed_gimbal_position_callback)
 
