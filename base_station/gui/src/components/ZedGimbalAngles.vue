21532d6b9e788ca7d6ac559b028557b14b00db51
Cameron Tressler <camtressler@gmail.com>
2022-01-15 16:15:35 -0500

    Update Auton GUI (#839)
    * Auton GUI Redesign, raw data, waypoint editor fixes etc.
    
    * Revert "[nav-sim] Add noise to simulated sensors (#545)" (#581)
    
    This reverts commit 918b9d6298cb331fdc31ea9b113c37c6d02523dc.
    
    * Added Chlorphyll UV button, moved zero encoders button. Removed waypoint editor.
    
    * SA Changes and Auton waypoint editor grid
    
    * Auton fixes, drivel velocity data on all guis
    
    * amino.vue
    
    * Ammonia component w/ auto logic, some changes to mosfet lcm logic in chlorophyll component
    
    * Save work on generatereport
    
    * cleaned up loose vue files and added mosfetID object to SATask.vue
    
    * Fixed mosfetIDs object usage in child vue components
    
    * Added Generate Report button to SA GUI
    
    * Added lcm channel for spectral data in LCMEcho and SATask
    
    * Rearranged waypoint editor to make space for zed gimbal angles
    
    * [nav] Add Navigation README Documentation (#761)
    
    Add README documentation to guide understanding of navigation codebase.
    
    * Moved Raw data and added Zed Gimbal Angles to auton gui
    
    * Removed jetson/nav/README.md because of PR issue
    
    Co-authored-by: vagrant <vagrant@mrover-devbox>
    Co-authored-by: aswarner <43736653+aswarner@users.noreply.github.com>
    Co-authored-by: jnnanni <jnnanni@umich.edu>
    Co-authored-by: ErikLytle <elytle@umich.edu>

---
+++ base_station/gui/src/components/ZedGimbalAngles.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class = "wrap">
+        <p>Zed Gimbal Angle: {{angle}}°</p>
+    </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      angle: 0
+    }
+  },
+  created: function () {
+    this.$parent.subscribe('/zed_gimbal_data', (msg) => {
+      this.angle = msg.angle;
+    })
+  }
+}
+</script>
+
+<style scoped>
+   
+  .wrap {
+      padding: 0px;
+      padding-left: 5px;
+      padding-right: 5px;
+      border: none;
+      margin: 0px 0px 0px 0px;
+      grid-template-rows: auto;
+  }
+
+  .wrap p {
+    display: inline;
+  }
+
+</style>
