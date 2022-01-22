fa4aa44b39ae5f29068d471b49035bad911a7f13
qhdwight <qhdwight@gmail.com>
2022-01-22 17:59:13 -0500

    Fix sl::Mat memory owning bug

--- jetson/percep_obs_detect/src/obs-detector.cu
+++ jetson/percep_obs_detect/src/obs-detector.cu
@@ -72,8 +72,9 @@ void ObsDetector::setupParamaters(std::string parameterFile) {
 
 void ObsDetector::update() {
     GPU_Cloud pc;
+    // sl::Mat frame is outside of if statement scope since it owns the memory and we don't want that to de-allocate sooner than expected
+    sl::Mat frame(cloud_res, sl::MAT_TYPE::F32_C4, sl::MEM::GPU);
     if (source == DataSource::ZED) {
-        sl::Mat frame(cloud_res, sl::MAT_TYPE::F32_C4, sl::MEM::GPU);
         zed.grab();
         zed.retrieveMeasure(frame, sl::MEASURE::XYZRGBA, sl::MEM::GPU, cloud_res);
         getRawCloud(pc, frame);
