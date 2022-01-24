25a80728b03d81a877032f4ba533d231012ba511
Joshua Siegel <jsiegel2202@gmail.com>
2022-01-23 20:16:00 -0500

    testing changes with arduino

--- jetson/imu_channel/src/__main__.py
+++ jetson/imu_channel/src/__main__.py
@@ -1,12 +1,14 @@
-# accel_x accel_y accel_z gyro_x gyro_y gyro_z bearing
 import lcm
 import serial
+from serial import SerialException
 from rover_msgs import IMUData
 import math
 
 
 def main():
     lcm_ = lcm.LCM()
+    ser = serial.Serial('/dev/ttyACM0', 115200)
+    attempts = 0
 
     while (1):
         imu_data = IMUData()
@@ -17,9 +19,18 @@ def main():
         imu_data.pitch_rad = 0
         imu_data.yaw_rad = 0
 
-        with serial.Serial('/dev/ttyS1', 115200, timeout=1) as ser:
+        try:
             line = ser.readline()
-            vals = [float(val.strip()) for val in line.split()]
+            attempts = 0
+        except SerialException:
+            attempts += 1
+            if attempts > 100:
+                print("unable to read from serial port...ending program")
+                break
+
+        vals = [float(val.strip()) for val in line.split()]
+        print("vals: ", vals)
+        try:
             imu_data.accel_x_g = vals[0]
             imu_data.accel_y_g = vals[1]
             imu_data.accel_z_g = vals[2]
@@ -31,6 +42,8 @@ def main():
             imu_data.bearing_deg = vals[6]
 
             lcm_.publish("/imu_data", imu_data.encode())
+        except IndexError:
+            print("incomplete msg")
 
 
 if __name__ == "__main__":
