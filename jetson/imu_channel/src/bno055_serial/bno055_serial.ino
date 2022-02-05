026c970f0f172c5587f40d5a229d4e63873b2deb
Riley Bridges <rlybrdgs@gmail.com>
2022-02-04 22:59:04 -0500

    cleaned up BNO IMU driver

--- jetson/imu_channel/src/bno055_serial/bno055_serial.ino
+++ jetson/imu_channel/src/bno055_serial/bno055_serial.ino
@@ -9,176 +9,67 @@
    To use this driver you will also need to download the Adafruit_Sensor
    library and include it in your libraries folder.
 
-   You should also assign a unique ID to this sensor for use with
-   the Adafruit Sensor API so that you can identify this particular
-   sensor in any data logs, etc.  To assign a unique ID, simply
-   provide an appropriate value in the constructor below (12345
-   is used by default in this example).
-
    Connections
    ===========
    Connect SCL to SCL pin (analog 5 on Arduino UNO)
    Connect SDA to SDA pin (analog 4 on Arduino UNO)
-   Connect VDD to 3-5V DC (depending on your board's logic level)
-   Connect GROUND to common ground
-
-   History
-   =======
-   2015/MAR/03  - First release (KTOWN)
-   2015/AUG/27  - Added calibration and system status helpers
+   Connect VIN to 3-5V DC (depending on your board's logic level)
+   Connect GND to common ground
 */
 
 /* Set the delay between fresh samples */
 #define BNO055_SAMPLERATE_DELAY_MS (100)
+#define I2C_ADDRESS 0x28
+#define SENSOR_ID 55
 
 #define G 9.81
 
-// Check I2C device address and correct line below (by default address is 0x29 or 0x28)
-//                                   id, address
-Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28);
+Adafruit_BNO055 bno = Adafruit_BNO055(SENSOR_ID, I2C_ADDRESS);
 
-/**************************************************************************/
-/*
-    Displays some basic information on this sensor from the unified
-    sensor API sensor_t type (see Adafruit_Sensor for more information)
-*/
-/**************************************************************************/
-void displaySensorDetails(void)
-{
-  sensor_t sensor;
-  bno.getSensor(&sensor);
-  Serial.println("------------------------------------");
-  Serial.print  ("Sensor:       "); Serial.println(sensor.name);
-  Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
-  Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
-  Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println(" xxx");
-  Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println(" xxx");
-  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" xxx");
-  Serial.println("------------------------------------");
-  Serial.println("");
-  delay(500);
-}
-
-/**************************************************************************/
-/*
-    Display some basic info about the sensor status
-*/
-/**************************************************************************/
-void displaySensorStatus(void)
-{
-  /* Get the system status values (mostly for debugging purposes) */
-  uint8_t system_status, self_test_results, system_error;
-  system_status = self_test_results = system_error = 0;
-  bno.getSystemStatus(&system_status, &self_test_results, &system_error);
-
-  /* Display the results in the Serial Monitor */
-  Serial.println("");
-  Serial.print("System Status: 0x");
-  Serial.println(system_status, HEX);
-  Serial.print("Self Test:     0x");
-  Serial.println(self_test_results, HEX);
-  Serial.print("System Error:  0x");
-  Serial.println(system_error, HEX);
-  Serial.println("");
-  delay(500);
-}
-
-/**************************************************************************/
-/*
-    Display sensor calibration status
-*/
-/**************************************************************************/
-void displayCalStatus(void)
-{
-  /* Get the four calibration values (0..3) */
-  /* Any sensor data reporting 0 should be ignored, */
-  /* 3 means 'fully calibrated" */
-  uint8_t system, gyro, accel, mag;
-  system = gyro = accel = mag = 0;
-  bno.getCalibration(&system, &gyro, &accel, &mag);
-
-  /* The data should be ignored until the system calibration is > 0 */
-  Serial.print("\t");
-  if (!system)
-  {
-    Serial.print("! ");
-  }
-
-  /* Display the individual values */
-  Serial.print("Sys:");
-  Serial.print(system, DEC);
-  Serial.print(" G:");
-  Serial.print(gyro, DEC);
-  Serial.print(" A:");
-  Serial.print(accel, DEC);
-  Serial.print(" M:");
-  Serial.print(mag, DEC);
-}
-
-/**************************************************************************/
-/*
-    Arduino setup function (automatically called at startup)
-*/
-/**************************************************************************/
 void setup(void)
 {
   Serial.begin(115200);
-  Serial.println("Orientation Sensor Test"); Serial.println("");
 
-  /* Initialise the sensor */
-  if(!bno.begin())
+  // init the IMU
+  if (!bno.begin())
   {
     /* There was a problem detecting the BNO055 ... check your connections */
     Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
-    while(1);
+    while (1);
   }
-
   delay(1000);
 
-  /* Display some basic information on this sensor */
-  displaySensorDetails();
-
-  /* Optional: Display current status */
-  displaySensorStatus();
-
+  // use the external crystal clock for better accuracy
   bno.setExtCrystalUse(true);
 }
 
-/**************************************************************************/
-/*
-    Arduino loop function, called once 'setup' is complete (your own code
-    should go here)
-*/
-/**************************************************************************/
 void loop(void)
 {
-  /* Get a new sensor event */
+  // get orientation, acceleration, and gyroscope data,
+  // each from their own sensor event
   sensors_event_t orientationData, accelData, gyroData;
   bno.getEvent(&orientationData, Adafruit_BNO055::VECTOR_EULER);
   bno.getEvent(&accelData, Adafruit_BNO055::VECTOR_ACCELEROMETER);
   bno.getEvent(&gyroData, Adafruit_BNO055::VECTOR_GYROSCOPE);
 
-  /* Display the floating point data */
-
-  // Serial.print("accel X: ");
+  // send the floating point data over serial in the following format:
+  // accel_x accel_y accel_z gyro_x gyro_y gyro_z bearing
   Serial.print(accelData.acceleration.x / G, 4);
-  // Serial.print("\taccel Y: ");
   Serial.print(" ");
   Serial.print(accelData.acceleration.y / G, 4);
-  // Serial.print("\taccel Z: ");
   Serial.print(" ");
   Serial.print(accelData.acceleration.z / G, 4);
 
-  // // Serial.print("gyro X: ");
   // Serial.print(" ");
   // Serial.print(gyroData.gyro.x * (180/PI), 4);
-  // // Serial.print("\tgyro Y: ");
   // Serial.print(" ");
   // Serial.print(gyroData.gyro.y * (180/PI), 4);
-  // // Serial.print("\tgyro Z: ");
   // Serial.print(" ");
   // Serial.print(gyroData.gyro.z * (180/PI), 4);
 
+  // currently, instead of printing gyro data,
+  // we are sending the gyro, accel, and mag calibration values in its place
+  // if this continues to be necessary, we will change the LCM struct and the driver
   uint8_t system, gyro, accel, mag;
   system = gyro = accel = mag = 0;
   bno.getCalibration(&system, &gyro, &accel, &mag);
@@ -189,19 +80,12 @@ void loop(void)
   Serial.print(" ");
   Serial.print(mag, DEC);
 
-  // Serial.print("\tbearing X: ");
+  // print the bearing around the axis perpendicular to the board,
+  // for some reason it is labeled as the x axis for orientation data,
+  // should be the Z axis
   Serial.print(" ");
   Serial.println(orientationData.orientation.x, 4);
 
-  /* Optional: Display calibration status */
-  // displayCalStatus();
-
-  /* Optional: Display sensor status (debug only) */
-  //displaySensorStatus();
-
-  /* New line for the next sample */
-  // Serial.println("");
-
-  /* Wait the specified delay before requesting nex data */
+  // wait the specified delay before requesting nex data
   delay(BNO055_SAMPLERATE_DELAY_MS);
 }
