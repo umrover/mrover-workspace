7f7f99533dfdc70c6915179a478224cf3c2d28de
Carmen Giger <cgiger00@gmail.com>
2022-02-08 12:24:59 -0500

    ID update

--- jetson/odrive_bridge/src/__main__.py
+++ jetson/odrive_bridge/src/__main__.py
@@ -224,7 +224,7 @@ class OdriveBridge(object):
         # odrive 1 --> middle motors
         # odrive 2 --> back motors
 
-        odrives = ["206E37635753", "335B36563539", "335536553539"]
+        odrives = ["335D36623539", "335B36563539", "335536553539"]
 
         id = odrives[legal_controller]
 
