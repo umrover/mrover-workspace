f8889c21507e5f3e366ddee0431abfd99e843383
tabiosg <gutstabios@gmail.com>
2022-02-11 01:54:47 -0500

    Clean up code - change variable names and change function arguments

--- raspi/zed_gimbal_bridge/src/__main__.py
+++ raspi/zed_gimbal_bridge/src/__main__.py
@@ -1,4 +1,3 @@
-# import lcm
 import asyncio
 import math  # needed for math.nan
 import moteus
@@ -9,15 +8,18 @@ from rover_msgs import ZedGimbalPosition
 
 lcm_ = aiolcm.AsyncLCM()
 
+# assume connection to JC1
 f = moteus_pi3hat.Pi3HatRouter(
         servo_bus_map={1: [1]},
 )
 
 c = moteus.Controller(transport=f, id=1)
-desired_revolutions = 0
+desired_rev = 0
 
 
-def restrict_angle(input_angle, lower_bound, upper_bound):
+def restrict_angle(input_angle):
+    lower_bound = -180
+    upper_bound = 180
     if input_angle > upper_bound:
         return upper_bound
     elif input_angle < lower_bound:
@@ -27,44 +29,39 @@ def restrict_angle(input_angle, lower_bound, upper_bound):
 
 def zed_gimbal_position_callback(channel, msg):
     zed_struct = ZedGimbalPosition.decode(msg)
-    desired_degrees = zed_struct.angle
-    desired_degrees = restrict_angle(desired_degrees, -180, 180)
-    global desired_revolutions
-    desired_revolutions = desired_degrees / 360.0
-
-    c.make_position(position=math.nan, velocity=0.2, maximum_torque=0.3,
-                    stop_position=desired_revolutions, watchdog_timeout=10, query=True)
+    degrees = zed_struct.angle
+    degrees = restrict_angle(degrees)
+    global desired_rev
+    desired_rev = degrees / 360.0
 
 
 async def publish_zed_gimbal_position():
     while (True):
-        global desired_revolutions
+        global desired_rev
 
         state = await c.set_position(position=math.nan, velocity=0.2, maximum_torque=0.3,
-                                     stop_position=desired_revolutions, watchdog_timeout=70, query=True)
+                                     stop_position=desired_rev, watchdog_timeout=70, query=True)
 
         fault_value = state.values[moteus.Register.FAULT]
         if fault_value != 0:
-            print(fault_value)
+            print("Error #" + str(fault_value))
             await c.set_stop()
             await asyncio.sleep(0.5)
             continue
 
-        pos_revolutions = state.values[moteus.Register.POSITION]
-        pos_degrees = 360.0 * pos_revolutions
-        pos_degrees = restrict_angle(pos_degrees, -180, 180)
+        rev = state.values[moteus.Register.POSITION]
+        degrees = 360.0 * rev
+        degrees = restrict_angle(degrees)
 
         zed_struct = ZedGimbalPosition()
-        zed_struct.angle = pos_degrees
+        zed_struct.angle = degrees
         lcm_.publish('/zed_gimbal_data', zed_struct.encode())
 
         await asyncio.sleep(0.5)
 
 
 def main():
-
     lcm_.subscribe("/zed_gimbal_cmd", zed_gimbal_position_callback)
-
     run_coroutines(lcm_.loop(), publish_zed_gimbal_position())
 
 
