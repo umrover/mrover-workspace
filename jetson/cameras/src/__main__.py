f551d5ffdb26a8dbbc6e7cce2c1730d06afe0193
tabiosg <gutstabios@gmail.com>
2022-02-14 19:09:37 -0500

    Camera test

--- jetson/cameras/src/__main__.py
+++ jetson/cameras/src/__main__.py
@@ -1,7 +1,9 @@
 from rover_common import aiolcm
 from rover_common.aiohelper import run_coroutines
 from rover_msgs import Cameras
-import subprocess
+import gi
+gi.require_version("Gst", "1.0")
+from gi.repository import Gst # noqa
 
 lcm_ = aiolcm.AsyncLCM()
 pipeline = [None, None, None, None, None, None, None, None]
@@ -12,18 +14,22 @@ ports = [0, 1, 3, 4, 5, 6, 7, 8]
 
 def start_pipeline(index):
     global pipeline
-    # this program assumes that the jetson has git cloned video viewer
-    cmd = ("./../jetson-utils/build/aarch64/bin/video-viewer /dev/video" + str(video_names[index]) +
-           " rtp://10.0.0.1:500" + str(ports[index]) + " --headless")
+    pipeline_string = ("v4l2src /dev/video" + str(video_names[index]) +
+                       "! videoscale ! videoconvert ! x264enc tune=zerolatency "
+                       "bitrate=500 speed-preset=superfast ! rtph264pay ! "
+                       "udpsink host=10.0.0.1 port=500" + str(ports[index]))
 
-    pipeline[index] = subprocess.Popen("exec " + cmd, stdout=subprocess.PIPE, shell=True)
+    if pipeline[index] is None:
+        pipeline[index] = Gst.parse_launch(pipeline_string)
+
+    pipeline[index].set_state(Gst.State.PLAYING)
     print("Playing camera " + str(index) + " pipeline.")
 
 
 def stop_pipeline(index):
     global pipeline
 
-    pipeline[index].kill()
+    pipeline[index].set_state(Gst.State.PAUSED)
     print("Stopping camera " + str(index) + " pipeline.")
 
 
