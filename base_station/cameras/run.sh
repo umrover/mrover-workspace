40b80dcfebaa39e6becd8badbfa472911c58be61
tabiosg <gutstabios@gmail.com>
2022-02-15 20:11:59 -0500

    Remove backslashes and correct pipeline

--- base_station/cameras/run.sh
+++ base_station/cameras/run.sh
@@ -1,8 +1,8 @@
 for CAM_NUM_0 in {0..1}
 do
-    gst-launch-1.0 udpsrc port=500${CAM_NUM_0} ! \"application/x-rtp, encoding-name=(string)H264, payload=96\" ! rtph264depay ! decodebin ! videoconvert ! autovideosink
+    gst-launch-1.0 udpsrc port=500${CAM_NUM_0} ! "application/x-rtp, encoding-name=(string)H264, payload=96" ! rtph264depay ! decodebin ! videoconvert ! autovideosink
 done
 for CAM_NUM_1 in {3..8}
 do
-gst-launch-1.0 udpsrc port=500${CAM_NUM_1} ! \"application/x-rtp, encoding-name=(string)H264, payload=96\" ! rtph264depay ! decodebin ! videoconvert ! autovideosink
+gst-launch-1.0 udpsrc port=500${CAM_NUM_1} ! "application/x-rtp, encoding-name=(string)H264, payload=96" ! rtph264depay ! decodebin ! videoconvert ! autovideosink
 done
