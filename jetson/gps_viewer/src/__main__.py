7f89b0f3e5bfc502ed310b7b843d449b94ac4a72
qhdwight <qhdwight@gmail.com>
2022-02-16 14:42:47 -0500

    Fix plotting

--- jetson/gps_viewer/src/__main__.py
+++ jetson/gps_viewer/src/__main__.py
@@ -2,6 +2,8 @@ import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
 import lcm
+from rover_common import aiolcm
+from rover_common.aiohelper import run_coroutines
 
 from rover_msgs import Odometry, IMUData, GPS
 
@@ -49,44 +51,47 @@ class GPSRequestHandler(BaseHTTPRequestHandler):
 def odometry_callback(channel, msg):
     global __lcm, __data
     msg = Odometry.decode(msg)
-    print(msg.val)
     __data["odo"]["longitude"] = msg.longitude_min
     __data["odo"]["latitude"] = msg.latitude_min
-    __lcm.handle()
 
 
 def imu_callback(channel, msg):
     global __lcm, __data
+    print("penis")
     msg = IMUData.decode(msg)
-    print(msg.val)
     __data["heading"] = msg.bearing_deg
-    __lcm.handle()
 
 
 def gps_callback(channel, msg):
     global __lcm, __data
     msg = GPS.decode(msg)
-    print(msg.val)
-    __data["gps"]["longitude"] = msg.longitude_min
-    __data["gps"]["latitude"] = msg.latitude_min
+    print(msg.longitude_min)
+    print(msg.latitude_min)
+    __data["gps"]["longitude"] = msg.longitude_deg + msg.longitude_min / 60
+    __data["gps"]["latitude"] = msg.latitude_deg + msg.latitude_min / 60
 
 
 def make_handler(request, client_address, server):
-    global __data
+    global __data, __lcm
     # __data["gps"]["longitude"] += 0.001
     # __data["odo"]["latitude"] += 0.001
+    while __lcm.handle_timeout(1):
+        pass
     return GPSRequestHandler(__data, request, client_address, server)
 
 
 def main():
     global __lcm
-    __lcm = lcm.LCM()
-    __lcm.subscribe("/Odometry", odometry_callback)
-    __lcm.subscribe("/IMUData", imu_callback)
-    __lcm.subscribe("/GPS", imu_callback)
+    __lcm = lcm.LCM() #aiolcm.AsyncLCM()
+    __lcm.subscribe("/odometry", odometry_callback)
+    __lcm.subscribe("/imu_data", imu_callback)
+    __lcm.subscribe("/gps", gps_callback)
+    # run_coroutines(__lcm.loop())
     with HTTPServer(("0.0.0.0", 8080), make_handler) as web_server:
         try:
             web_server.serve_forever()
+            #__lcm.loop()
+            #run_coroutines(web_server.serve_forever(), __lcm.loop())
         except KeyboardInterrupt:
             pass
 
