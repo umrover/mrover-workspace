df5f48f9875f9b0e0a3795f90cf7dd8b1753f2f7
tabiosg <gutstabios@gmail.com>
2022-03-06 10:49:40 -0500

    Update README, ansible scritps, and array bug

--- base_station/cameras/run.py
+++ base_station/cameras/run.py
@@ -1,13 +1,11 @@
 import subprocess
 
-ports = [3, 4, 5, 6, 7, 8, 9, 10, 11]
+ports = [3, 4, 5, 6, 7, 8, 9, 10]
 pipeline = [None, None, None, None, None, None, None, None]
 
 for i, val in enumerate(ports):
     cmd = "gst-launch-1.0 udpsrc port=500" + str(val) + " ! \"application/x-rtp, encoding-name=(string)H264, payload=96\" ! rtph264depay ! decodebin ! videoconvert ! autovideosink"
     pipeline[i] = subprocess.Popen("exec " + cmd, stdout=subprocess.PIPE, shell=True)
 
-shutdown = input("Type yes to kill the program.")
-if shutdown == "yes":
-    for i, val in enumerate(pipeline):
-        val.kill()
+while True:
+    pass
