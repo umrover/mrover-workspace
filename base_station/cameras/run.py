be9a94d69c88e11d59ba42177a523fbb8338d40a
tabiosg <gutstabios@gmail.com>
2022-03-12 14:37:53 -0500

    Change ports back to 0 and 1 for base_station camera program

--- base_station/cameras/run.py
+++ base_station/cameras/run.py
@@ -1,7 +1,7 @@
 import subprocess
 
-ports = [3, 4, 5, 6, 7, 8, 9, 10]
-pipeline = [None, None, None, None, None, None, None, None]
+ports = [0, 1]
+pipeline = [None, None]
 
 for i, val in enumerate(ports):
     cmd = "gst-launch-1.0 udpsrc port=500" + str(val) + " ! \"application/x-rtp, encoding-name=(string)H264, payload=96\" ! rtph264depay ! decodebin ! videoconvert ! autovideosink"
