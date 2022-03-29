051de5294df99f9777c31c50a2a98e26607bc990
catress <catress@umich.edu>
2022-03-29 01:11:34 -0400

    Remove reverse toggle and functionality

--- base_station/gui/src/components/WaypointEditor.vue
+++ base_station/gui/src/components/WaypointEditor.vue
@@ -43,11 +43,6 @@
       <div class="box datagrid">
         <div class="autonmode">
           <Checkbox ref="checkbox" v-bind:name="'Autonomy Mode'" v-on:toggle="toggleAutonMode($event)"/>
-          <label for="toggle_button" :class="{'active': reverseDrive}" class="toggle__button">
-            <span class="toggle__label" >Reverse</span>
-            <input type="checkbox" id="toggle_button">
-            <span class="toggle__switch" v-on:click="reverseDrive = !reverseDrive"></span>
-          </label>
         </div>
         <div class="stats">
           <p>
@@ -121,9 +116,7 @@ export default {
       },
 
       storedWaypoints: [],
-      route: [],
-
-      reverseDrive: false
+      route: []
     }
   },
 
@@ -140,7 +133,6 @@ export default {
     interval = window.setInterval(() => {
 
       this.$parent.publish('/auton', {type: 'AutonState', is_auton: this.autonEnabled})
-      this.$parent.publish('/auton_reverse', {type: 'ReverseDrive', reverse: this.reverseDrive})
 
       let course = {
         num_waypoints: this.route.length,
@@ -402,64 +394,6 @@ export default {
 
   .waypoint-headers{
     margin: 5px 0px 0px 5px;
-  }
-
-  .toggle__button {
-      vertical-align: top;
-      user-select: none;
-      cursor: pointer;
-  }
-  .toggle__button input[type="checkbox"] {
-      opacity: 0;
-      position: absolute;
-      width: 1px;
-      height: 1px;
-  }
-  .toggle__button .toggle__switch {
-      display:inline-block;
-      height:12px;
-      border-radius:6px;
-      width:40px;
-      background: #BFCBD9;
-      box-shadow: inset 0 0 1px #BFCBD9;
-      position:relative;
-      margin-left: 10px;
-      transition: all .25s;
-  }
-  .toggle__button .toggle__switch::after, 
-  .toggle__button .toggle__switch::before {
-      content: "";
-      position: absolute;
-      display: block;
-      height: 18px;
-      width: 18px;
-      border-radius: 50%;
-      left: 0;
-      top: -3px;
-      transform: translateX(0);
-      transition: all .25s cubic-bezier(.5, -.6, .5, 1.6);
-  }
-  .toggle__button .toggle__switch::after {
-      background: #4D4D4D;
-      box-shadow: 0 0 1px #666;
-  }
-  .toggle__button .toggle__switch::before {
-      background: #4D4D4D;
-      box-shadow: 0 0 0 3px rgba(0,0,0,0.1);
-      opacity:0;
-  }
-  .active .toggle__switch {
-    background: #FFEA9B;
-    box-shadow: inset 0 0 1px #FFEA9B;
-  }
-  .active .toggle__switch::after,
-  .active .toggle__switch::before{
-      transform:translateX(40px - 18px);
-  }
-  .active .toggle__switch::after {
-      left: 23px;
-      background: #FFCB05;
-      box-shadow: 0 0 1px #FFCB05;
   }
 
 </style>
