1e1dbfba97e189710c986a5f5d482b3781c146b2
Cameron Tressler <catress@umich.edu>
2022-03-29 21:58:36 -0400

    Fix strip test custom controls (#995)

--- base_station/gui/src/components/StripTest.vue
+++ base_station/gui/src/components/StripTest.vue
@@ -1,5 +1,5 @@
 <template>
-<div class="status wrap" v-bind:style="{backgroundColor: background_color}">
+<div class="status wrap" v-bind:style="{backgroundColor: current_background}">
     <div class="inner_pad">
         <div>
             <h3> Strip Test Controls </h3>
@@ -7,187 +7,116 @@
         <div class="controls">
             <div>
                 <label for="strip">Perform strip test on strip:</label>
-                    <select v-model = "strip" name="strip" id="strip">
-                        <option value=0>0</option>
-                        <option value=1>1</option>
-                        <option value=2>2</option>
-                    </select>
-                <div class="commands" v-if="strip == 0">
-                    <button v-on:click="stripTest0()"> Start Strip 0 Test </button>
-                </div>
-                <div class="commands" v-if="strip == 1">
-                    <button v-on:click="stripTest1()"> Start Strip 1 Test </button>
-                </div>
-                <div class="commands" v-if="strip == 2">
-                    <button v-on:click="stripTest2()"> Start Strip 2 Test </button>
+                <select v-model="current_strip" name="strip" id="strip">
+                    <option value=0>0</option>
+                    <option value=1>1</option>
+                    <option value=2>2</option>
+                </select>
+                <div class="commands" v-if="!servos[current_strip].has_dipped">
+                    <button v-on:click="stripTest(current_strip)"> start strip {{ current_strip }} test </button>
                 </div>
             </div>
         </div>
-    <button v-on:click="back_to_start()">Back to start</button>
-    <div class="input">
-        Angle: <input type='number' v-model.number='angle'>
-        <button v-on:click="userInput()"> Move to angle</button>
-    </div>
+        <button v-on:click="back_to_start()">Back to start</button>
+        <br>
+        <div class="input">
+            Angle: <input type='number' v-model.number="custom_angle">
+            <button v-on:click="userInput()">Move to angle</button>
+        </div>
     </div>
 </div>
 </template>
 
 <script>
-const serv2_start = 100;
-const serv2_dip = 30;
-const serv1_start = 100;
-const serv1_dip = 30;
-const serv0_start = 80;
-const serv0_dip = 10;
+const start_position = [80, 100, 100]
+const dip_position = [10, 30, 30]
 
-
-const undipped = '#A54657';
-const dipping = '#FABC2A';
-const dipped = '#AEF78E';
-
-
-
+const undipped = '#A54657'
+const dipping = '#FABC2A'
+const dipped = '#AEF78E'
+const unknown = '#EEEEEE'
 
 export default {
     data () {
         return {
-            strip: 0,
-            angle0: serv0_start,
-            angle1: serv1_start,
-            angle2: serv2_start,
-            serv2_start: serv2_start,
-            serv2_dip: serv2_dip,
-            serv2_has_dip: false,
-            serv2_color: undipped,
-            serv1_start: serv1_start,
-            serv1_dip: serv1_dip,
-            serv1_has_dip: false,
-            serv1_color: undipped,
-            serv0_start: serv0_start,
-            serv0_dip: serv0_dip,
-            serv0_has_dip: false,
-            serv0_color: undipped,
-            background_color: undipped,
-            undipped: undipped,
-            dipping: dipping,
-            dipped: dipped
+            current_strip: 0,
+            current_background: undipped,
+
+            custom_angle: 0,
+
+            servos: [
+                {
+                    angle: start_position[0],
+                    has_dipped: false,
+                    color: undipped
+                },
+                {
+                    angle: start_position[1],
+                    has_dipped: false,
+                    color: undipped
+                },
+                {
+                    angle: start_position[2],
+                    has_dipped: false,
+                    color: undipped
+                }
+            ]
         }
     },
+
     methods: {
-        setPart: function() {
+        sendCommand: function() {
             this.$parent.publish("/servo_cmd", {
-            'type': 'ServoCmd',
-            'angle0': this.angle0,
-            'angle1': this.angle1,
-            'angle2': this.angle2
+                'type': 'ServoCmd',
+                'angle0': this.servos[0].angle,
+                'angle1': this.servos[1].angle,
+                'angle2': this.servos[2].angle
             })
         },
 
-        stripTest2: function() {
-            this.angle2 = this.serv2_dip;
-            this.setPart();
-            setTimeout(() => {
-                this.angle2 = this.serv2_start;
-                this.serv2_has_dip = true;
-                this.setPart();
-                }, 10000);
-        },
+        stripTest: function(id) {
+            if (this.servos[id].has_dipped) {
+                return
+            }
+            this.servos[id].has_dipped = true
+            this.servos[id].angle = dip_position[id]
+            this.servos[id].color = dipping
+            this.current_background = dipping
 
-        stripTest1: function() {
-            this.angle1 = this.serv1_dip;
-            this.setPart();
+            this.sendCommand()
+
             setTimeout(() => {
-                this.angle1 = this.serv1_start;
-                this.serv1_has_dip = true;
-                this.setPart();
-                }, 10000);
-        },
-        
-        stripTest0: function() {
-            this.angle0 = this.serv0_dip;
-            this.setPart();
-            setTimeout(() => {
-                this.angle0 = this.serv0_start;
-                this.serv0_has_dip = true;
-                this.setPart();
-                }, 10000);
+                if (this.servos[id].angle === dip_position[id]) {
+                    this.servos[id].angle = start_position[id]
+                    this.servos[id].color = dipped
+                    this.current_background = dipped
+
+                    this.sendCommand()
+                }
+            }, 10000)
         },
 
         back_to_start: function() {
-            if (this.strip == 0) {
-                this.angle0 = serv0_start;
-            }
-            if (this.strip == 1) {
-                this.angle1 = serv1_start;
-            }
-            if (this.strip == 2) {
-                this.angle2 = serv2_start;
-            }
+            this.updateCurrentStrip(start_position[this.current_strip], false, undipped)
         },
 
         userInput: function() {
-            if (this.strip == 0) {
-                this.angle0 = this.angle;
-            }
-            if (this.strip == 1) {
-                this.angle1 = this.angle;
-            }
-            if (this.strip == 2) {
-                this.angle2 = this.angle;
-            }
+            this.updateCurrentStrip(this.custom_angle, true, unknown)
         },
+
+        updateCurrentStrip: function(angle, has_dipped, color) {
+            this.servos[this.current_strip].angle = angle
+            this.servos[this.current_strip].has_dipped = has_dipped
+            this.servos[this.current_strip].color = color
+            this.current_background = color
+
+            this.sendCommand()
+        }
     },
+
     watch: {
-        strip: function (val) {
-            if (val == 2) {
-                this.background_color = this.serv2_color;
-            } else if (val == 1) {
-                this.background_color = this.serv1_color;
-            } else {
-                this.background_color = this.serv0_color;
-            }
-        },
-        angle2: function (val) {
-            if (val == 100) {
-                if (this.serv2_has_dip) {
-                    this.serv2_color = this.dipped;
-                } else {
-                    this.serv2_color = this.undipped;
-                }
-            } else if (val == 30) {
-                this.serv2_color = this.dipping;
-            }
-            if (this.strip == 2) {
-                this.background_color = this.serv2_color;
-            }
-        },
-        angle1: function (val) {
-            if (val == 100) {
-                if (this.serv1_has_dip) {
-                    this.serv1_color = this.dipped;
-                } else {
-                    this.serv1_color = this.undipped;
-                }
-            } else if (val == 30) {
-                this.serv1_color = this.dipping;
-            }
-            if (this.strip == 1) {
-                this.background_color = this.serv1_color;
-            }
-        },
-        angle0: function (val) {
-            if (val == 80) {
-                if (this.serv0_has_dip) {
-                    this.serv0_color = this.dipped;
-                } else {
-                    this.serv0_color = this.undipped;
-                }
-            } else if (val == 10) {
-                this.serv0_color = this.dipping;
-            }
-            if (this.strip == 0) {
-                this.background_color = this.serv0_color;
-            }
+        current_strip: function(val) {
+            this.current_background = this.servos[val].color
         }
     }
 }
