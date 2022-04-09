1f5dbcc6a33a390f27b9104c0ebb80cf74baf889
ErikLytle <elytle@umich.edu>
2022-04-09 14:28:11 -0400

    made Scoop Limit Switch use toggle component

--- base_station/gui/src/components/ScoopUV.vue
+++ base_station/gui/src/components/ScoopUV.vue
@@ -1,35 +1,17 @@
 <template>
 <div class="wrap">
-  <div>
-    <h3> Scoop UV Bulb </h3>
-  </div>
-  <label for="toggle_button" :class="{'active': uv_bulb == 1}" class="toggle__button">
-      <span v-if="uv_bulb == 1" class="toggle__label" >Scoop UV On</span>
-      <span v-if="uv_bulb == 0" class="toggle__label" >Scoop UV Off</span>
-
-      <input type="checkbox" id="toggle_button">
-      <span class="toggle__switch" v-on:click="toggleUVBulb()"></span>
-  </label>
-  <div class="shutdown">
-    <label for="toggle_button" :class="{'active': shutdown == 1}" class="toggle__button">
-      <span v-if="shutdown == 1" class="toggle__label" >UV Auto shutoff On</span>
-      <span v-if="shutdown == 0" class="toggle__label" >UV Auto shutoff Off</span>
-      
-      <input type="checkbox" id="toggle_button">
-      <span class="toggle__switch" v-on:click="toggleShutdown()"></span>
-    </label>
-
-    <label for="toggle_button" :class="{'active': scoopLimit == 1}" class="toggle__button">
-        <span v-if="scoopLimit == 1" class="toggle__label" >Limit Switch On</span>
-        <span v-if="scoopLimit == 0" class="toggle__label" >Limit Switch Off</span>
-
-        <input type="checkbox" id="toggle_button">
-        <span class="toggle__switch" v-on:click="toggleLimit()"></span>
-    </label>
+    <div>
+      <h3> Scoop UV Bulb </h3>
+    </div>
 
     <div :class="{'active': scoopActive}">
       <ToggleButton id="scoop_button" labelEnableText="Scoop UV On" labelDisableText="Scoop UV Off" v-on:change="toggleScoop()"/>
     </div>
+
+    <div :class="{'active': scoopLimitActive}">
+      <ToggleButton id="scoop_limit_switch" :defaultState="true" labelEnableText="Limit Switch On" labelDisableText="Limit Switch Off" v-on:change="toggleLimit()"/>
+    </div>
+
 </div>  
 </template>
 
@@ -41,7 +23,8 @@ export default {
     return {
       scoopUV: 0,
       scoopLimit: 1,
-      scoopActive: false
+      scoopActive: false,
+      scoopLimitActive: true
     }
   },
 
@@ -59,6 +42,11 @@ export default {
     toggleScoop: function () {
       this.scoopActive = !this.scoopActive
       this.setPart(this.mosfetIDs.scoopUV, this.scoopActive)
+    },
+
+    toggleLimit: function () {
+      this.scoopLimitActive = !this.scoopLimitActive
+      this.setLimit(this.scoopLimitActive)
     },
     
     setPart: function(id, enabled) {
