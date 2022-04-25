12f9418c8ff373a8c6b6022496de5cfa3efb1ed0
catress <catress@umich.edu>
2022-04-25 17:16:50 -0400

    Implement toggle compoent (Erik's changes)

--- base_station/gui/src/components/Raman.vue
+++ base_station/gui/src/components/Raman.vue
@@ -2,26 +2,27 @@
   <div class="odom-wrap">
     <h3> Raman laser </h3>
     <div class="sacontrols buttons">
-      <label for="toggle_button" :class="{'active': raman_laser == 1}" class="toggle__button">
-        <span v-if="raman_laser == 1" class="toggle__label" >Raman Laser On</span>
-        <span v-if="raman_laser == 0" class="toggle__label" >Raman Laser Off</span>
-
-        <input type="checkbox" id="toggle_button">
-          <span class="toggle__switch" v-if="raman_laser == 0" v-on:click="raman_laser=1, setPart(mosfetIDs.raman_laser, true)"></span>
-          <span class="toggle__switch" v-if="raman_laser == 1" v-on:click="raman_laser=0, setPart(mosfetIDs.raman_laser, false)"></span>
-      </label>
+      <div>
+        <ToggleButton id="raman" v-bind:currentState="ramanActive" labelEnableText="Raman Laser On" labelDisableText="Raman Laser Off" v-on:change="toggleRaman()"/>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import ToggleButton from './ToggleButton.vue'
 
 export default {
   data () {
     return {
-      raman_laser: 0
+      ramanActive: false
     }
   },
+
+  components: {
+    ToggleButton
+  },
+
   props: {
     mosfetIDs: {
       type: Object,
@@ -29,34 +30,20 @@ export default {
     },
   },
 
-    methods: {
-      setPart: function(id, enabled) {
-        this.$parent.publish("/mosfet_cmd", {
-          'type': 'MosfetCmd',
-          'device': id,
-          'enable': enabled
-        })
-      },
-      // sendCollect: function (raman_laser) {
-      //   this.$parent.publish("/mosfet_cmd", {
-      //   'type': 'MosfetCmd',
-      //   'device': raman_laser,
-      //   'enable': true
-      // })
-      // // delete obj and disabled, do publish inside set timeout
-      //   let obj = this.$refs["raman"]
-      //   obj.disabled = true
-      //   setTimeout(this.pubHelper, 2000, raman_laser);
-      // },
-      // pubHelper: function (raman_laser){
-      //   this.$refs["raman"].disabled = false;
-      //   this.$parent.publish("/mosfet_cmd", {
-      //       'type': 'MosfetCmd',
-      //       'device': raman_laser,
-      //       'enable': false
-      //     })
-      // }
+  methods: {
+    toggleRaman: function () {
+      this.ramanActive = !this.ramanActive
+      this.setPart(this.mosfetIDs.raman_laser, this.ramanActive)
     },
+
+    setPart: function(id, enabled) {
+      this.$parent.publish("/mosfet_cmd", {
+        'type': 'MosfetCmd',
+        'device': id,
+        'enable': enabled
+      })
+    },
+  },
 }
 </script>
 
@@ -77,71 +64,5 @@ export default {
 
   .odom-wrap p {
     display: inline;
-  }
-
-  .toggle__button {
-    vertical-align: middle;
-    user-select: none;
-    cursor: pointer;
-  }
-
-  .toggle__button input[type="checkbox"] {
-      opacity: 0;
-      position: absolute;
-      width: 1px;
-      height: 1px;
-  }
-
-  .toggle__button .toggle__switch {
-      display:inline-block;
-      height:12px;
-      border-radius:6px;
-      width:40px;
-      background: #BFCBD9;
-      box-shadow: inset 0 0 1px #BFCBD9;
-      position:relative;
-      margin-left: 10px;
-      transition: all .25s;
-  }
-
-  .toggle__button .toggle__switch::after, 
-  .toggle__button .toggle__switch::before {
-      content: "";
-      position: absolute;
-      display: block;
-      height: 18px;
-      width: 18px;
-      border-radius: 50%;
-      left: 0;
-      top: -3px;
-      transform: translateX(0);
-      transition: all .25s cubic-bezier(.5, -.6, .5, 1.6);
-  }
-
-  .toggle__button .toggle__switch::after {
-      background: #4D4D4D;
-      box-shadow: 0 0 1px #666;
-  }
-
-  .toggle__button .toggle__switch::before {
-      background: #4D4D4D;
-      box-shadow: 0 0 0 3px rgba(0,0,0,0.1);
-      opacity:0;
-  }
-
-  .active .toggle__switch {
-    background: #FFEA9B;
-    box-shadow: inset 0 0 1px #FFEA9B;
-  }
-
-  .active .toggle__switch::after,
-  .active .toggle__switch::before{
-      transform:translateX(40px - 18px);
-  }
-
-  .active .toggle__switch::after {
-      left: 23px;
-      background: #FFCB05;
-      box-shadow: 0 0 1px #FFCB05;
   }
 </style>
