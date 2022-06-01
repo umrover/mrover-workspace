b89b1f66850782165d012c87cc6779e8c9a84e09
Rocker Base Station <rockerbasestation@umich.edu>
2022-06-01 10:22:21 -0400

    Use JPG map files

--- base_station/gui/src/components/AutonRoverMap.vue
+++ base_station/gui/src/components/AutonRoverMap.vue
@@ -47,7 +47,7 @@ import L from '../leaflet-rotatedmarker.js'
 const MAX_ODOM_COUNT = 1000
 const DRAW_FREQUENCY = 10
 const onlineUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-const offlineUrl = '/static/map/{z}/{x}/{y}.png'
+const offlineUrl = '/static/map/{z}/{x}/{y}.jpg'
 
 export default {
   name: 'RoverMap',
