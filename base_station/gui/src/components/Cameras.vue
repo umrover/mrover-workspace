25dcbf9338a79e2b8e1d9fea7bcc8b5b5bf51ff3
Carmen <cgiger00@gmail.com>
2022-06-03 12:53:53 -0600

    rebase

--- base_station/gui/src/components/Cameras.vue
+++ base_station/gui/src/components/Cameras.vue
@@ -9,6 +9,9 @@
     <div class="cameraselection">
       <CameraSelection class="cameraspace1" v-bind:camsEnabled="camsEnabled" v-bind:names="names" v-bind:numCams="numCams" v-on:cam_index="setCamIndex($event)"/>
     </div>
+    <div class="keyboard">
+      <GimbalControls/>
+    </div>
   </div>
 </template>
 
@@ -16,6 +19,7 @@
   import CameraSelection from './CameraSelection.vue'
   import CommIndicator from './CommIndicator.vue'
   import Checkbox from './Checkbox.vue'
+  import GimbalControls from './GimbalControls.vue'
 
   let interval;
 
@@ -107,7 +111,8 @@
     components: {
       CameraSelection,
       CommIndicator,
-      Checkbox
+      Checkbox,
+      GimbalControls
     }
   }
 </script>
