68aadce013f0d373733502c8eb94427c1a133e35
John Trager <johntrager0226@gmail.com>
2022-07-16 16:41:10 -0400

    recovery detection draft (not tested)

--- jetson/nav/recovery.cpp
+++ jetson/nav/recovery.cpp
@@ -2,12 +2,11 @@
 #include "utilities.hpp"
 
 Recovery::Recovery(int offset, double dispThresh) :
-                    dispThresh(dispThresh){
+                    offset(offset), dispThresh(dispThresh){
 
-    offset = std::chrono::seconds(offset);
 }//Recovery()
 
-bool Recovery::isStuck(int offset, double threshold){
+bool Recovery::isStuck(){
     // check displacement (TODO: have condition for if we are turning! or just don't call if we are turning?)
     // if displacement < threshold: return true
     // else return false
@@ -40,10 +39,44 @@ void Recovery::update(Odometry currOdom, time_point currTime){
 }//update()
 
 
-std::chrono::duration Recovery::getDuration(time_point p1, time_point p2){
-    // get duration between two points in seconds
+int Recovery::getDuration(time_point p1, time_point p2){
+    // get duration between two points in seconds (rounded to nearest second)
     // let p1 be the newer of the time points and p2 be the later of the time points
     // for our use case
-    return std::chrono::duration_cast<std::chrono::seconds>(p1-p2).count();
+    auto milliseconds = chrono::duration_cast<chrono::milliseconds>(p1 - p2);
+    return int((milliseconds.count()/1000.0) + 0.5);
 }//getDuration
 
+Odometry Recovery::getPointToFollow(){
+    return mPath[mPathIndex];
+}
+
+void makeRecoveryPath(const std::shared_ptr<Rover>& rover){
+    // generate recovery path (J turn)
+    // TODO: add different recovery paths or put in config
+
+    Odometry cur = rover->odometry();
+
+    double distBack = 3;
+    double distBackLeft = 2;
+    double distBackLeftLeft = 2;
+
+    // Quick and dirty method for making points
+    // can change later for less magic numbers (TODO)
+
+    // point that is straight back (rover relative)
+    Odometry back = createOdom(cur,mod(cur.bearing_deg+180,360),distBack,rover);
+
+    // point that is further back and left (rover relative)
+    Odometry backLeft = createOdom(back,mod(cur.bearing_deg+225,360),distBackLeft,rover);
+
+    // another point that is back and further left (rover relative)
+    Odometry backLeftLeft = createOdom(backLeft,mod(cur.bearing_deg+270,360),distBackLeftLeft,rover);
+
+    mPath.clear();
+
+    mPath.push_back(back);
+    mPath.push_back(backLeft);
+    mPath.push_back(backLeftLeft);
+
+}
--- jetson/nav/recovery.hpp
+++ jetson/nav/recovery.hpp
@@ -1,7 +1,7 @@
-
 #include <chrono>
 #include <ctime>
 #include <deque>
+#include <cmath>
 
 #include "rover_msgs/Odometry.hpp"
 
@@ -28,12 +28,16 @@ public:
 
     void update(Odometry currOdom, time_point currTime);
 
-
+    void makeRecoveryPath(const std::shared_ptr<Rover>& rover);
 
 private:
+    int getDuration(time_point p1, time_point p2);
+
+    Odometry getPointToFollow();
+
     // how long in seconds between the offset/lagging odom
     // and the current odom
-    std::chrono::duration offsetAmount;
+    int offsetAmount;
 
     // what minimum threshold in displacement
     // before we say rover is stuck (in meters? TODO)
@@ -42,5 +46,8 @@ private:
     // deque of running odoms
     std::deque<OdomTimePoint> odoms;
 
-    std::chrono::duration getDuration(time_point p1, time_point p2);
+    // recovery points
+    std::deque<Odometry> mPath;
+
+    size_t mPathIndex = 0;
 };
